b1ad043c2a40dc2862d45ba22602908ace777d73
Yuji Mise <miseyu2000@gmail.com>
2024-02-16 11:55:46 +0900

    feat: add dataframeTransformer action implementation (#43)

---
+++ crates/action/src/dataframe_transformer.rs
@@ -0,0 +1,85 @@
+use core::result::Result;
+use std::{collections::HashMap, sync::Arc};
+
+use anyhow::anyhow;
+use rhai::Dynamic;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use tracing::debug;
+
+use reearth_flow_workflow::graph::NodeProperty;
+
+use crate::action::{ActionContext, ActionDataframe, ActionValue};
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct PropertySchema {
+    operations: Vec<Operation>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub(crate) struct Operation {
+    transform_expr: String,
+    target_port: String,
+}
+
+impl TryFrom<NodeProperty> for PropertySchema {
+    type Error = anyhow::Error;
+
+    fn try_from(node_property: NodeProperty) -> Result<Self, anyhow::Error> {
+        serde_json::from_value(Value::Object(node_property)).map_err(|e| {
+            anyhow!(
+                "Failed to convert NodeProperty to PropertySchema with {}",
+                e
+            )
+        })
+    }
+}
+
+pub(crate) async fn run(
+    ctx: ActionContext,
+    inputs: Option<ActionDataframe>,
+) -> anyhow::Result<ActionDataframe> {
+    let props = PropertySchema::try_from(ctx.node_property)?;
+    debug!(?props, "read");
+    let inputs = inputs.ok_or(anyhow!("No Input"))?;
+    let expr_engine = Arc::clone(&ctx.expr_engine);
+    let params = inputs
+        .keys()
+        .filter(|&key| inputs.get(key).unwrap().is_some())
+        .filter(|&key| {
+            matches!(
+                inputs.get(key).unwrap().clone().unwrap(),
+                ActionValue::Bool(_)
+                    | ActionValue::Number(_)
+                    | ActionValue::String(_)
+                    | ActionValue::Map(_)
+            )
+        })
+        .map(|key| (key.to_owned(), inputs.get(key).unwrap().clone().unwrap()))
+        .collect::<HashMap<_, _>>();
+
+    let mut output = ActionDataframe::new();
+    for (port, data) in inputs {
+        let data = match data {
+            Some(data) => data,
+            None => continue,
+        };
+        let operation = props
+            .operations
+            .iter()
+            .find(|operation| operation.target_port == port)
+            .ok_or(anyhow!("No Operation"))?;
+        let ast = expr_engine.compile(&operation.transform_expr)?;
+        let scope = expr_engine.new_scope();
+        for (k, v) in &params {
+            scope.set(k, v.clone().into());
+        }
+        scope.set(&port, data.into());
+        let new_value = scope.eval_ast::<Dynamic>(&ast)?;
+        let new_value: ActionValue = new_value.try_into()?;
+        output.insert(port, Some(new_value));
+    }
+    Ok(output)
+}
