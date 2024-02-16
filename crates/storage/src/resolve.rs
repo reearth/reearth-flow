2376198168a23e69231b50086b66b044e70d3e9c
Yuji Mise <miseyu2000@gmail.com>
2024-02-16 19:53:30 +0900

    feat: update dependencies and add new state crate

--- crates/storage/src/resolve.rs
+++ crates/storage/src/resolve.rs
@@ -1,13 +1,13 @@
 use reearth_flow_common::uri::Uri;
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, RwLock};
 
 use crate::operator::resolve_operator;
 use crate::storage::Storage;
 
 #[derive(Debug, Default, Clone)]
 pub struct StorageResolver {
-    storages: Arc<Mutex<HashMap<Uri, Arc<Storage>>>>,
+    storages: Arc<RwLock<HashMap<Uri, Arc<Storage>>>>,
 }
 
 impl StorageResolver {
@@ -17,10 +17,15 @@ impl StorageResolver {
 
     /// Resolves the given URI.
     pub fn resolve(&self, uri: &Uri) -> anyhow::Result<Arc<Storage>> {
-        let mut storages = self.storages.lock().map_err(|e| anyhow::anyhow!("{}", e))?;
+        let storages = self.storages.read().map_err(|e| anyhow::anyhow!("{}", e))?;
         if let Some(storage) = storages.get(&uri.root_uri()) {
             return Ok(Arc::clone(storage));
         }
+        drop(storages);
+        let mut storages = self
+            .storages
+            .write()
+            .map_err(|e| anyhow::anyhow!("{}", e))?;
         let op = resolve_operator(uri)?;
         let storage = Arc::new(Storage::new(uri.root_uri(), op));
         storages.insert(uri.root_uri(), Arc::clone(&storage));
