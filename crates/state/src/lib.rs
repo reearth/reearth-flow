ba618905a8659725da1513cf26cec376179558b7
Yuji Mise <miseyu2000@gmail.com>
2024-02-17 14:30:22 +0900

    feat: refactor state crate and add dependencies

--- crates/state/src/lib.rs
+++ crates/state/src/lib.rs
@@ -1,170 +1,101 @@
-//! # Example
-//!
-//! ```rust,no_run,ignore
-//! use serde::{Serialize,Deserialize};
-//!
-//! #[derive(Serialize,Deserialize)]
-//! struct Foo {
-//!     foo: String
-//! }
-//!
-//! let db = State::new("data").unwrap();
-//! let f = Foo { foo: "bar".to_owned() };
-//! let id = db.save(&f).unwrap();
-//! let obj = db.get::<Foo>(&id).unwrap();
-//! db.delete(&id).unwrap();
-//! ```
-//!
-//! Creating a state instance that is living in the memory can be done like this:
-//!
-//! ```rust,no_run,ignore
-//! let db = State::new(IN_MEMORY).unwrap();
-//! ```
-
-mod file_state;
-mod json_state;
-mod memory_state;
-
-use parking_lot::RwLock;
 use serde::{Deserialize, Serialize};
 use std::{
-    collections::BTreeMap,
-    io::Result,
+    io::{Error, ErrorKind, Result},
     path::{Path, PathBuf},
     sync::Arc,
 };
 
-use file_state::FileState;
-use json_state::JsonState;
-use memory_state::MemoryState;
+use reearth_flow_common::uri::Uri;
+use reearth_flow_storage::resolve::StorageResolver;
+use reearth_flow_storage::storage::Storage;
 
 #[derive(Debug, Clone)]
-pub struct State(StateType);
-
-#[derive(Debug, Clone)]
-enum StateType {
-    File(Arc<RwLock<FileState>>, PathBuf),
-    Memory(MemoryState),
+pub struct State {
+    storage: Arc<Storage>,
+    root: PathBuf,
 }
 
-pub const IN_MEMORY: &str = "::memory::";
-
 impl State {
-    pub fn new<P: AsRef<Path>>(path: P) -> Result<Self> {
-        if path.as_ref() == Path::new(IN_MEMORY) {
-            Ok(Self(StateType::Memory(MemoryState::default())))
-        } else {
-            let s = FileState::new(path)?;
-            let p = s.path().to_path_buf();
-            Ok(Self(StateType::File(Arc::new(RwLock::new(s)), p)))
-        }
+    pub fn new(root: &Uri, storage_resolver: Arc<StorageResolver>) -> Result<Self> {
+        let storage = storage_resolver
+            .resolve(root)
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        Ok(Self {
+            storage,
+            root: Path::new(root.root()).to_path_buf(),
+        })
     }
 
-    /// Returns the storage path for the backing JSON state.
-    pub fn path(&self) -> &Path {
-        match &self.0 {
-            StateType::File(_, p) => p,
-            StateType::Memory(_) => Path::new(IN_MEMORY),
-        }
-    }
-
-    pub fn save<T>(&self, obj: &T) -> Result<String>
+    pub async fn save<T>(&self, obj: &T, id: &str) -> Result<()>
     where
         for<'de> T: Serialize + Deserialize<'de>,
     {
-        match &self.0 {
-            StateType::File(f, _) => f.write().save(obj),
-            StateType::Memory(m) => m.save(obj),
-        }
+        let s = self.object_to_string(obj)?;
+        let content = bytes::Bytes::from(s);
+        self.storage
+            .put(self.id_to_location(id).as_path(), content)
+            .await
+            .map_err(|e| Error::new(ErrorKind::Other, e))
     }
 
-    pub fn save_with_id<T>(&self, obj: &T, id: &str) -> Result<String>
-    where
-        for<'de> T: Serialize + Deserialize<'de>,
-    {
-        match &self.0 {
-            StateType::File(f, _) => f.write().save_with_id(obj, id),
-            StateType::Memory(m) => m.save_with_id(obj, id),
-        }
-    }
-
-    pub fn get<T>(&self, id: &str) -> Result<T>
+    pub async fn get<T>(&self, id: &str) -> Result<T>
     where
         for<'de> T: Deserialize<'de>,
     {
-        match &self.0 {
-            StateType::File(f, _) => f.read().get(id),
-            StateType::Memory(m) => m.get(id),
-        }
+        let result = self.storage.get(self.id_to_location(id).as_path()).await?;
+        let byte = result.bytes().await?;
+        let content =
+            String::from_utf8(byte.to_vec()).map_err(|e| Error::new(ErrorKind::Other, e))?;
+        self.string_to_object(content.as_str())
     }
 
-    pub fn all<T>(&self) -> Result<BTreeMap<String, T>>
+    pub async fn delete(&self, id: &str) -> Result<()> {
+        self.storage
+            .delete(self.id_to_location(id).as_path())
+            .await
+            .map_err(|e| Error::new(ErrorKind::Other, e))
+    }
+
+    fn string_to_object<T>(&self, s: &str) -> Result<T>
     where
         for<'de> T: Deserialize<'de>,
     {
-        match &self.0 {
-            StateType::File(f, _) => f.read().all(),
-            StateType::Memory(m) => m.all(),
-        }
+        serde_json::from_str(s).map_err(|err| Error::new(ErrorKind::Other, err))
     }
 
-    pub fn delete(&self, id: &str) -> Result<()> {
-        match &self.0 {
-            StateType::File(f, _) => f.write().delete(id),
-            StateType::Memory(m) => m.delete(id),
-        }
+    fn id_to_location(&self, id: &str) -> PathBuf {
+        Path::new(format!("{}/{}", self.root.to_str().unwrap_or_default(), id).as_str())
+            .to_path_buf()
+    }
+
+    fn object_to_string<T: Serialize>(&self, obj: &T) -> Result<String> {
+        serde_json::to_string(obj).map_err(|err| Error::new(ErrorKind::Other, err))
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use serde_derive::{Deserialize, Serialize};
-    use std::thread;
-    use tempfile::tempdir;
+    use serde::{Deserialize, Serialize};
 
     #[derive(Serialize, Deserialize)]
     struct Data {
         x: i32,
     }
 
-    fn multi_threaded_write(state: State) {
-        let mut threads: Vec<thread::JoinHandle<()>> = vec![];
-        for i in 0..20 {
-            let db = state.clone();
-            let x = Data { x: i };
-            threads.push(thread::spawn(move || {
-                db.save_with_id(&x, &i.to_string()).unwrap();
-            }));
-        }
-        for t in threads {
-            t.join().unwrap();
-        }
-        let all = state.all::<Data>().unwrap();
-        assert_eq!(all.len(), 20);
-        for (id, data) in all {
-            assert_eq!(data.x.to_string(), id);
-        }
-    }
-
-    #[test]
-    fn multi_threaded_write_with_dir() {
+    #[tokio::test]
+    async fn test_write_and_read() {
         #[derive(Serialize, Deserialize)]
         struct Data {
             x: i32,
         }
-        let dir = tempdir().expect("Could not create temporary directory");
-        let state = State::new(dir.path()).unwrap();
-        multi_threaded_write(state);
-    }
 
-    #[test]
-    fn multi_threaded_write_in_memory() {
-        #[derive(Serialize, Deserialize)]
-        struct Data {
-            x: i32,
-        }
-        let state = State::new(IN_MEMORY).unwrap();
-        multi_threaded_write(state);
+        let storage_resolver = Arc::new(StorageResolver::new());
+
+        let state = State::new(&Uri::for_test("ram://workflows/"), storage_resolver).unwrap();
+        let data = Data { x: 42 };
+        state.save(&data, "test").await.unwrap();
+        let result: Data = state.get("test").await.unwrap();
+        assert_eq!(result.x, 42);
     }
 }
