c0c5e69595968cd7c3dcd4ec9c4a946b4a0ed86c
Yuji Mise <miseyu2000@gmail.com>
2024-02-21 13:01:42 +0900

    chore: refactor action logging macros (#61)

--- crates/action-log/src/lib.rs
+++ crates/action-log/src/lib.rs
@@ -2,12 +2,15 @@ mod action;
 pub mod factory;
 mod split;
 
-use slog::info as slog_info;
-pub use slog::{o, Discard, Drain, Logger as ActionLogger};
-use std::sync::Arc;
-use tracing::info as tracing_info;
+pub use slog::info as slog_info;
+pub use tracing::info as tracing_info;
 
-pub fn action_log(logger: Arc<ActionLogger>, message: &str) {
-    slog_info!(logger, "{}", message);
-    tracing_info!(message);
+pub use slog::{o, Discard, Drain, Logger as ActionLogger};
+
+#[macro_export]
+macro_rules! action_log {
+    ($logger:expr, $($args:tt)*) => {{
+        $crate::slog_info!($logger, $($args)*);
+        $crate::tracing_info!($($args)*);
+    }};
 }
