067ac3b950ce01cec35891912e29a7e6649ce047
Yuji Mise <miseyu2000@gmail.com>
2024-03-14 20:03:31 +0900

    chore(worker): remove unused modules and refactor imports

---
+++ worker/crates/action-universal/src/attribute.rs
@@ -0,0 +1,5 @@
+pub(crate) mod aggregator;
+pub(crate) mod keeper;
+pub(crate) mod manager;
+pub(crate) mod merger;
+pub(crate) mod sorter;
--- worker/crates/action-universal/src/attribute_manager.rs
+++
@@ -1,3 +0,0 @@
-mod runner;
-
-pub use runner::AttributeManager;
