1ac3479fa95076cf740236c1b56da5b81817c57e
Yuji Mise <miseyu2000@gmail.com>
2024-03-23 00:21:48 +0900

    chore(worker): Refactor DataframeTransformer to support additional operations (#107)

--- worker/crates/action-universal/src/dataframe/transformer.rs
+++ worker/crates/action-universal/src/dataframe/transformer.rs
@@ -25,11 +25,12 @@ pub(super) struct Operation {
 #[typetag::serde(name = "DataframeTransformer")]
 impl Action for DataframeTransformer {
     async fn run(&self, ctx: ActionContext, inputs: Option<ActionDataframe>) -> ActionResult {
-        let inputs = inputs.ok_or(Error::input("No Input"))?;
+        let inputs = inputs.unwrap_or_default();
         let expr_engine = Arc::clone(&ctx.expr_engine);
         let params = convert_dataframe_to_scope_params(&inputs);
 
         let mut output = ActionDataframe::new();
+        let ports = inputs.keys().cloned().collect::<Vec<_>>();
         for (port, data) in inputs {
             let data = match data {
                 Some(data) => data,
@@ -44,20 +45,116 @@ impl Action for DataframeTransformer {
                 continue;
             }
             let operation = operation.unwrap();
-            let ast = expr_engine
-                .compile(&operation.transform_expr)
-                .map_err(Error::internal_runtime)?;
             let scope = expr_engine.new_scope();
             for (k, v) in &params {
                 scope.set(k, v.clone().into());
             }
             scope.set(port.as_ref(), data.into());
             let new_value = scope
-                .eval_ast::<Dynamic>(&ast)
+                .eval::<Dynamic>(&operation.transform_expr)
                 .map_err(Error::internal_runtime)?;
             let new_value: ActionValue = new_value.try_into()?;
             output.insert(port.clone(), Some(new_value));
         }
+        for operation in &self.operations {
+            if ports.contains(&operation.target_port) {
+                continue;
+            }
+            let scope = expr_engine.new_scope();
+            for (k, v) in &params {
+                scope.set(k, v.clone().into());
+            }
+            let new_value = scope
+                .eval::<Dynamic>(operation.transform_expr.as_str())
+                .map_err(Error::internal_runtime)?;
+            let new_value: ActionValue = new_value.try_into()?;
+            output.insert(operation.target_port.clone(), Some(new_value));
+        }
         Ok(output)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[tokio::test]
+    async fn test_dataframe_transformer() {
+        // Create a sample input dataframe
+        let mut input_data = ActionDataframe::new();
+        input_data.insert(
+            Port::new("port1"),
+            Some(ActionValue::String("value1".to_owned())),
+        );
+        input_data.insert(
+            Port::new("port2"),
+            Some(ActionValue::Number(serde_json::Number::from(42))),
+        );
+
+        // Create a sample context
+        let ctx = ActionContext::default();
+
+        // Create a sample dataframe transformer
+        let transformer = DataframeTransformer {
+            operations: vec![
+                Operation {
+                    transform_expr: r#"`${env.get("port1")}hogehoge`"#.to_owned(),
+                    target_port: Port::new("port1"),
+                },
+                Operation {
+                    transform_expr: r#"env.get("port2") * 2"#.to_owned(),
+                    target_port: Port::new("port2"),
+                },
+            ],
+        };
+
+        // Run the transformer
+        let result = transformer.run(ctx, Some(input_data)).await;
+
+        // Check the result
+        assert!(result.is_ok());
+        let output_data = result.unwrap();
+        assert_eq!(
+            output_data.get(&Port::new("port1")),
+            Some(&Some(ActionValue::String("value1hogehoge".to_owned())))
+        );
+        assert_eq!(
+            output_data.get(&Port::new("port2")),
+            Some(&Some(ActionValue::Number(serde_json::Number::from(84))))
+        );
+    }
+
+    #[tokio::test]
+    async fn test_dataframe_transformer_no_input() {
+        // Create a sample context
+        let ctx = ActionContext::default();
+
+        // Create a sample dataframe transformer
+        let transformer = DataframeTransformer {
+            operations: vec![
+                Operation {
+                    transform_expr: r#""hogehoge""#.to_owned(),
+                    target_port: Port::new("port1"),
+                },
+                Operation {
+                    transform_expr: "15".to_owned(),
+                    target_port: Port::new("port2"),
+                },
+            ],
+        };
+
+        // Run the transformer without input
+        let result = transformer.run(ctx, None).await;
+
+        // Check the result
+        assert!(result.is_ok());
+        let output_data = result.unwrap();
+        assert_eq!(
+            output_data.get(&Port::new("port1")),
+            Some(&Some(ActionValue::String("hogehoge".to_owned())))
+        );
+        assert_eq!(
+            output_data.get(&Port::new("port2")),
+            Some(&Some(ActionValue::Number(serde_json::Number::from(15))))
+        );
     }
 }
