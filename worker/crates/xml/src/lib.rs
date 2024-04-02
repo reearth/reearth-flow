a2f875f75844dd91edb106454cb48f23a9e5da75
Yuji Mise <miseyu2000@gmail.com>
2024-04-02 19:31:48 +0900

    feat(worker): add XML module and dependencies

---
+++ worker/crates/xml/src/lib.rs
@@ -0,0 +1,17 @@
+mod convert;
+mod decl;
+mod display;
+mod dom_impl;
+pub mod error;
+mod mutex;
+mod name;
+mod namespace;
+mod node;
+mod options;
+pub mod parser;
+mod syntax;
+mod text;
+mod trait_impl;
+mod traits;
+
+pub(crate) type Result<T, E = error::Error> = std::result::Result<T, E>;
