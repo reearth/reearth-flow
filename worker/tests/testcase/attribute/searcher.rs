c2216bb0a30062f0eb9180c41327ed44879f783b
Akihito Kirisaki <kirisaki@klara.works>
2024-04-04 18:59:46 +0900

    tests(worker): finish test

--- worker/tests/testcase/attribute/searcher.rs
+++ worker/tests/testcase/attribute/searcher.rs
@@ -6,32 +6,33 @@ use crate::helper::init_test_runner;
 async fn test_run() {
     let executor = init_test_runner("attribute/searcher", vec!["searcher.json"]).await;
     let result = executor.start().await;
-    println!("{:?}", result);
     assert!(result.is_ok());
     let result = result.unwrap();
     let default_port = result.get(&Port::new("default")).unwrap();
     assert!(default_port.is_some());
     let default_port = default_port.clone().unwrap();
     match default_port {
-        ActionValue::Array(array) => {
-            assert_eq!(array.len(), 2);
-            let first = array.first().unwrap();
-            let second = array.get(1).unwrap();
-            match first {
-                ActionValue::Map(first) => {
-                    let first = first.clone();
-                    let city = first.get("city").unwrap();
-                    assert_eq!(city, &ActionValue::String("Tokyo".to_string()));
-                }
+        ActionValue::Map(kv) => {
+            let kv = match kv.get("sample_data").unwrap() {
+                ActionValue::Map(kv) => kv,
                 _ => panic!("unexpected value"),
-            }
-            match second {
-                ActionValue::Map(second) => {
-                    let second = second.clone();
-                    let city = second.get("city").unwrap();
-                    assert_eq!(city, &ActionValue::String("Osaka".to_string()));
-                }
-                _ => panic!("unexpected value"),
+            };
+            match kv.get("string").unwrap() {
+                ActionValue::Array(xs) => assert_eq!(xs.len(), 1),
+                _ => panic!("unexpected value")
+            };
+            match kv.get("map").unwrap() {
+                ActionValue::Map(kv) => {
+                    match kv.get("item2").unwrap() {
+                        ActionValue::Array(xs) => assert_eq!(xs.len(), 1),
+                        _ => panic!("unexpected value"),
+                    };
+                    match kv.get("item3").unwrap() {
+                        ActionValue::Array(xs) => assert_eq!(xs.len(), 3),
+                        _ => panic!("unexpexted value"),
+                    }
+                },
+                _ => panic!("unexpected")
             }
         }
         _ => panic!("unexpected value"),
