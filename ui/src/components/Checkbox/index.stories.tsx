79ff9f415c1af39dd3ad927d516790c7623052f4
jashan <20891087+jashanbhullar@users.noreply.github.com>
2024-04-23 14:59:39 +0530

    - checkbox story

---
+++ ui/src/components/Checkbox/index.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Checkbox } from ".";
+
+const meta = {
+  component: Checkbox,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof Checkbox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const commonArgs = {
+  children: "Button",
+};
+
+export const Default: Story = {
+  args: {
+    ...commonArgs,
+  },
+};
