548bd0f5839e2b5e802c7a5a73780fd388a41010
jashan <20891087+jashanbhullar@users.noreply.github.com>
2024-04-23 15:04:49 +0530

    - Input story

---
+++ ui/src/components/Input/index.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Input } from ".";
+
+const meta = {
+  component: Input,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const commonArgs = {};
+
+export const Default: Story = {
+  args: {
+    ...commonArgs,
+  },
+};
