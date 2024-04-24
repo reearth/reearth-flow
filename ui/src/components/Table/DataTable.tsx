c803989dd5d4aec3fb17350a4e51d73e46d0a3bd
jashan <20891087+jashanbhullar@users.noreply.github.com>
2024-04-24 09:22:25 +0530

    Added - filtering - sorting - select columns - Visibility - pagination in form of stories

--- ui/src/components/Table/DataTable.tsx
+++ ui/src/components/Table/DataTable.tsx
@@ -1,5 +1,24 @@
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import {
+  ColumnDef,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { useState } from "react";
 
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  Button,
+  Input,
+} from "@flow/components";
 import { useT } from "@flow/providers";
 
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from ".";
@@ -7,54 +26,141 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from ".
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
+  selectColumns?: boolean;
+  showPagination?: boolean;
+  showFiltering?: boolean;
 }
 
-function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+function DataTable<TData, TValue>({
+  columns,
+  data,
+  selectColumns = false,
+  showPagination = false,
+  showFiltering = false,
+}: DataTableProps<TData, TValue>) {
   const t = useT();
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState({});
+  const [globalFilter, setGlobalFilter] = useState("");
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    // Pagination
+    getPaginationRowModel: showPagination ? getPaginationRowModel() : undefined,
+    // Sorting
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    // Visibility
+    onColumnVisibilityChange: setColumnVisibility,
+    // Row selection
+    onRowSelectionChange: setRowSelection,
+    // Filtering
+    onGlobalFilterChange: setGlobalFilter,
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting,
+      columnVisibility,
+      rowSelection,
+      globalFilter,
+    },
   });
 
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map(headerGroup => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map(header => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(header.column.columnDef.header, header.getContext())}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map(row => (
-              <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
-                {row.getVisibleCells().map(cell => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+    <div>
+      <div className="flex items-center py-4">
+        {showFiltering && (
+          <Input
+            placeholder={t("Search") + "..."}
+            value={globalFilter ?? ""}
+            onChange={e => setGlobalFilter(String(e.target.value))}
+            className="max-w-sm"
+          />
+        )}
+        {selectColumns && (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="ml-auto">
+                {t("Columns")}
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              {table
+                .getAllColumns()
+                .filter(column => column.getCanHide())
+                .map(column => {
+                  return (
+                    <DropdownMenuCheckboxItem
+                      key={column.id}
+                      className="capitalize"
+                      checked={column.getIsVisible()}
+                      onCheckedChange={value => column.toggleVisibility(!!value)}>
+                      {column.id}
+                    </DropdownMenuCheckboxItem>
+                  );
+                })}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map(headerGroup => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map(header => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(header.column.columnDef.header, header.getContext())}
+                    </TableHead>
+                  );
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                {t("No Results")}
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map(row => (
+                <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                  {row.getVisibleCells().map(cell => (
+                    <TableCell key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  {t("No Results")}
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      {showPagination && (
+        <div className="flex items-center justify-end space-x-2 py-4">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}>
+            {t("Previous")}
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}>
+            {t("Next")}
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
--- ui/src/components/Table/index.stories.tsx
+++ ui/src/components/Table/index.stories.tsx
@@ -1,4 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { ArrowUpDown } from "lucide-react";
+
+import { Button, Checkbox } from "@flow/components";
 
 import { DataTable } from ".";
 
@@ -48,5 +51,99 @@ const commonArgs = {
 export const Table: Story = {
   args: {
     ...commonArgs,
+  },
+};
+
+export const SelectColumns: Story = {
+  args: {
+    ...commonArgs,
+    selectColumns: true,
+  },
+};
+
+export const SortTable: Story = {
+  args: {
+    ...commonArgs,
+    columns: [
+      {
+        accessorKey: "status",
+        header: "Status",
+      },
+      {
+        // More sorting can be added to the table
+        accessorKey: "email",
+        header: ({ column }) => {
+          return (
+            <Button
+              variant="ghost"
+              onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+              Email
+              <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+          );
+        },
+      },
+      {
+        accessorKey: "amount",
+        header: "Amount",
+      },
+    ],
+  },
+};
+
+export const SelectRows: Story = {
+  args: {
+    ...commonArgs,
+    columns: [
+      {
+        id: "status",
+        header: ({ table }) => (
+          <Checkbox
+            checked={
+              table.getIsAllPageRowsSelected() ||
+              (table.getIsSomePageRowsSelected() && "indeterminate")
+            }
+            onCheckedChange={value => table.toggleAllPageRowsSelected(!!value)}
+            aria-label="Select all"
+          />
+        ),
+        cell: ({ row }) => (
+          <Checkbox
+            checked={row.getIsSelected()}
+            onCheckedChange={value => row.toggleSelected(!!value)}
+            aria-label="Select row"
+          />
+        ),
+        enableSorting: false,
+        enableHiding: false,
+      },
+      ...commonArgs.columns,
+    ],
+  },
+};
+
+export const ShowPagination: Story = {
+  args: {
+    ...commonArgs,
+    data: [...Array(100).keys()].map(_ => ({
+      amount: Math.floor(Math.random() * 300),
+      email: Math.random().toString(36).slice(2, 7) + "@mail.com",
+      id: Math.random().toString(36).slice(2, 7),
+      status: ["success", "failure", "pending", "canceled"][Math.floor(Math.random() * 4)],
+    })),
+    showPagination: true,
+  },
+};
+
+export const ShowFiltering: Story = {
+  args: {
+    ...commonArgs,
+    data: [...Array(100).keys()].map(_ => ({
+      amount: Math.floor(Math.random() * 300),
+      email: Math.random().toString(36).slice(2, 7) + "@mail.com",
+      id: Math.random().toString(36).slice(2, 7),
+      status: ["success", "failure", "pending", "canceled"][Math.floor(Math.random() * 4)],
+    })),
+    showFiltering: true,
   },
 };
