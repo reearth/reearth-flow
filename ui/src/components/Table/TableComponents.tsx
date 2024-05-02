f1ae4ff3eb26623948de36d4dca403aa39ff5358
jashan <20891087+jashanbhullar@users.noreply.github.com>
2024-05-02 11:09:40 +0530

    - Minor fix in row selection

--- ui/src/components/Table/TableComponents.tsx
+++ ui/src/components/Table/TableComponents.tsx
@@ -44,7 +44,7 @@ const TableRow = React.forwardRef<HTMLTableRowElement, React.HTMLAttributes<HTML
     <tr
       ref={ref}
       className={cn(
-        "border-b border-zinc-700 transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+        "border-b border-zinc-700 transition-colors hover:bg-muted/50 data-[state=selected]:bg-zinc-700",
         className,
       )}
       {...props}
