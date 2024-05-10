3e9577e3d42d743ecd24dba92c91ed5dc20b6276
Yuji Mise <miseyu2000@gmail.com>
2024-05-10 10:33:30 +0900

    feat(worker): add domain of definition validator action (#185)

--- worker/crates/action-universal/src/plateau.rs
+++ worker/crates/action-universal/src/plateau.rs
@@ -1,5 +1,7 @@
 mod dictionaries_initiator;
+mod domain_of_definition_validator;
 mod types;
 mod udx_folder_extractor;
 mod xml_attribute_extractor;
+pub use dictionaries_initiator::DictionariesInitiator;
 pub use udx_folder_extractor::UdxFolderExtractor;
