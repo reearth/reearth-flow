66a94d3d33792630e291581b50479c91c9312528
jashan <20891087+jashanbhullar@users.noreply.github.com>
2024-05-15 15:08:06 +0530

    - 2D/3D buttons in the map

--- ui/src/features/BottomPanel/components/Map/index.tsx
+++ ui/src/features/BottomPanel/components/Map/index.tsx
@@ -1,12 +1,25 @@
 import { CoreVisualizer } from "@reearth/core";
 
+import { Button } from "@flow/components";
 import fires from "@flow/mock_data/fires.json";
 
 type Props = {};
 
 const Map: React.FC<Props> = () => {
+  const buttons = ["2D", "3D"];
   return (
     <div className="relative w-6/12">
+      <div className="flex flex-col flex-wrap bg-zinc-900/50 border border-zinc-700 rounded-md text-zinc-400 transition-all top-2 left-2 absolute z-10 p-1">
+        {buttons.map(b => (
+          <Button
+            className={`transition-all text-zinc-400 hover:bg-zinc-700 hover:text-zinc-100 cursor-pointer `}
+            variant="ghost"
+            size="icon"
+            key={b}>
+            {b}
+          </Button>
+        ))}
+      </div>
       <CoreVisualizer
         ready={true}
         engine="cesium"
