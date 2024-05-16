4c0b9d1be3498d0479cab3cce9cd128716cf8051
KaWaite <34051327+KaWaite@users.noreply.github.com>
2024-05-16 16:41:33 +0900

    fix(ui): refactor Map

--- ui/src/features/BottomPanel/components/Map/index.tsx
+++ ui/src/features/BottomPanel/components/Map/index.tsx
@@ -1,4 +1,4 @@
-import { CoreVisualizer } from "@reearth/core";
+import { CoreVisualizer, SceneMode, SceneProperty, Layer } from "@reearth/core";
 import { useEffect, useState } from "react";
 
 import { Button } from "@flow/components";
@@ -6,13 +6,53 @@ import fires from "@flow/mock_data/fires.json";
 
 type Props = {};
 
-type SceneMode = "3d" | "2d" | "columbus";
-
 const sceneModes: SceneMode[] = ["2d", "3d"];
 
 const Map: React.FC<Props> = () => {
   const [isReady, setIsReady] = useState(false);
   const [sceneMode, setSceneMode] = useState<SceneMode>("2d");
+
+  const sceneProperty: SceneProperty = {
+    default: {
+      sceneMode,
+    },
+    camera: {
+      camera: {
+        lng: 127.05177672074426,
+        lat: -6.260283141094241,
+        height: 7594277.78896907,
+        heading: 1.129814464206902e-9,
+        pitch: -1.5707963267948966,
+        roll: 0,
+        fov: 1,
+        aspectRatio: 1,
+      },
+    },
+    tiles: [
+      {
+        id: "default",
+        tile_type: "default",
+      },
+    ],
+  };
+
+  const layers: Layer[] = [
+    {
+      id: "marker",
+      type: "simple",
+      data: {
+        type: "geojson",
+        value: fires,
+      },
+      marker: {
+        imageColor: {
+          expression: {
+            conditions: [["true", "color('#FF0000')"]],
+          },
+        },
+      },
+    },
+  ];
 
   useEffect(() => {
     if (isReady) return;
@@ -34,48 +74,11 @@ const Map: React.FC<Props> = () => {
         ))}
       </div>
       <CoreVisualizer
-        ready={isReady}
         engine="cesium"
-        sceneProperty={{
-          default: {
-            sceneMode,
-          },
-          camera: {
-            camera: {
-              lng: 127.05177672074426,
-              lat: -6.260283141094241,
-              height: 7594277.78896907,
-              heading: 1.129814464206902e-9,
-              pitch: -1.5707963267948966,
-              roll: 0,
-              fov: 1,
-              aspectRatio: 1,
-            },
-          },
-          tiles: [
-            {
-              id: "default",
-              tile_type: "default",
-            },
-          ],
-        }}
-        layers={[
-          {
-            id: "marker",
-            type: "simple",
-            data: {
-              type: "geojson",
-              value: fires,
-            },
-            marker: {
-              imageColor: {
-                expression: {
-                  conditions: [["true", "color('#FF0000')"]],
-                },
-              },
-            },
-          },
-        ]}
+        isBuilt
+        ready={isReady}
+        sceneProperty={sceneProperty}
+        layers={layers}
       />
     </div>
   );
