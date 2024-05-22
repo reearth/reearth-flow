f6f482fe493bd905593400134c12036a83d18e33
json singh <20891087+jashanbhullar@users.noreply.github.com>
2024-05-22 12:23:58 +0530

    - added auth0 code - removed the cognito part - added comments to be addressed later

---
+++ ui/src/auth/authProvider.tsx
@@ -0,0 +1,42 @@
+import { Auth0Provider } from "@auth0/auth0-react";
+import React, { createContext, ReactNode, useState } from "react";
+
+import { getAuthInfo, getSignInCallbackUrl, logInToTenant } from "@flow/config";
+
+import { useAuth0Auth } from "./auth0Auth";
+import type { AuthHook } from "./authHook";
+
+export const AuthContext = createContext<AuthHook | null>(null);
+
+const Auth0Wrapper = ({ children }: { children: ReactNode }) => {
+  const auth = useAuth0Auth();
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+};
+
+export const AuthProvider: React.FC<{ children?: ReactNode }> = ({ children }) => {
+  const [authInfo] = useState(() => {
+    // TODO: The below function currently does nothing
+    logInToTenant(); // note that it includes side effect
+    return getAuthInfo();
+  });
+
+  const domain = authInfo?.auth0Domain;
+  const clientId = authInfo?.auth0ClientId;
+  const audience = authInfo?.auth0Audience;
+
+  return domain && clientId ? (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        audience: audience,
+        scope: "openid profile email offline_access",
+        redirect_uri: getSignInCallbackUrl(),
+      }}
+      useRefreshTokens
+      useRefreshTokensFallback
+      cacheLocation="localstorage">
+      <Auth0Wrapper>{children}</Auth0Wrapper>
+    </Auth0Provider>
+  ) : null;
+};
---
+++ ui/src/auth/index.tsx
@@ -0,0 +1,18 @@
+import { withAuthenticationRequired } from "@auth0/auth0-react";
+import { ReactNode } from "react";
+
+import { useAuthenticationRequired } from "./useAuth";
+
+export { AuthProvider } from "./authProvider";
+export { useAuth, useCleanUrl, useAuthenticationRequired } from "./useAuth";
+
+const AuthenticationRequiredPage: React.FC<{ children?: ReactNode }> = ({ children }) => {
+  const [isAuthenticated] = useAuthenticationRequired(); // TODO: show error
+  return isAuthenticated && children ? <>{children}</> : null;
+};
+
+const withAuthorisation = (): ((props: any) => React.FC<any>) => {
+  return withAuthenticationRequired as unknown as (props: any) => React.FC<any>;
+};
+
+export const AuthenticatedPage = withAuthorisation()(AuthenticationRequiredPage);
