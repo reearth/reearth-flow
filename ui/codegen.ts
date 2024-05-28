571e5160bff36f0e1f2585a26b25700b5b5c2bb9
json singh <20891087+jashanbhullar@users.noreply.github.com>
2024-05-28 18:22:58 +0530

    - updated codegen

--- ui/codegen.ts
+++ ui/codegen.ts
@@ -2,50 +2,15 @@ import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   schema: "../api/gql/*.graphql",
-  documents: ["src/**/*.tsx"],
+  documents: ["src/**/*.tsx", "src/**/*.ts"],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
     "./src/lib/gql/__gen__/": {
       preset: "client",
+      // TODO: Add necessary plugins
+      // plugins:
     },
   },
 };
-
-// TODO: merge with Reearth codegen
-// const rootGQLDirectory = "src/services/gql/";
-
-// const rootGenerateDirectory = `${rootGQLDirectory}__gen__/`;
-
-// const config: CodegenConfig = {
-//   overwrite: true,
-//   schema: "../server/gql/*.graphql",
-//   documents: [`${rootGQLDirectory}fragments/*.ts`, `${rootGQLDirectory}queries/*.ts`],
-//   generates: {
-//     [rootGenerateDirectory]: {
-//       preset: "client",
-//       presetConfig: {
-//         gqlTagName: "gql",
-//         fragmentMasking: false,
-//       },
-//       config: {
-//         useTypeImports: true,
-//         scalars: {
-//           DateTime: "Date",
-//           FileSize: "number",
-//           ID: "string",
-//           Cursor: "string",
-//           URL: "string",
-//           Lang: "string",
-//           TranslatedString: "{ [lang in string]?: string } | null",
-//           JSON: "any",
-//         },
-//       },
-//     },
-//     [`${rootGenerateDirectory}/fragmentMatcher.json`]: {
-//       plugins: ["fragment-matcher"],
-//     },
-//   },
-//   ignoreNoDocuments: true,
-// };
 
 export default config;
