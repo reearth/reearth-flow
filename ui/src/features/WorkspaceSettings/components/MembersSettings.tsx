6d46f7e57a0af27f106691c36610faf85f30e5e4
KaWaite <kyle.waite@outlook.com>
2024-06-03 11:28:35 +0900

    edit members ui

--- ui/src/features/WorkspaceSettings/components/MembersSettings.tsx
+++ ui/src/features/WorkspaceSettings/components/MembersSettings.tsx
@@ -3,12 +3,11 @@ import { useState } from "react";
 
 import {
   Button,
+  Checkbox,
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-  Input,
-  Label,
 } from "@flow/components";
 import { useT } from "@flow/providers";
 import { useCurrentWorkspace } from "@flow/stores";
@@ -16,10 +15,13 @@ import { Role } from "@flow/types";
 
 type Filter = "all" | Role;
 
+const roles = ["admin", "reader", "writer"];
+
 const MembersSettings: React.FC = () => {
   const t = useT();
   const [currentWorkspace] = useCurrentWorkspace();
-  const members = currentWorkspace?.members ?? [];
+
+  const [currentFilter, setFilter] = useState<Filter>("all");
 
   const filters: { id: Filter; title: string }[] = [
     { id: "all", title: t("All") },
@@ -28,7 +30,12 @@ const MembersSettings: React.FC = () => {
     { id: "writer", title: t("Writer") },
   ];
 
-  const [currentFilter, setFilter] = useState<Filter>("all");
+  const members =
+    currentWorkspace?.members?.filter(m =>
+      currentFilter !== "all" ? m.role === currentFilter : true,
+    ) ?? [];
+
+  const [selectedMembers, setSelectedMembers] = useState<string[]>([]);
 
   return (
     <div>
@@ -38,11 +45,45 @@ const MembersSettings: React.FC = () => {
           <Button>{t("Add Members")}</Button>
         </div>
         <div className="border border-zinc-700 rounded font-extralight">
-          <div className="flex justify-between items-center gap-2 p-2 border-b border-zinc-700">
+          <div className="flex justify-between items-center gap-2 p-2 border-b border-zinc-700 h-[42px]">
             <div className="flex items-center gap-2">
+              <Checkbox
+                className="border-zinc-600 mx-2"
+                checked={selectedMembers.length === members.length}
+                onClick={() =>
+                  setSelectedMembers(
+                    selectedMembers.length !== members.length ? members.map(m => m.userId) : [],
+                  )
+                }
+              />
               <User />
-              <p>{`${members.length} ${t("Members")}`}</p>
+              <p>
+                {selectedMembers.length
+                  ? `${selectedMembers.length} ${selectedMembers.length === 1 ? t("member selected") : t("members selected")}`
+                  : `${members.length} ${t("Members")}`}
+              </p>
             </div>
+            {selectedMembers.length > 0 && (
+              <div className="flex gap-4">
+                <DropdownMenu>
+                  <DropdownMenuTrigger className="flex items-center gap-1">
+                    <p className="text-sm">{t("Change role")}</p>
+                    <CaretDown className="w-2 h-2" />
+                  </DropdownMenuTrigger>
+
+                  <DropdownMenuContent className="min-w-[70px]">
+                    {roles.map((role, idx) => (
+                      <DropdownMenuItem key={idx} onClick={() => console.log(role)}>
+                        {role}
+                      </DropdownMenuItem>
+                    ))}
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                <Button className="h-[25px]" size="sm" variant="destructive">
+                  {t("Remove selected")}
+                </Button>
+              </div>
+            )}
             <div>
               <DropdownMenu>
                 <DropdownMenuTrigger className="flex items-center gap-2">
@@ -64,31 +105,24 @@ const MembersSettings: React.FC = () => {
             </div>
           </div>
           <div className="max-h-[50vh] overflow-auto">
-            {members
-              .filter(m => (currentFilter !== "all" ? m.role === currentFilter : true))
-              ?.map(member => (
-                <div key={member.userId} className="flex items-center gap-8 px-4 py-2">
-                  <p>{member.user.name}</p>
-                  <p className="font-thin capitalize text-sm">{member.role}</p>
-                </div>
-              ))}
+            {members.map(member => (
+              <div key={member.userId} className="flex items-center gap-4 px-4 py-2">
+                <Checkbox
+                  className="border-zinc-600"
+                  checked={selectedMembers.includes(member.userId)}
+                  onClick={() =>
+                    setSelectedMembers(prev =>
+                      prev.includes(member.userId)
+                        ? [...prev.filter(pm => pm !== member.userId)]
+                        : [...prev, member.userId],
+                    )
+                  }
+                />
+                <p>{member.user.name}</p>
+                <p className="px-4 font-thin capitalize text-sm">{member.role}</p>
+              </div>
+            ))}
           </div>
-        </div>
-        <div className="flex flex-col gap-2">
-          <Label htmlFor="workspace-name">{t("Workspace Name")}</Label>
-          <Input
-            id="workspace-name"
-            placeholder={t("Workspace Name")}
-            // defaultValue={currentWorkspace?.name}
-          />
-        </div>
-        <div className="flex flex-col gap-2">
-          <Label htmlFor="workspace-description">{t("Workspace Description")}</Label>
-          <Input
-            id="workspace-description"
-            placeholder={t("Workspace Description")}
-            // defaultValue={currentWorkspace?.description}
-          />
         </div>
         <Button className="self-end">{t("Save")}</Button>
       </div>
