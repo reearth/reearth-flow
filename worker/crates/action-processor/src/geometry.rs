d56a101d5a238f0987fb48aa98f06c52c5312031
Yuji Mise <miseyu2000@gmail.com>
2024-06-05 19:08:14 +0900

    feat(worker): add splitter module to the geometry crate

--- worker/crates/action-processor/src/geometry.rs
+++ worker/crates/action-processor/src/geometry.rs
@@ -3,4 +3,5 @@ pub mod errors;
 pub mod extruder;
 pub mod filter;
 pub mod mapping;
+pub mod splitter;
 pub mod three_dimention_box_replacer;
--- worker/crates/action-processor/src/geometry/mapping.rs
+++ worker/crates/action-processor/src/geometry/mapping.rs
@@ -5,7 +5,8 @@ use reearth_flow_runtime::node::{NodeKind, ProcessorFactory};
 
 use super::{
     coordinate_system_setter::CoordinateSystemSetterFactory, extruder::ExtruderFactory,
-    filter::GeometryFilterFactory, three_dimention_box_replacer::ThreeDimentionBoxReplacerFactory,
+    filter::GeometryFilterFactory, splitter::GeometrySplitterFactory,
+    three_dimention_box_replacer::ThreeDimentionBoxReplacerFactory,
 };
 
 pub static ACTION_MAPPINGS: Lazy<HashMap<String, NodeKind>> = Lazy::new(|| {
@@ -14,6 +15,7 @@ pub static ACTION_MAPPINGS: Lazy<HashMap<String, NodeKind>> = Lazy::new(|| {
         Box::<ExtruderFactory>::default(),
         Box::<ThreeDimentionBoxReplacerFactory>::default(),
         Box::<GeometryFilterFactory>::default(),
+        Box::<GeometrySplitterFactory>::default(),
     ];
     factories
         .into_iter()
