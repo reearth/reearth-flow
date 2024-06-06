997f23fb3ccb638029ed6ea13a8f419c884f7dba
json singh <20891087+jashanbhullar@users.noreply.github.com>
2024-06-06 11:37:15 +0530

    - User query refactored

--- ui/src/types/user.ts
+++ ui/src/types/user.ts
@@ -1,7 +1,15 @@
+import { ApiResponse } from "./api";
+
+export type GetMe = {
+  me: User | undefined | null;
+  isLoading: boolean;
+} & ApiResponse;
+
 export type User = {
   id: string;
   name: string;
   email: string;
+  myWorkspaceId: string;
   lang?: string;
   theme?: string;
   // workspace?: string;
