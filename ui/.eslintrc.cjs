d899b3dea0ac2a18e828aca32b99a8b18950036c
json singh <20891087+jashanbhullar@users.noreply.github.com>
2024-06-07 16:25:58 +0530

    - adding tanstack react-query eslint

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -1,25 +1,33 @@
 module.exports = {
-  extends: ["reearth", "plugin:storybook/recommended"],
+  extends: [
+    "reearth",
+    "plugin:storybook/recommended",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
+  ],
   root: true,
   env: { browser: true, es2020: true },
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
-    "import/order": ["warn", {
-      "pathGroups": [
-        {
-          "pattern": "@flow/**",
-          "group": "external",
-          "position": "after",
-        }
-      ],
-      "pathGroupsExcludedImportTypes": ["builtin"],
-      "newlines-between": "always",
-      "alphabetize": {
-        "order": "asc",
-        "caseInsensitive": true
-      }
-    }]
-  }
-}
+    "import/order": [
+      "warn",
+      {
+        pathGroups: [
+          {
+            pattern: "@flow/**",
+            group: "external",
+            position: "after",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    "@tanstack/query/exhaustive-deps": 0,
+  },
+};
