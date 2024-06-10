fd60c2fc73452009972228e8fa237941b04fe462
json singh <20891087+jashanbhullar@users.noreply.github.com>
2024-06-10 12:40:46 +0530

    - index should reroute to workspace - workspace will handle the rest

--- ui/src/features/LoadingPage/index.tsx
+++ ui/src/features/LoadingPage/index.tsx
@@ -1,37 +1,12 @@
 import { useNavigate } from "@tanstack/react-router";
-import { useEffect, useState } from "react";
 
 import { Loading } from "@flow/components";
-import { useWorkspace } from "@flow/lib/gql";
 
 const LoadingPage: React.FC = () => {
-  const { getWorkspaces: getWorkspace } = useWorkspace();
   const navigate = useNavigate();
-  const [error, setError] = useState<string | undefined>(undefined);
+  navigate({ to: `/workspace/` });
 
-  const { workspaces, isLoading } = getWorkspace();
-
-  useEffect(() => {
-    if (isLoading) return;
-    else if (!workspaces) {
-      setError("Unable to fetch workspaces");
-      return;
-    }
-    const defaultWorkspace = workspaces.find(w => w.personal);
-
-    if (!defaultWorkspace) {
-      setError("No personal id workspace found...");
-      return;
-    }
-    navigate({ to: `/workspace/${defaultWorkspace.id}` });
-  }, [workspaces, setError, navigate, isLoading]);
-
-  if (isLoading) {
-    return <Loading show={isLoading} />;
-  }
-
-  // TODO: Show proper error
-  return error && <div>{error}</div>;
+  return <Loading />;
 };
 
 export { LoadingPage };
