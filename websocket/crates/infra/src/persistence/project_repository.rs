f9755be498ec3adb39361e8f792e71c41add0cb7
Akihito Kirisaki <kirisaki@klara.works>
2024-06-25 14:46:06 +0900

    feat(websocket): Support snapshot for GCS

--- websocket/crates/infra/src/persistence/project_repository.rs
+++ websocket/crates/infra/src/persistence/project_repository.rs
@@ -1,14 +1,16 @@
 use async_trait::async_trait;
 
-use crate::persistence::redis::redis_client::RedisClient;
+use crate::persistence::gcs::gcs_client::GcsClient;
 use crate::persistence::redis::flow_project_lock::FlowProjectLock;
-use std::sync::Arc;
+use crate::persistence::redis::redis_client::RedisClient;
 use std::error::Error;
+use std::sync::Arc;
 
 use flow_websocket_domain::project::{Project, ProjectEditingSession};
-use flow_websocket_domain::repository::{ProjectRepository, ProjectEditingSessionRepository, ProjectSnapshotRepository};
+use flow_websocket_domain::repository::{
+    ProjectEditingSessionRepository, ProjectRepository, ProjectSnapshotRepository,
+};
 use flow_websocket_domain::snapshot::ProjectSnapshot;
-
 
 pub struct ProjectRedisRepository {
     redis_client: Arc<RedisClient>,
@@ -18,7 +20,10 @@ pub struct ProjectRedisRepository {
 impl ProjectRedisRepository {
     pub fn new(redis_client: Arc<RedisClient>) -> Self {
         let global_lock = FlowProjectLock::new(redis_client.connection());
-        Self { redis_client, global_lock }
+        Self {
+            redis_client,
+            global_lock,
+        }
     }
 }
 
@@ -38,7 +43,10 @@ impl ProjectEditingSessionRepository for ProjectRedisRepository {
         Ok(())
     }
 
-    async fn get_active_session(&self, project_id: &str) -> Result<Option<ProjectEditingSession>, Box<dyn Error>> {
+    async fn get_active_session(
+        &self,
+        project_id: &str,
+    ) -> Result<Option<ProjectEditingSession>, Box<dyn Error>> {
         let key = format!("project:{}:active_session", project_id);
         self.redis_client.get(&key).await
     }
@@ -58,7 +66,10 @@ impl ProjectSnapshotRepository for ProjectRedisRepository {
         Ok(())
     }
 
-    async fn get_latest_snapshot(&self, project_id: &str) -> Result<Option<ProjectSnapshot>, Box<dyn Error>> {
+    async fn get_latest_snapshot(
+        &self,
+        project_id: &str,
+    ) -> Result<Option<ProjectSnapshot>, Box<dyn Error>> {
         let key = format!("project:{}:latest_snapshot", project_id);
         self.redis_client.get(&key).await
     }
@@ -66,5 +77,42 @@ impl ProjectSnapshotRepository for ProjectRedisRepository {
     async fn get_latest_snapshot_state(&self, project_id: &str) -> Result<Vec<u8>, Box<dyn Error>> {
         let key = format!("project:{}:latest_snapshot_state", project_id);
         self.redis_client.get(&key).await
+    }
+}
+
+/// A `ProjectGcsRepository` is a thin wrapper of `GcsClient`.
+pub struct ProjectGcsRepository {
+    client: GcsClient,
+}
+
+impl ProjectGcsRepository {
+    /// Returns the `ProjectGcsRepository`.
+    fn new(client: GcsClient) -> Self {
+        Self { client }
+    }
+}
+
+#[async_trait]
+impl ProjectSnapshotRepository for ProjectGcsRepository {
+    /// Create a snapshot.
+    async fn create_snapshot(&self, snapshot: ProjectSnapshot) -> Result<(), Box<dyn Error>> {
+        let path = format!("snapshot/{}", snapshot.id);
+        self.client.upload(path, &snapshot);
+        Ok(())
+    }
+
+    /// Get the latest snapshot.
+    async fn get_latest_snapshot(
+        &self,
+        project_id: &str,
+    ) -> Result<Option<ProjectSnapshot>, Box<dyn Error>> {
+        let path = format!("snapshot/{}:latest_snapshot", project_id);
+        self.client.download(path).await
+    }
+
+    /// Get the state of the latest snapshot.
+    async fn get_latest_snapshot_state(&self, project_id: &str) -> Result<Vec<u8>, Box<dyn Error>> {
+        let path = format!("snapshot/{}:latest_snapshot_state", project_id);
+        self.client.download(path).await
     }
 }
