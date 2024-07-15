9ee251bd59021755dcaa0e722c203d408c9bbec5
Dongri Jin <dongrium@gmail.com>
2024-07-15 10:21:49 +0900

    WIP

--- worker/crates/action-processor/src/feature/filter.rs
+++ worker/crates/action-processor/src/feature/filter.rs
@@ -116,6 +116,9 @@ impl Processor for FeatureFilter {
         ctx: ExecutorContext,
         fw: &mut dyn ProcessorChannelForwarder,
     ) -> Result<(), BoxedError> {
+
+        println!("==================feature======================");
+        
         let expr_engine = Arc::clone(&ctx.expr_engine);
         let feature = &ctx.feature;
         let mut routing = false;
