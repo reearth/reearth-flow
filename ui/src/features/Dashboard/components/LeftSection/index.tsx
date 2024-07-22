aab3532f98336a989ee43480b5c98f52dcfdbfb0
json singh <20891087+jashanbhullar@users.noreply.github.com>
2024-07-22 18:25:23 +0530

    replaced bg-zinc with bg-background

--- ui/src/features/Dashboard/components/LeftSection/index.tsx
+++ ui/src/features/Dashboard/components/LeftSection/index.tsx
@@ -2,7 +2,7 @@ import { RunsSection, WorkspaceSection } from "./components";
 
 const LeftSection: React.FC = () => {
   return (
-    <div className="flex w-[250px] flex-col justify-between gap-[8px] border-r border-zinc-700 bg-zinc-900/50">
+    <div className="flex w-[250px] flex-col justify-between gap-[8px] border-r border-zinc-700 bg-background-900/50">
       <div className="flex flex-col gap-8">
         <RunsSection />
         <WorkspaceSection />
