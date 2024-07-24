6b437dc766f21dea80cd8f90db913e2eff66d83d
json singh <20891087+jashanbhullar@users.noreply.github.com>
2024-07-24 20:47:11 +0530

    - chill scenes

--- ui/src/features/Editor/components/OverlayUI/components/Breadcrumb/index.tsx
+++ ui/src/features/Editor/components/OverlayUI/components/Breadcrumb/index.tsx
@@ -21,15 +21,15 @@ const Breadcrumb: React.FC = () => {
         className="flex cursor-default select-none items-center gap-3 rounded-br-md px-2 py-1"
         onMouseLeave={() => setIsHovered(undefined)}>
         <p
-          className={`max-w-[100px] truncate font-extralight text-zinc-400 transition-all delay-0 duration-500 ${isHovered?.includes("workspace") ? "max-w-[50vw] select-text delay-500" : undefined}`}
+          className={`max-w-[100px] truncate font-extralight transition-all delay-0 duration-500 ${isHovered?.includes("workspace") ? "max-w-[50vw] select-text delay-500" : undefined}`}
           onMouseEnter={() => setIsHovered(h => [...(h ?? []), "workspace"])}>
           {currentWorkspace?.name}
         </p>
-        <p className="font-extralight text-zinc-500">
+        <p className="font-extralight">
           <CaretRight />
         </p>
         <p
-          className={`max-w-[100px] truncate font-extralight text-zinc-400 transition-all delay-0 duration-500 ${isHovered?.includes("project") ? "max-w-[50vw] select-text delay-500" : undefined}`}
+          className={`max-w-[100px] truncate font-extralight  transition-all delay-0 duration-500 ${isHovered?.includes("project") ? "max-w-[50vw] select-text delay-500" : undefined}`}
           onMouseEnter={() => setIsHovered(h => [...(h ?? []), "project"])}>
           {currentProject?.name}
         </p>
