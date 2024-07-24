5038bd4951f39b89e83183da4af1bd25d6bc6275
json singh <20891087+jashanbhullar@users.noreply.github.com>
2024-07-24 13:08:51 +0530

    - hope it works

--- ui/src/components/Tabs/index.tsx
+++ ui/src/components/Tabs/index.tsx
@@ -14,7 +14,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-9 items-center justify-center rounded p-1 text-zinc-400",
+      "inline-flex h-9 items-center justify-center rounded p-1 text-accent/70",
       className,
     )}
     {...props}
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-light ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background-700 data-[state=active]:text-zinc-300 data-[state=active]:shadow hover:bg-background-700/50",
+      "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-light ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow hover:bg-secondary/50",
       className,
     )}
     {...props}
