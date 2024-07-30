a07101181020f97af284e17ac6565376d8ac655c
Jashanpreet Singh (json singh) <20891087+jashanbhullar@users.noreply.github.com>
2024-07-30 08:06:09 +0530

    feat(ui): Update Me Implementation (#381)
    * - yarn i18n
    
    * Update me functionality
    - added graphql query
    - added implementation
    - updated account dialog component
    
    * - ja translation update
    - minor changes

--- ui/src/features/Dialog/components/settings/AccountDialog.tsx
+++ ui/src/features/Dialog/components/settings/AccountDialog.tsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
+
+import { Button, Input, Label } from "@flow/components";
 import { useUser } from "@flow/lib/gql";
 import { useT } from "@flow/lib/i18n";
 
 import { ContentHeader, ContentSection } from "..";
 
-import { FieldWrapper } from "./components";
-
 const AccountDialogContent: React.FC = () => {
   const t = useT();
-  const { useGetMe } = useUser();
-  const { me } = useGetMe();
+  const { useGetMe, updateMe } = useUser();
+  const { me, isLoading } = useGetMe();
+  const [userName, setUserName] = useState<string | undefined>(me?.name);
+  const [email, setEmail] = useState<string | undefined>(me?.email);
+  const [showError, setShowError] = useState<boolean>(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleUpdateMe = async () => {
+    setLoading(true);
+    setShowError(false);
+    if (!userName || !email) {
+      setLoading(false);
+      return;
+    }
+    const { me } = await updateMe({ name: userName, email });
+    setLoading(false);
+    if (!me) {
+      setShowError(true);
+      return;
+    }
+  };
+
   return (
     <>
       <ContentHeader title={t("Account settings")} />
@@ -17,18 +38,38 @@ const AccountDialogContent: React.FC = () => {
           title={t("Basic information")}
           content={
             <div className="mt-2 flex flex-col gap-6">
-              <FieldWrapper>
-                <div>
-                  <p>{t("Name")}</p>
-                  <p className="text-xs">{me?.name}</p>
-                </div>
-              </FieldWrapper>
-              <FieldWrapper>
-                <div>
-                  <p>{t("Email address")}</p>
-                  <p className="text-xs">{me?.email}</p>
-                </div>
-              </FieldWrapper>
+              <div>
+                <Label htmlFor="user-name">{t("User Name")}</Label>
+                <Input
+                  id="user-name"
+                  placeholder={t("User Name")}
+                  disabled={isLoading}
+                  value={userName}
+                  onChange={e => setUserName(e.target.value)}
+                />
+              </div>
+              <div>
+                <Label htmlFor="user-email">{t("Email")}</Label>
+                <Input
+                  id="user-email"
+                  placeholder={t("User Name")}
+                  disabled={isLoading}
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                />
+              </div>
+              <div>
+                <Button
+                  className="self-end"
+                  disabled={isLoading || loading || !userName || !email}
+                  onClick={handleUpdateMe}>
+                  {t("Save")}
+                </Button>
+              </div>
+              <div
+                className={`self-end text-xs text-destructive ${showError ? "opacity-70" : "opacity-0"}`}>
+                {showError && t("Failed to update Account details")}
+              </div>
             </div>
           }
         />
