f52187f29bf810a50c7468b8bbd4c51e14266fc1
json singh <20891087+jashanbhullar@users.noreply.github.com>
2024-07-30 16:27:46 +0530

    - change names of processor, sink and source to transformer, Writer and Reader respectively

--- ui/src/lib/fetch/actions/useFetch.ts
+++ ui/src/lib/fetch/actions/useFetch.ts
@@ -15,7 +15,52 @@ enum ActionFetchKeys {
 const BASE_URL = config().api;
 
 export const useFetch = () => {
-  const fetcher = async <T>(url: string, signal: AbortSignal): Promise<T> => {
+  const transformResponse = <T extends Action | Action[] | Segregated>(response: T): T => {
+    const CHANGE_NAMES: {
+      [key: string]: string;
+    } = {
+      processor: "Transformer",
+      sink: "Writer",
+      source: "Reader",
+    };
+
+    if (Array.isArray(response)) {
+      return response.map(a => transformAction(a)) as T;
+    } else if (typeof response?.name === "string") {
+      return transformAction(response as Action) as T;
+    }
+
+    // This is because TS doesn't have a way to differentiate between either A or B when writing A | B
+    // Details: https://stackoverflow.com/questions/46370222/why-does-a-b-allow-a-combination-of-both-and-how-can-i-prevent-it
+    const segregated: Segregated = response as Segregated;
+    return Object.keys(segregated).reduce((obj, rootKey) => {
+      obj[rootKey] = Object.keys(segregated[rootKey]).reduce(
+        (obj: { [key: string]: Action[] | undefined }, key) => {
+          const actions = segregated[rootKey][key]?.map(a => transformAction(a));
+          if (CHANGE_NAMES[key]) {
+            obj[CHANGE_NAMES[key]] = actions;
+          } else {
+            obj[key] = actions;
+          }
+          return obj;
+        },
+        {},
+      );
+      return obj;
+    }, {} as Segregated) as T;
+
+    function transformAction(action: Action) {
+      return {
+        ...action,
+        type: CHANGE_NAMES[action.type] ? CHANGE_NAMES[action.type] : action.type,
+      };
+    }
+  };
+
+  const fetcher = async <T extends Action[] | Segregated | Action>(
+    url: string,
+    signal: AbortSignal,
+  ): Promise<T> => {
     const response = await fetch(url, { signal });
 
     if (!response.ok) {
@@ -25,8 +70,8 @@ export const useFetch = () => {
     if (status != 200) {
       throw new Error(`status not 200. received ${status}`);
     }
-    const data = response.json();
-    return data;
+    const data = await response.json();
+    return transformResponse(data);
   };
 
   const useGetActionsFetch = () =>
