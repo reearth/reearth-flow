a022b1164bd3baef422a66a20708ab58ae108f86
json singh <20891087+jashanbhullar@users.noreply.github.com>
2024-08-02 15:03:25 +0530

    - tailwind only need to check in /src

--- ui/tailwind.config.ts
+++ ui/tailwind.config.ts
@@ -3,12 +3,7 @@ import type { Config } from "tailwindcss";
 const config = {
   darkMode: ["class"],
   important: true,
-  content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-  ],
+  content: ["./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
     container: {
