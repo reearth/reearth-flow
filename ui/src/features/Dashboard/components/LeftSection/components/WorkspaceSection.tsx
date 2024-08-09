4543a3982357b16de57346624bf31d6a10370cb1
KaWaite <kyle.waite@outlook.com>
2024-08-09 15:44:56 +0900

    fix workspace settings link styles

--- ui/src/features/Dashboard/components/LeftSection/components/WorkspaceSection.tsx
+++ ui/src/features/Dashboard/components/LeftSection/components/WorkspaceSection.tsx
@@ -8,23 +8,23 @@ const WorkspaceSection: React.FC = () => {
   const navigate = useNavigate();
   return (
     <div className="flex flex-1 items-end p-4">
-      <div className="flex flex-col gap-1">
+      <div className="flex w-full flex-col gap-1">
         <div
-          className="flex flex-1 cursor-pointer items-center gap-2 rounded py-1 hover:bg-accent"
+          className="-mx-2 flex flex-1 cursor-pointer items-center gap-2 rounded px-2 py-1 hover:bg-accent"
           onClick={() => navigate({ to: `settings/general` })}
         >
           <Toolbox weight="thin" />
           <p className="text-sm font-extralight">{t("General Settings")}</p>
         </div>
         <div
-          className="flex flex-1 cursor-pointer items-center gap-2 rounded py-1 hover:bg-accent"
+          className="-mx-2 flex flex-1 cursor-pointer items-center gap-2 rounded px-2 py-1 hover:bg-accent"
           onClick={() => navigate({ to: `settings/members` })}
         >
           <UsersThree weight="thin" />
           <p className="text-sm font-extralight">{t("Member Settings")}</p>
         </div>
         <div
-          className="flex flex-1 cursor-pointer items-center gap-2 rounded py-1 hover:bg-accent"
+          className="-mx-2 flex flex-1 cursor-pointer items-center gap-2 rounded px-2 py-1 hover:bg-accent"
           onClick={() => navigate({ to: `settings/integrations` })}
         >
           <PlugsConnected weight="thin" />
