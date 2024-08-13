cf9a6150464ba971bc4a65c0d6065619777d284e
Yuji Mise <miseyu2000@gmail.com>
2024-08-13 11:54:57 +0900

    feat(worker): Update attribute module structure

--- worker/crates/action-processor/src/attribute.rs
+++ worker/crates/action-processor/src/attribute.rs
@@ -2,7 +2,7 @@ pub mod aggregator;
 pub mod duplicate_filter;
 pub mod errors;
 pub mod file_path_info_extractor;
-pub mod keeper;
 pub mod manager;
+pub mod mapper;
 pub mod mapping;
 pub mod statistics_calculator;
