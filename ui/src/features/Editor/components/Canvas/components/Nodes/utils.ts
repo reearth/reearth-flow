05412da48c9890ffbb0e9590d1fc2aeea87a569a
json singh <20891087+jashanbhullar@users.noreply.github.com>
2024-08-28 17:27:43 +0530

    - notification color fixes

--- ui/src/features/Editor/components/Canvas/components/Nodes/utils.ts
+++ ui/src/features/Editor/components/Canvas/components/Nodes/utils.ts
@@ -3,9 +3,9 @@ import { Status } from "@flow/types";
 export const getPropsFrom = (status?: Status) => {
   const style =
     status === "success"
-      ? "bg-green-500"
+      ? "bg-success"
       : status === "failure"
-        ? "bg-red-500"
+        ? "bg-destructive"
         : status === "active"
           ? "active-node-status"
           : "bg-primary";
