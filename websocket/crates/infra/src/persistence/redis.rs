8f774152664e2bdb17e539009e0e59b180819eb5
Akihito Kirisaki <kirisaki@klara.works>
2024-09-12 16:07:27 +0900

    refactor(websocket): rename modules to modern style

---
+++ websocket/crates/infra/src/persistence/redis.rs
@@ -0,0 +1,3 @@
+pub mod flow_project_lock;
+pub mod flow_project_redis_data_manager;
+pub mod redis_client;
--- websocket/crates/infra/src/persistence/redis/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod redis_client;
-pub mod flow_project_lock;
-pub mod flow_project_redis_data_manager;
