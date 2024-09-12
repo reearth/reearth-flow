89be431a1dca881b8980a4ed3c947edd02f5bc54
Yuji Mise <miseyu2000@gmail.com>
2024-09-12 10:40:07 +0900

    chore(worker): Refactor action-sink module to use EchoSink and NoopSink

--- worker/crates/action-processor/src/lib.rs
+++ worker/crates/action-processor/src/lib.rs
@@ -1,8 +1,10 @@
 pub mod attribute;
+pub mod echo;
 pub mod feature;
 pub mod file;
 pub mod geometry;
 pub mod mapping;
+pub mod noop;
 pub mod xml;
 
 #[cfg(test)]
--- worker/crates/action-processor/src/mapping.rs
+++ worker/crates/action-processor/src/mapping.rs
@@ -1,12 +1,24 @@
 use std::collections::HashMap;
 
 use once_cell::sync::Lazy;
-use reearth_flow_runtime::node::NodeKind;
+use reearth_flow_runtime::node::{NodeKind, ProcessorFactory};
 
-use crate::{attribute, feature, file, geometry, xml};
+use crate::{
+    attribute, echo::EchoProcessorFactory, feature, file, geometry, noop::NoopProcessorFactory, xml,
+};
 
 pub static ACTION_FACTORY_MAPPINGS: Lazy<HashMap<String, NodeKind>> = Lazy::new(|| {
     let mut mapping = HashMap::new();
+    let factories: Vec<Box<dyn ProcessorFactory>> = vec![
+        Box::<EchoProcessorFactory>::default(),
+        Box::<NoopProcessorFactory>::default(),
+    ];
+    mapping.extend(
+        factories
+            .into_iter()
+            .map(|f| (f.name().to_string(), NodeKind::Processor(f)))
+            .collect::<HashMap<_, _>>(),
+    );
     mapping.extend(attribute::mapping::ACTION_FACTORY_MAPPINGS.clone());
     mapping.extend(feature::mapping::ACTION_FACTORY_MAPPINGS.clone());
     mapping.extend(geometry::mapping::ACTION_FACTORY_MAPPINGS.clone());
