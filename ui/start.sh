ac4b835242f910abf8aae2950cf567fdd75f3849
pyshx <piyushchauhan1004@gmail.com>
2024-09-18 09:40:53 +0530

    rename flow config to reearth config

--- ui/start.sh
+++ ui/start.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-# Generate the flow_config.json from the template
-envsubst < /usr/share/nginx/html/flow_config.template.json > /usr/share/nginx/html/flow_config.json
+# Signal handling
+trap 'echo "Shutting down..."; nginx -s quit; exit 0' SIGTERM SIGINT
+
+# Generate the reearth_config.json from the template
+envsubst < /usr/share/nginx/html/reearth_config.template.json > /usr/share/nginx/html/reearth_config.json
 
 # Start nginx
-nginx -g 'daemon off;'
+nginx -g 'daemon off;' &
+
+# Wait for any process to exit
+wait -n
+
+# Exit with status of process that exited first
+exit $?
