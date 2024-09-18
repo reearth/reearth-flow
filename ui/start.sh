e1d263f7a53a894e07e4700661fc8c9a522cc37b
pyshx <piyushchauhan1004@gmail.com>
2024-09-18 09:51:20 +0530

    ui: lint and better error on start.sh

--- ui/start.sh
+++ ui/start.sh
@@ -5,6 +5,10 @@ trap 'echo "Shutting down..."; nginx -s quit; exit 0' SIGTERM SIGINT
 
 # Generate the reearth_config.json from the template
 envsubst < /usr/share/nginx/html/reearth_config.template.json > /usr/share/nginx/html/reearth_config.json
+if [ $? -ne 0 ]; then
+    echo "Error generating flow_config.json"
+    exit 1
+fi
 
 # Start nginx
 nginx -g 'daemon off;' &
