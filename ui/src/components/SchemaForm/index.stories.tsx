4cac63711459ba5f9d49e56422d877b90189eb68
jashan <20891087+jashanbhullar@users.noreply.github.com>
2024-09-24 20:14:50 +0530

    - updated storybook for better DX

--- ui/src/components/SchemaForm/index.stories.tsx
+++ ui/src/components/SchemaForm/index.stories.tsx
@@ -1,5 +1,19 @@
+import {
+  ChevronDownIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from "@radix-ui/react-icons";
 import { RJSFSchema } from "@rjsf/utils";
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta } from "@storybook/react";
+import { useEffect, useMemo, useState } from "react";
+
+import { Button } from "../buttons";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../DropdownMenu";
 
 import { SchemaForm } from ".";
 
@@ -13,50 +27,119 @@ const meta = {
 } satisfies Meta<typeof SchemaForm>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
 
 const commonArgs: {
   schema: RJSFSchema;
 } = {
   schema: {
-    title: "A registration form",
-    description: "A simple form example.",
-    type: "object",
-    required: ["firstName", "lastName"],
-    properties: {
-      firstName: {
+    $schema: "http://json-schema.org/draft-07/schema#",
+    definitions: {
+      Attribute: {
         type: "string",
-        title: "First name",
-        default: "Chuck",
-      },
-      lastName: {
-        type: "string",
-        title: "Last name",
-      },
-      age: {
-        type: "integer",
-        title: "Age",
-      },
-      bio: {
-        type: "string",
-        title: "Bio",
-      },
-      password: {
-        type: "string",
-        title: "Password",
-        minLength: 3,
-      },
-      telephone: {
-        type: "string",
-        title: "Telephone",
-        minLength: 10,
       },
     },
+    properties: {
+      groupBy: {
+        items: {
+          $ref: "#/definitions/Attribute",
+        },
+        type: ["array", "null"],
+      },
+      outputAttribute: {
+        $ref: "#/definitions/Attribute",
+      },
+    },
+    required: ["outputAttribute"],
+    title: "AreaOnAreaOverlayerParam",
+    type: "object",
   },
 };
 
-export const Default: Story = {
-  args: {
-    ...commonArgs,
-  },
+const fetcher = async (url: string) => {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error("response not ok");
+  }
+  const status = response.status;
+  if (status != 200) {
+    throw new Error(`status not 200. received ${status}`);
+  }
+  return await response.json();
+};
+
+export const Default = () => {
+  const [actions, setActions] = useState<any[]>([]);
+  const [selectedAction, setSelectedAction] = useState();
+  const [schema, setSchema] = useState<RJSFSchema>(commonArgs.schema);
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetcher("http://localhost:8080/actions");
+      setActions(data);
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetcher(
+        `http://localhost:8080/actions/${selectedAction}`,
+      );
+      setSchema(data.parameter);
+    })();
+  }, [selectedAction, setSchema]);
+
+  const selectedIndex = useMemo(
+    () => actions.findIndex((el: any) => el.name === selectedAction),
+    [actions, selectedAction],
+  );
+
+  console.log(selectedIndex);
+
+  const selectNext = () => console.log();
+
+  return (
+    <div className="flex w-[80vw] flex-col gap-2">
+      <div className="flex items-center justify-between rounded border p-2">
+        <div>Action Name: {selectedAction ? selectedAction : "Default"}</div>
+        <div className="flex w-1/3 items-center justify-between gap-2">
+          <Button
+            size="sm"
+            variant="outline"
+            disabled={selectedIndex <= 0}
+            onClick={() => setSelectedAction(actions[selectedIndex - 1].name)}>
+            <ChevronLeftIcon />
+          </Button>
+          <Button
+            size="sm"
+            variant="outline"
+            disabled={selectedIndex >= actions.length}
+            onClick={() => setSelectedAction(actions[selectedIndex + 1].name)}>
+            <ChevronRightIcon />
+          </Button>
+          <DropdownMenu modal={true}>
+            <DropdownMenuTrigger className="flex h-8 items-center rounded border bg-background px-1 hover:bg-accent">
+              <p className="text-sm"> Select Action</p>
+              <ChevronDownIcon />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="h-96 overflow-auto" align="center">
+              {actions.map(({ name }) => (
+                <DropdownMenuItem
+                  onClick={() => setSelectedAction(name)}
+                  key={name}>
+                  {name}
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <Button variant="outline" disabled size="sm" className="text-sm">
+            Enter Custom Schema
+          </Button>
+        </div>
+      </div>
+      <div className="rounded border p-2">
+        <SchemaForm schema={schema} />
+      </div>
+    </div>
+  );
 };
