aa3a3ec5fcccb983fb68e1fa9c98fc76ef064b39
pyshx <piyushchauhan1004@gmail.com>
2024-09-27 20:10:43 +0530

    fix: ui docker nginx config

---
+++ ui/docker/40-envsubst-on-reearth_config.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -e
+
+ME=$(basename "$0")
+
+entrypoint_log() {
+    if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
+        echo "$@"
+    fi
+}
+
+auto_envsubst() {
+    local template_file="/tmp/reearth_config.json.tmpl"
+    local output_file="/opt/reearth-flow/reearth_config.json"
+    local filter="${NGINX_ENVSUBST_FILTER:-}"
+    local defined_envs
+
+    if [ ! -f "$template_file" ]; then
+        entrypoint_log "$ME: ERROR: template file $template_file does not exist"
+        return 1
+    fi
+
+    if [ ! -w "$(dirname "$output_file")" ]; then
+        entrypoint_log "$ME: ERROR: $(dirname "$output_file") is not writable"
+        return 1
+    fi
+
+    defined_envs=$(printf '${%s} ' $(env | cut -d= -f1 | awk "/${filter}/ {print}"))
+
+    entrypoint_log "$ME: Running envsubst on $template_file to $output_file"
+    envsubst "$defined_envs" < "$template_file" > "$output_file"
+
+    if [ $? -ne 0 ]; then
+        entrypoint_log "$ME: ERROR: envsubst failed"
+        return 1
+    fi
+
+    entrypoint_log "$ME: Successfully generated $output_file"
+}
+
+auto_envsubst
+exit 0
