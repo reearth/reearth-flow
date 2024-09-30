fc05c367989b44f596e632cc83cdf75c52427200
Yuji Mise <miseyu2000@gmail.com>
2024-09-30 12:51:19 +0900

    feat(engine): improve logging functionality and add slow action logger (#540)
    * feat(engine): improve logging functionality and add slow action logger
    
    * fix

--- engine/plateau-gis-quality-checker/src/components/Loading/index.tsx
+++ engine/plateau-gis-quality-checker/src/components/Loading/index.tsx
@@ -1,7 +1,5 @@
 import { DoubleArrowRightIcon } from "@radix-ui/react-icons";
 
-import { FlowLogo } from "../FlowLogo";
-
 import "./styles.css";
 
 type Props = {
@@ -16,11 +14,6 @@ const Loading: React.FC<Props> = ({ className, show }) => {
         <div className="flex h-full items-center">
           <div className="flex flex-col items-center gap-1">
             <div className="flex gap-3">
-              <FlowLogo
-                id="loading-svg"
-                className="mb-8 rounded-lg bg-logo p-1"
-                style={{ height: "110px", width: "110px" }}
-              />
               <DoubleArrowRightIcon className="size-[110px]" />
             </div>
             <p className="text-xl font-light">実行中...</p>
--- engine/plateau-gis-quality-checker/src/components/index.ts
+++ engine/plateau-gis-quality-checker/src/components/index.ts
@@ -1,5 +1,4 @@
 export * from "./Button";
-export * from "./FlowLogo";
 export * from "./Input";
 export * from "./Label";
 export * from "./Progress";
