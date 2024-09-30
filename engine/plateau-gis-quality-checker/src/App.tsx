63f0759e6eb678a93a7d972c845c4e23325e37a6
Yuji Mise <miseyu2000@gmail.com>
2024-09-30 09:56:11 +0900

    chore(engine): update quality check workflows and adjust UI layout

--- engine/plateau-gis-quality-checker/src/App.tsx
+++ engine/plateau-gis-quality-checker/src/App.tsx
@@ -78,7 +78,7 @@ function App() {
   return (
     <div className="dark relative h-screen bg-card text-zinc-300">
       <div className="relative flex h-[53px] items-center justify-between border-b bg-secondary px-4 py-2">
-        <FlowLogo className="" />
+        <div />
         <p className="absolute left-1/2 mx-auto -translate-x-1/2 text-xl font-thin text-white">
           PLATEAU 品質検査ツール
         </p>
