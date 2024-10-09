3b31e8ab2e0265df2048a630337026b0cdc73bbc
KaWaite <34051327+KaWaite@users.noreply.github.com>
2024-10-09 16:03:10 +0900

    new logo + a little ui tweaks (#562)

--- ui/src/components/Loading/index.tsx
+++ ui/src/components/Loading/index.tsx
@@ -1,5 +1,3 @@
-import { DoubleArrowRightIcon } from "@radix-ui/react-icons";
-
 import { FlowLogo } from "..";
 
 import "./styles.css";
@@ -9,14 +7,12 @@ const Loading: React.FC<{ show?: boolean }> = () => {
     <div className="absolute left-0 top-0 z-40 flex h-screen w-full justify-center bg-secondary">
       <div className="flex h-full items-center">
         <div className="flex flex-col gap-5">
-          <div className="flex gap-3">
+          <div className="flex flex-col items-center gap-8">
             <FlowLogo
               id="loading-svg"
-              // TODO: Fix the specific red color
-              className="mb-8 rounded-lg bg-red-900/50 p-1"
               style={{ height: "110px", width: "110px" }}
             />
-            <DoubleArrowRightIcon className="size-[110px]" />
+            <p className="text-2xl font-thin">Re:Earth Flow</p>
           </div>
         </div>
       </div>
--- ui/src/features/NotFound/index.tsx
+++ ui/src/features/NotFound/index.tsx
@@ -12,19 +12,19 @@ const NotFound: React.FC<Props> = ({ message }) => {
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-10">
-        <div className="flex items-center gap-4">
-          <div className="rounded bg-logo p-2">
+      <div className="flex flex-col items-center gap-14">
+        <div className="flex items-center gap-10">
+          <div className="p-2">
             <FlowLogo className="size-[75px]" />
           </div>
           <p className="text-4xl dark:font-extralight">{t("Not Found")}</p>
         </div>
         {message && (
-          <p className="dark:font-extralight text-red-500">{message}</p>
+          <p className="text-red-500 dark:font-extralight">{message}</p>
         )}
         <Link to={"/"}>
           <Button variant="outline">
-            <p className="dark:font-extralight italic">{t("Go to Home")}</p>
+            <p className="italic dark:font-extralight">{t("Go to Home")}</p>
           </Button>
         </Link>
       </div>
