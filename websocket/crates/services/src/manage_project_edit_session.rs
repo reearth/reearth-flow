b2dab88bc3447f45472e5b3151afcfc3c829debc
xy <xyzmhx@gmail.com>
2024-10-10 17:58:21 +0900

    Refactor ManageEditSessionService

--- websocket/crates/services/src/manage_project_edit_session.rs
+++ websocket/crates/services/src/manage_project_edit_session.rs
@@ -1,22 +1,22 @@
-use chrono::Utc;
-use flow_websocket_domain::generate_id;
-use flow_websocket_domain::project::ProjectEditingSession;
-use flow_websocket_domain::repository::{
-    ProjectEditingSessionRepository, ProjectSnapshotRepository, RedisDataManager,
-    SnapshotDataRepository,
+use chrono::{DateTime, Utc};
+use flow_websocket_domain::{
+    generate_id,
+    project::ProjectEditingSession,
+    repository::{
+        ProjectEditingSessionRepository, ProjectSnapshotRepository, RedisDataManager,
+        SnapshotDataRepository,
+    },
+    snapshot::{Metadata, ObjectDelete, ObjectTenant, SnapshotInfo},
+    types::{data::SnapshotData, snapshot::ProjectSnapshot},
 };
-use flow_websocket_domain::snapshot::{Metadata, ObjectDelete, ObjectTenant, SnapshotInfo};
-use flow_websocket_domain::types::data::SnapshotData;
-use flow_websocket_domain::types::snapshot::ProjectSnapshot;
 use mockall::automock;
 use std::sync::Arc;
-use tokio::time::sleep;
+use tokio::time::{sleep, Duration};
 
-use crate::types::ManageProjectEditSessionTaskData;
-use crate::ProjectServiceError;
+use crate::{types::ManageProjectEditSessionTaskData, ProjectServiceError};
 
-const MAX_EMPTY_SESSION_DURATION: i64 = 10 * 1000; // 10 seconds
-const MAX_SNAPSHOT_DELTA: i64 = 5 * 60 * 1000; // 5 minutes
+const MAX_EMPTY_SESSION_DURATION: i64 = 10_000; // 10 seconds
+const MAX_SNAPSHOT_DELTA: i64 = 300_000; // 5 minutes
 
 pub struct ManageEditSessionService<R, S, D, M>
 where
@@ -57,12 +57,11 @@ where
         &self,
         mut data: ManageProjectEditSessionTaskData,
     ) -> Result<(), ProjectServiceError> {
-        let session = self
+        if let Some(mut session) = self
             .session_repository
             .get_active_session(&data.project_id)
-            .await?;
-
-        if let Some(mut session) = session {
+            .await?
+        {
             session
                 .load_session(&*self.snapshot_repository, &data.session_id)
                 .await?;
@@ -95,9 +94,7 @@ where
             && data.clients_disconnected_at.is_none()
         {
             data.clients_disconnected_at = Some(Utc::now());
-        }
-
-        if current_client_count > 0 {
+        } else if current_client_count > 0 {
             data.clients_disconnected_at = None;
         }
 
@@ -123,45 +120,52 @@ where
     ) -> Result<(), ProjectServiceError> {
         if let Some(last_snapshot_at) = data.last_snapshot_at {
             let current_time = Utc::now();
-            let snapshot_time_delta = (current_time - last_snapshot_at).num_milliseconds();
-            if snapshot_time_delta > MAX_SNAPSHOT_DELTA {
-                let state = session.get_state_update(&*self.redis_data_manager).await?;
-
-                let metadata = Metadata::new(
-                    generate_id(14, "snap"),
-                    session.project_id.clone(),
-                    session.session_id.clone(),
-                    String::new(),
-                    String::new(),
-                );
-
-                let snapshot_state = SnapshotInfo::new(
-                    None,   // created_by
-                    vec![], // changes_by
-                    ObjectTenant {
-                        id: session.tenant.id.clone(),
-                        key: session.tenant.key.clone(),
-                    },
-                    ObjectDelete {
-                        deleted: false,
-                        delete_after: None,
-                    },
-                    Some(current_time),
-                    Some(current_time),
-                );
-
-                let snapshot = ProjectSnapshot::new(metadata, snapshot_state);
-                let snapshot_data =
-                    SnapshotData::new(session.project_id.clone(), state, None, None);
-
-                self.snapshot_repository.create_snapshot(snapshot).await?;
-                self.snapshot_data_repository
-                    .create_snapshot_data(snapshot_data)
-                    .await?;
-
+            if (current_time - last_snapshot_at).num_milliseconds() > MAX_SNAPSHOT_DELTA {
+                self.create_snapshot(session, current_time).await?;
                 data.last_snapshot_at = Some(current_time);
             }
         }
+        Ok(())
+    }
+
+    async fn create_snapshot(
+        &self,
+        session: &mut ProjectEditingSession,
+        current_time: DateTime<Utc>,
+    ) -> Result<(), ProjectServiceError> {
+        let state = session.get_state_update(&*self.redis_data_manager).await?;
+
+        let metadata = Metadata::new(
+            generate_id(14, "snap"),
+            session.project_id.clone(),
+            session.session_id.clone(),
+            String::new(),
+            String::new(),
+        );
+
+        let snapshot_state = SnapshotInfo::new(
+            None,
+            vec![],
+            ObjectTenant {
+                id: session.tenant.id.clone(),
+                key: session.tenant.key.clone(),
+            },
+            ObjectDelete {
+                deleted: false,
+                delete_after: None,
+            },
+            Some(current_time),
+            Some(current_time),
+        );
+
+        let snapshot = ProjectSnapshot::new(metadata, snapshot_state);
+        let snapshot_data = SnapshotData::new(session.project_id.clone(), state, None, None);
+
+        self.snapshot_repository.create_snapshot(snapshot).await?;
+        self.snapshot_data_repository
+            .create_snapshot_data(snapshot_data)
+            .await?;
+
         Ok(())
     }
 
@@ -192,9 +196,8 @@ where
         session: &ProjectEditingSession,
         data: &ManageProjectEditSessionTaskData,
     ) -> Result<(), ProjectServiceError> {
-        let current_editing_session = session.active_editing_session().await?;
-        if current_editing_session.as_ref() == Some(&data.session_id) {
-            sleep(std::time::Duration::from_secs(5)).await;
+        if session.active_editing_session().await?.as_ref() == Some(&data.session_id) {
+            sleep(Duration::from_secs(5)).await;
         }
         Ok(())
     }
