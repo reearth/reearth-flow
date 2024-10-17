2eb764245c09ef5e578bccff2248bc8d93c4ec12
rot1024 <aayhrot@gmail.com>
2024-10-17 23:24:05 +0900

    update docker

--- ui/docker/40-envsubst-on-reearth_config.sh
+++ ui/docker/40-envsubst-on-reearth_config.sh
@@ -1,38 +1,8 @@
 #!/bin/sh
+
 set -e
 
-ME=$(basename "$0")
+TEMPLATE_FILE="/tmp/reearth_config.json.template"
+OUTPUT_FILE="/usr/share/nginx/html/reearth_config.json"
 
-entrypoint_log() {
-    if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
-        echo "$@"
-    fi
-}
-
-auto_envsubst() {
-    local template_file="/tmp/reearth_config.json.tmpl"
-    local output_file="/opt/reearth-flow/reearth_config.json"
-
-    if [ ! -f "$template_file" ]; then
-        entrypoint_log "$ME: ERROR: template file $template_file does not exist"
-        return 1
-    fi
-
-    if [ ! -w "$(dirname "$output_file")" ]; then
-        entrypoint_log "$ME: ERROR: $(dirname "$output_file") is not writable"
-        return 1
-    fi
-
-    entrypoint_log "$ME: Running envsubst on $template_file to $output_file"
-    envsubst < "$template_file" > "$output_file"
-
-    if [ $? -ne 0 ]; then
-        entrypoint_log "$ME: ERROR: envsubst failed"
-        return 1
-    fi
-
-    entrypoint_log "$ME: Successfully generated $output_file"
-}
-
-auto_envsubst
-exit 0
+envsubst < "$TEMPLATE_FILE" > "$OUTPUT_FILE"
