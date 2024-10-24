1959af766bed2edd51a5c94c57771213d5ec0633
KaWaite <kyle.waite@outlook.com>
2024-10-24 11:37:07 +0900

    regen yarn.lock and upgrade cesium

--- ui/src/routes/workspaces_.$workspaceId_.runs.$tab.lazy.tsx
+++ ui/src/routes/workspaces_.$workspaceId_.runs.$tab.lazy.tsx
@@ -1,12 +1,14 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
+import { createLazyFileRoute } from '@tanstack/react-router'
 
-import { WorkspaceIdWrapper } from "@flow/features/PageWrapper";
-import { Runs } from "@flow/features/Runs";
+import { WorkspaceIdWrapper } from '@flow/features/PageWrapper'
+import { Runs } from '@flow/features/Runs'
 
-export const Route = createLazyFileRoute("/workspaces/$workspaceId/runs/$tab")({
+export const Route = createLazyFileRoute(
+  '/workspaces_/$workspaceId_/runs/$tab',
+)({
   component: () => (
     <WorkspaceIdWrapper>
       <Runs />
     </WorkspaceIdWrapper>
   ),
-});
+})
