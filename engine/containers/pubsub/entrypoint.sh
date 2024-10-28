2c230a69c5286cc5a21c0a45abc006ef7b684118
Yuji Mise <miseyu2000@gmail.com>
2024-10-28 10:49:56 +0900

    refactor(engine): reorganize Dockerfiles and update paths for better structure (#596)

--- engine/containers/pubsub/entrypoint.sh
+++ engine/containers/pubsub/entrypoint.sh
@@ -8,12 +8,12 @@ export PUBSUB_EMULATOR_HOST=0.0.0.0:8085
 gcloud beta emulators pubsub start --project=$PUBSUB_PROJECT_ID --host-port=$PUBSUB_EMULATOR_HOST --quiet &
 
 while ! nc -z localhost 8085; do
-  sleep 0.1
+    sleep 0.1
 done
 
 . env/bin/activate
 # Create topics and subscriptions
-IFS=',' read -ra TOPICS <<< "$TOPIC_IDS"
+IFS=',' read -ra TOPICS <<<"$TOPIC_IDS"
 for TOPIC in "${TOPICS[@]}"; do
     # Create a topic
     python3 publisher.py $PUBSUB_PROJECT_ID create $TOPIC
