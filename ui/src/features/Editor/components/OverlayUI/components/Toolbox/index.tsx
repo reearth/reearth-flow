79715bc08bcc43b652da647ad1980f7a3c15b61f
popy01012 <49387730+popy01012@users.noreply.github.com>
2024-10-31 07:48:47 +0900

    Update index.tsx

--- ui/src/features/Editor/components/OverlayUI/components/Toolbox/index.tsx
+++ ui/src/features/Editor/components/OverlayUI/components/Toolbox/index.tsx
@@ -3,16 +3,19 @@ import {
   ArrowArcRight,
   Database,
   Disc,
+  DotsSix,
   Lightning,
   Note,
   RectangleDashed,
 } from "@phosphor-icons/react";
-import { memo, type DragEvent } from "react";
+import { memo, type DragEvent, useState } from "react";
 import { createRoot } from "react-dom/client";
 
 import { IconButton } from "@flow/components";
 import { useT } from "@flow/lib/i18n";
 import { type NodeType } from "@flow/types";
+
+import { Plus } from "@phosphor-icons/react";
 
 type ToolboxItem<T> = {
   id: T;
@@ -22,16 +25,19 @@ type ToolboxItem<T> = {
 
 type Tool = ToolboxItem<NodeType>;
 
-type CanvasAction = "undo" | "redo";
+type CanvasAction = "undo" | "redo" | "add";
 type Action = ToolboxItem<CanvasAction>;
 
 type Props = {
   undoDisabled?: boolean;
   onRedo?: () => void;
   onUndo?: () => void;
+  onWorkflowAdd?: (position?: {
+    x: number,
+    y: number
+  }) => void;
 };
-
-const Toolbox: React.FC<Props> = ({ onRedo, onUndo }) => {
+const Toolbox: React.FC<Props> = ({ onRedo, onUndo, onWorkflowAdd }) => {
   const t = useT();
 
   const availableTools: Tool[] = [
@@ -59,6 +65,11 @@ const Toolbox: React.FC<Props> = ({ onRedo, onUndo }) => {
       id: "batch",
       name: t("Batch Node"),
       icon: <RectangleDashed weight="thin" />,
+    },
+    {
+      id: "subworkflow",
+      name: t("Create new sub workflow"),
+      icon: <Plus className="size-4 stroke-1" weight="thin" />,
     },
   ];
 
@@ -89,7 +100,7 @@ const Toolbox: React.FC<Props> = ({ onRedo, onUndo }) => {
     root.render(
       <div className="flex size-12 rounded bg-secondary">
         <div
-          className={`flex w-full justify-center rounded align-middle  ${nodeType === "reader" ? "bg-node-reader/60" : nodeType === "writer" ? "bg-node-writer/60" : "bg-node-transformer/60"}`}>
+          className={`flex w-full justify-center rounded align-middle  ${nodeType === "reader" ? "bg-node-reader/60" : nodeType === "writer" ? "bg-node-writer/60" : nodeType === "subworkflow" ? "bg-node-entrance/60" : "bg-node-transformer/60"}`}>
           <Lightning className="self-center" />
         </div>
       </div>,
@@ -103,12 +114,56 @@ const Toolbox: React.FC<Props> = ({ onRedo, onUndo }) => {
       root.unmount();
       document.body.removeChild(dragPreviewContainer);
     }, 0);
+
+  };
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isDragging, setIsDragging] = useState(false);
+
+  const transparentImage = new Image();
+  transparentImage.src =
+    'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
+
+  const handleDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+    setIsDragging(true);
+    event.dataTransfer.setDragImage(transparentImage, 0, 0);
+    console.log("start")
+  };
+
+  const handleDrag = (event: React.DragEvent<HTMLDivElement>) => {
+    if (!isDragging) return;
+    setPosition({
+      x: event.clientX,
+      y: event.clientY,
+    });
+  };
+
+  const handleDragEnd = () => {
+    setIsDragging(false);
+    console.log(position.x)
+    console.log(position.y)
   };
 
   return (
-    <div className="pointer-events-none absolute bottom-1 left-2 top-2 flex shrink-0 gap-2 [&>*]:pointer-events-auto">
-      <div className="self-start rounded-md bg-secondary">
+    <div className="pointer-events-none absolute bottom-1 left-2 top-2 flex shrink-0 gap-2 [&>*]:pointer-events-auto" >
+      <div className="self-start rounded-md bg-secondary"
+        style={{
+        position: 'absolute',
+        left: position.x-65,
+        top: position.y-5,
+    }}>
         <div className="flex flex-col flex-wrap rounded-md border transition-all">
+        <div className="my-2 w-full border-t" />
+        <div
+          className="icon-wrapper"
+          draggable
+          onDragStart={handleDragStart}  
+          onDrag={handleDrag}            
+          onDragEnd={handleDragEnd}
+          style={{ cursor: 'move' }}>
+        <DotsSix className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium disabled:opacity-50 size-9 cursor-pointer transition-all dndnode-reader rounded-[4px]"/>
+        </div>
+        <div className="my-2 w-full border-t" />
           {availableTools.map((tool) => (
             <IconButton
               key={tool.id}
@@ -117,6 +172,16 @@ const Toolbox: React.FC<Props> = ({ onRedo, onUndo }) => {
               tooltipText={tool.name}
               icon={tool.icon}
               onDragStart={(event) => onDragStart(event, tool.id)}
+              onDragEnd={(event) =>
+                tool.id === "subworkflow"
+                ? onWorkflowAdd?.({ x: event.clientX-50, y: event.clientY})
+                :undefined
+              }
+              onClick={() =>
+                tool.id === "subworkflow"
+                  ? onWorkflowAdd?.()
+                  :undefined
+              }
               draggable
             />
           ))}
@@ -144,3 +209,4 @@ const Toolbox: React.FC<Props> = ({ onRedo, onUndo }) => {
 };
 
 export default memo(Toolbox);
+
