d53e66daf7b3617c9d0780c9972bbbbd1d0f2c46
akiyatomohiro <thairwars@gmail.com>
2024-10-31 15:07:28 +0900

    fix check permittion

--- api/internal/rbac/definitions.go
+++ api/internal/rbac/definitions.go
@@ -4,46 +4,46 @@ import (
 	"github.com/reearth/reearthx/cerbos/generator"
 )
 
+const (
+	ResourceProject  = "project"
+	ResourceWorkflow = "workflow"
+)
+
+const (
+	ActionRead = "read"
+	ActionEdit = "edit"
+)
+
+const (
+	roleOwner      = "owner"
+	roleMaintainer = "maintainer"
+	roleWriter     = "writer"
+	roleReader     = "reader"
+)
+
 func DefineResources(builder *generator.ResourceBuilder) []generator.ResourceDefinition {
-	const (
-		resourceProject  = "project"
-		resourceWorkflow = "workflow"
-	)
-
-	const (
-		actionRead = "read"
-		actionEdit = "edit"
-	)
-
-	const (
-		roleOwner      = "owner"
-		roleMaintainer = "maintainer"
-		roleWriter     = "writer"
-		roleReader     = "reader"
-	)
-
 	return builder.
-		AddResource(resourceProject, []generator.ActionDefinition{
-			generator.NewActionDefinition(actionRead, []string{
+		AddResource(ResourceProject, []generator.ActionDefinition{
+			generator.NewActionDefinition(ActionRead, []string{
 				roleOwner,
 				roleMaintainer,
 				roleWriter,
 				roleReader,
 			}),
-			generator.NewActionDefinition(actionEdit, []string{
+			generator.NewActionDefinition(ActionEdit, []string{
 				roleOwner,
 				roleMaintainer,
 				roleWriter,
 			}),
 		}).
-		AddResource(resourceWorkflow, []generator.ActionDefinition{
-			generator.NewActionDefinition(actionRead, []string{
+		AddResource(ResourceWorkflow, []generator.ActionDefinition{
+			generator.NewActionDefinition(ActionRead, []string{
 				roleOwner,
 				roleMaintainer,
 				roleWriter,
 				roleReader,
 			}),
-			generator.NewActionDefinition(actionEdit, []string{
+			generator.NewActionDefinition(ActionEdit, []string{
 				roleOwner,
 				roleMaintainer,
 				roleWriter,
