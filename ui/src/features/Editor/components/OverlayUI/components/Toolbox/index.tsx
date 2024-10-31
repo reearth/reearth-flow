3da79a3e1eb9aa1c48346bf5c525acb68cdafb7d
popy01012 <49387730+popy01012@users.noreply.github.com>
2024-10-31 10:49:43 +0900

    Update index.tsx

--- ui/src/features/Editor/components/OverlayUI/components/Toolbox/index.tsx
+++ ui/src/features/Editor/components/OverlayUI/components/Toolbox/index.tsx
@@ -117,52 +117,10 @@ const Toolbox: React.FC<Props> = ({ onRedo, onUndo, onWorkflowAdd }) => {
 
   };
 
-  const [position, setPosition] = useState({ x: 85, y: 15 });
-  const [isDragging, setIsDragging] = useState(false);
-
-  const transparentImage = new Image();
-  transparentImage.src =
-    'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
-
-  const handleDragStart = (event: React.DragEvent<HTMLDivElement>) => {
-    setIsDragging(true);
-    event.dataTransfer.setDragImage(transparentImage, 0, 0);
-    console.log("start")
-  };
-
-  const handleDrag = (event: React.DragEvent<HTMLDivElement>) => {
-    if (!isDragging) return;
-    setPosition({
-      x: event.clientX,
-      y: event.clientY,
-    });
-  };
-
-  const handleDragEnd = () => {
-    setIsDragging(false);
-    console.log(position.x)
-    console.log(position.y)
-  };
-
   return (
     <div className="pointer-events-none absolute bottom-1 left-2 top-2 flex shrink-0 gap-2 [&>*]:pointer-events-auto" >
-      <div className="self-start rounded-md bg-secondary"
-        style={{
-        position: 'absolute',
-        left: position.x-65,
-        top: position.y-5,
-    }}>
+      <div className="self-start rounded-md bg-secondary">
         <div className="flex flex-col flex-wrap rounded-md border transition-all">
-        <div className="my-2 w-full border-t" />
-        <div
-          className="icon-wrapper"
-          draggable
-          onDragStart={handleDragStart}  
-          onDrag={handleDrag}            
-          onDragEnd={handleDragEnd}
-          style={{ cursor: 'move' }}>
-        <DotsSix className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium disabled:opacity-50 size-9 cursor-pointer transition-all dndnode-reader rounded-[4px]"/>
-        </div>
         <div className="my-2 w-full border-t" />
           {availableTools.map((tool) => (
             <IconButton
