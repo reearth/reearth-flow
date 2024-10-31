d4c63004f731706ab5d50f684e07b89987e007d0
Yuji Mise <miseyu2000@gmail.com>
2024-10-31 14:43:44 +0900

    feat(pubsub): implement pubsub module with message handling and error management (#602)

--- engine/worker/src/errors.rs
+++ engine/worker/src/errors.rs
@@ -1,5 +1,5 @@
 #[derive(thiserror::Error, Debug)]
-pub enum WorkerError {
+pub enum Error {
     #[error("Failed to create tokio runtime: {0}")]
     FailedToCreateTokioRuntime(#[source] std::io::Error),
 
@@ -15,6 +15,9 @@ pub enum WorkerError {
     #[error("Failed to create workflow: {0}")]
     FailedToCreateWorkflow(String),
 
+    #[error("Failed to encode: {0}")]
+    FailedToEncode(#[source] serde_json::Error),
+
     #[error("Failed to initialize cli: {0}")]
     Init(String),
 
@@ -22,9 +25,9 @@ pub enum WorkerError {
     Run(String),
 }
 
-pub type Result<T, E = WorkerError> = std::result::Result<T, E>;
+pub type Result<T, E = Error> = std::result::Result<T, E>;
 
-impl WorkerError {
+impl Error {
     pub(crate) fn init<T: ToString>(message: T) -> Self {
         Self::Init(message.to_string())
     }
