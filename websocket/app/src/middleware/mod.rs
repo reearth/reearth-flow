506b6bf565ff86310998af63c05978ad31c7105e
xy <xyzmhx@gmail.com>
2024-11-19 02:09:44 +0900

    Update axum err handle

--- websocket/app/src/middleware/mod.rs
+++ websocket/app/src/middleware/mod.rs
@@ -1,7 +1,11 @@
 mod auth;
 mod trace;
 use auth::auth_middleware;
-use axum::{error_handling::HandleErrorLayer, http::StatusCode, middleware, BoxError, Router};
+use axum::{
+    error_handling::HandleErrorLayer, http::StatusCode, middleware, response::IntoResponse,
+    BoxError, Json, Router,
+};
+use serde_json::json;
 
 use std::time::Duration;
 use tower::timeout::TimeoutLayer;
@@ -15,11 +19,21 @@ where
     router.layer(
         ServiceBuilder::new()
             .layer(HandleErrorLayer::new(|err: BoxError| async move {
-                if err.is::<tower::timeout::error::Elapsed>() {
-                    StatusCode::REQUEST_TIMEOUT
+                let (status, message) = if err.is::<tower::timeout::error::Elapsed>() {
+                    (StatusCode::REQUEST_TIMEOUT, "Request timed out")
                 } else {
-                    StatusCode::INTERNAL_SERVER_ERROR
-                }
+                    (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error")
+                };
+
+                let json_response = json!({
+                    "error": {
+                        "message": message,
+                        "code": status.as_u16(),
+                        "details": err.to_string()
+                    }
+                });
+
+                (status, Json(json_response)).into_response()
             }))
             .layer(TimeoutLayer::new(Duration::from_secs(10)))
             .layer(add_trace_middleware())
