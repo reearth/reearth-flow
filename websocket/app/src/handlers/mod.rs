48dc0369ac0bc0789b944f7c75c0ae5ab1eb5a65
xy <xyzmhx@gmail.com>
2024-11-23 01:32:50 +0900

    Separate websocket message handler

--- websocket/app/src/handlers/mod.rs
+++ websocket/app/src/handlers/mod.rs
@@ -1,4 +1,29 @@
+mod cleanup;
+mod heartbeat;
+mod message_handler;
+mod room_handler;
+mod socket_handler;
 mod types;
-mod websocket;
 
-pub use websocket::handle_upgrade;
+use crate::handlers::socket_handler::handle_socket;
+use crate::state::AppState;
+use axum::extract::{ws::WebSocketUpgrade, ConnectInfo, Path, Query, State};
+use axum::response::IntoResponse;
+use axum::response::Response;
+use flow_websocket_infra::types::user::User;
+use std::{net::SocketAddr, sync::Arc};
+use types::WebSocketQuery;
+
+pub async fn handle_upgrade(
+    ws: WebSocketUpgrade,
+    query: Query<WebSocketQuery>,
+    Path(room_id): Path<String>,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+    State(state): State<Arc<AppState>>,
+) -> Response {
+    let user = User::new(query.user_id().to_string(), None, None);
+    ws.on_upgrade(move |socket| {
+        handle_socket(socket, addr, room_id, state, query.project_id(), user)
+    })
+    .into_response()
+}
---
+++ websocket/app/src/handlers/socket_handler.rs
@@ -0,0 +1,76 @@
+use crate::handlers::message_handler::handle_message;
+use crate::state::AppState;
+use axum::extract::ws::WebSocket;
+use flow_websocket_infra::types::user::User;
+use futures_util::StreamExt;
+use std::sync::atomic::AtomicBool;
+use std::{net::SocketAddr, sync::Arc};
+use tokio::sync::{mpsc, Mutex};
+use tracing::debug;
+
+use super::cleanup::perform_cleanup;
+use super::heartbeat::start_heartbeat;
+
+pub async fn handle_socket(
+    socket: WebSocket,
+    addr: SocketAddr,
+    room_id: String,
+    state: Arc<AppState>,
+    project_id: Option<String>,
+    user: User,
+) {
+    let (sender, mut receiver) = socket.split();
+    let sender = Arc::new(Mutex::new(sender)); // 封装为 Arc<Mutex<>>
+    let is_cleaning_up = Arc::new(AtomicBool::new(false));
+    let (cleanup_tx, mut cleanup_rx) = mpsc::channel(1);
+
+    let cleanup = {
+        let is_cleaning_up = is_cleaning_up.clone();
+        let room_id = room_id.clone();
+        let user = user.clone();
+        let project_id = project_id.clone();
+        let state = state.clone();
+        let cleanup_tx = cleanup_tx.clone();
+
+        Arc::new(move || {
+            let _ = perform_cleanup(
+                is_cleaning_up.clone(),
+                room_id.clone(),
+                user.clone(),
+                project_id.clone(),
+                state.clone(),
+                cleanup_tx.clone(),
+            );
+        }) as Arc<dyn Fn() + Send + Sync>
+    };
+
+    let heartbeat_task = start_heartbeat(sender, addr, move || cleanup());
+
+    while let Some(msg) = receiver.next().await {
+        match msg {
+            Ok(msg) => {
+                if let Err(e) = handle_message(
+                    msg,
+                    addr,
+                    &room_id,
+                    project_id.clone(),
+                    state.clone(),
+                    user.clone(),
+                )
+                .await
+                {
+                    debug!("Error handling message: {:?}", e);
+                    continue;
+                }
+            }
+            Err(e) => {
+                debug!("Error receiving message: {:?}", e);
+                debug!("Message from {addr} is not ok");
+                continue;
+            }
+        }
+    }
+
+    let _ = cleanup_rx.recv().await;
+    heartbeat_task.abort();
+}
