6365b017527b1b4bf220bab97d58d69703515a67
KaWaite <kyle.waite@outlook.com>
2024-12-02 10:11:23 +0900

    improve workflow tabs

---
+++ ui/src/features/Editor/components/WorkflowTabs/WorkflowTab.tsx
@@ -0,0 +1,79 @@
+import { X } from "@phosphor-icons/react";
+import { useState } from "react";
+
+import { Input } from "@flow/components";
+import { useT } from "@flow/lib/i18n";
+
+type Props = {
+  currentWorkflowId?: string;
+  editId?: string;
+  id: string;
+  name?: string;
+  setName: (name: string) => void;
+  onWorkflowChange: (workflowId?: string) => void;
+  onWorkflowClose: (
+    workflowId: string,
+  ) => (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  onDoubleClick: (workflowId: string, name: string | undefined) => void;
+  onSubmit: () => void;
+};
+
+const WorkflowTab: React.FC<Props> = ({
+  currentWorkflowId,
+  editId,
+  id,
+  name,
+  setName,
+  onWorkflowChange,
+  onWorkflowClose,
+  onDoubleClick,
+  onSubmit,
+}) => {
+  const t = useT();
+  const [closeHover, setCloseHover] = useState(false);
+
+  const isEditing = editId === id;
+  return (
+    <div
+      className={`relative flex h-4/5 w-[150px] items-center justify-center rounded ${currentWorkflowId === id ? "bg-node-entrance/70 text-accent-foreground" : "hover:bg-node-entrance/30"} group cursor-pointer`}
+      onClick={() => onWorkflowChange(id)}
+      onDoubleClick={() => onDoubleClick(id, name)}
+      key={id}>
+      {isEditing ? (
+        <Input
+          className="h-4 border-none text-center text-xs focus-visible:ring-0 dark:font-extralight"
+          defaultValue={name}
+          placeholder={t("Set Workflow name")}
+          autoFocus
+          onKeyDownCapture={(e) => e.key === "Enter" && onSubmit()}
+          onChange={(e) => setName(e.target.value)}
+          onBlur={onSubmit}
+        />
+      ) : (
+        <p
+          className={`ml-[15px] mr-[19px] select-none truncate text-center text-xs group-hover:text-accent-foreground dark:font-extralight ${currentWorkflowId === id && "text-accent-foreground"}`}>
+          {name}
+        </p>
+      )}
+      {!isEditing && (
+        <div className="absolute right-0 flex h-full justify-end rounded">
+          <div
+            className={`flex h-full items-center justify-self-end overflow-hidden rounded px-1 hover:bg-node-exit ${closeHover ? "w-[150px] transition-all delay-200" : "w-[20px]"}`}
+            onMouseEnter={() => setCloseHover(true)}
+            onMouseLeave={() => setCloseHover(false)}
+            onClick={onWorkflowClose(id)}>
+            <div className="flex-1 overflow-hidden">
+              <p
+                className={`rounded-l text-center text-xs transition-all delay-200 dark:font-extralight ${closeHover ? "opacity-100" : "opacity-0"}`}>
+                {t("Close canvas")}
+              </p>
+            </div>
+            <X className="size-[12px]" weight="bold" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default WorkflowTab;
