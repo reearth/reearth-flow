15cf5633191a3586598d0dcb15503893a4929fde
Yuji Mise <miseyu2000@gmail.com>
2024-12-05 17:39:11 +0900

    feat(engine): update example workflow path and enhance error messages… (#676)

--- engine/runtime/action-processor/src/feature/sorter.rs
+++ engine/runtime/action-processor/src/feature/sorter.rs
@@ -1,4 +1,4 @@
-use std::{cmp::Ordering, collections::HashMap};
+use std::{cmp::Ordering, collections::HashMap, sync::Arc};
 
 use reearth_flow_runtime::{
     channels::ProcessorChannelForwarder,
@@ -7,7 +7,7 @@ use reearth_flow_runtime::{
     executor_operation::{ExecutorContext, NodeContext},
     node::{Port, Processor, ProcessorFactory, DEFAULT_PORT},
 };
-use reearth_flow_types::{Attribute, Feature};
+use reearth_flow_types::{Attribute, AttributeValue, Expr, Feature};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
@@ -44,12 +44,12 @@ impl ProcessorFactory for FeatureSorterFactory {
 
     fn build(
         &self,
-        _ctx: NodeContext,
+        ctx: NodeContext,
         _event_hub: EventHub,
         _action: String,
         with: Option<HashMap<String, Value>>,
     ) -> Result<Box<dyn Processor>, BoxedError> {
-        let params: FeatureSorterParam = if let Some(with) = with {
+        let params: FeatureSorterParam = if let Some(with) = with.clone() {
             let value: Value = serde_json::to_value(with).map_err(|e| {
                 FeatureProcessorError::SorterFactory(format!(
                     "Failed to serialize `with` parameter: {}",
@@ -68,8 +68,35 @@ impl ProcessorFactory for FeatureSorterFactory {
             )
             .into());
         };
+
+        let expr_engine = Arc::clone(&ctx.expr_engine);
+        let mut sort_by = Vec::new();
+        for sort in &params.sort_by {
+            let attribute_value = if let Some(attribute_value) = &sort.attribute_value {
+                Some(
+                    expr_engine
+                        .compile(attribute_value.as_ref())
+                        .map_err(|e| FeatureProcessorError::FilterFactory(format!("{:?}", e)))?,
+                )
+            } else {
+                if sort.attribute.is_none() {
+                    return Err(FeatureProcessorError::FilterFactory(
+                        "Either `attribute` or `attributeValue` is required".to_string(),
+                    )
+                    .into());
+                }
+                None
+            };
+            sort_by.push(CompiledSortBy {
+                attribute: sort.attribute.clone(),
+                attribute_value,
+                order: sort.order.clone(),
+            });
+        }
+
         let process = FeatureSorter {
-            params,
+            global_params: with,
+            params: FeatureSorterCompiledParam { sort_by },
             buffer: vec![],
         };
         Ok(Box::new(process))
@@ -78,7 +105,8 @@ impl ProcessorFactory for FeatureSorterFactory {
 
 #[derive(Debug, Clone)]
 pub struct FeatureSorter {
-    params: FeatureSorterParam,
+    global_params: Option<HashMap<String, serde_json::Value>>,
+    params: FeatureSorterCompiledParam,
     buffer: Vec<Feature>,
 }
 
@@ -88,10 +116,23 @@ pub struct FeatureSorterParam {
     sort_by: Vec<SortBy>,
 }
 
+#[derive(Debug, Clone)]
+pub struct FeatureSorterCompiledParam {
+    sort_by: Vec<CompiledSortBy>,
+}
+
 #[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
 #[serde(rename_all = "camelCase")]
 struct SortBy {
-    attribute: Attribute,
+    attribute: Option<Attribute>,
+    attribute_value: Option<Expr>,
+    order: Order,
+}
+
+#[derive(Debug, Clone)]
+struct CompiledSortBy {
+    attribute: Option<Attribute>,
+    attribute_value: Option<rhai::AST>,
     order: Order,
 }
 
@@ -126,16 +167,34 @@ impl Processor for FeatureSorter {
                 .sort_by
                 .iter()
                 .map(|sort_by| {
-                    let attribute = &sort_by.attribute;
+                    let (a, b) = if let Some(attribute) = &sort_by.attribute {
+                        (
+                            a.attributes.get(attribute).cloned(),
+                            b.attributes.get(attribute).cloned(),
+                        )
+                    } else if let Some(attribute_value) = &sort_by.attribute_value {
+                        let a_scope = a.new_scope(ctx.expr_engine.clone(), &self.global_params);
+                        let b_scope = b.new_scope(ctx.expr_engine.clone(), &self.global_params);
+                        (
+                            a_scope
+                                .eval_ast::<String>(attribute_value)
+                                .map(AttributeValue::String)
+                                .ok(),
+                            b_scope
+                                .eval_ast::<String>(attribute_value)
+                                .map(AttributeValue::String)
+                                .ok(),
+                        )
+                    } else {
+                        (None, None)
+                    };
                     let order = &sort_by.order;
-                    let a = a.attributes.get(attribute);
-                    let b = b.attributes.get(attribute);
                     match (a, b) {
                         (Some(a), Some(b)) => {
                             if *order == Order::Asc {
-                                a.partial_cmp(b)
+                                a.partial_cmp(&b)
                             } else {
-                                b.partial_cmp(a)
+                                b.partial_cmp(&a)
                             }
                         }
                         _ => None,
