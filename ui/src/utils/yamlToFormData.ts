55988ea1888a28df017249f9cc586d6d328d57b3
KaWaite <kyle.waite@outlook.com>
2024-12-18 14:31:31 +0900

    yaml->yml

--- ui/src/utils/yamlToFormData.ts
+++ ui/src/utils/yamlToFormData.ts
@@ -1,5 +1,5 @@
 export const yamlToFormData = (yaml: string, fileName?: string) => {
-  const yamlFile = new File([yaml], `${fileName || "untitled"}.yaml`, {
+  const yamlFile = new File([yaml], `${fileName || "untitled"}.yml`, {
     type: "application/x-yaml",
   });
   const formData = new FormData();
