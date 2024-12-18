9984ad018786776a8d4ad4a8d1aa45931cb825ec
pyshx <piyushchauhan1004@gmail.com>
2024-12-18 18:07:49 +0530

    remove false test

--- ui/src/utils/generateUUID.test.ts
+++ ui/src/utils/generateUUID.test.ts
@@ -1,9 +1,5 @@
 import { generateUUID } from "./generateUUID";
 
-test("random id with a default length of 24", () => {
-  expect(generateUUID().length).toBe(24);
-});
-
 test("two ids are not equal", () => {
   expect(generateUUID()).not.toBe(generateUUID());
 });
