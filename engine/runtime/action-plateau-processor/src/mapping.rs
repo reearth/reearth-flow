13370d28cb36b2f4c56c573083bba38105e66a2b
Yuji Mise <miseyu2000@gmail.com>
2024-12-18 13:48:25 +0900

    feat(engine): add plateau4 module and UDXFolderExtractor processor

--- engine/runtime/action-plateau-processor/src/mapping.rs
+++ engine/runtime/action-plateau-processor/src/mapping.rs
@@ -3,10 +3,11 @@ use std::collections::HashMap;
 use once_cell::sync::Lazy;
 use reearth_flow_runtime::node::NodeKind;
 
-use crate::plateau3;
+use crate::{plateau3, plateau4};
 
 pub static ACTION_FACTORY_MAPPINGS: Lazy<HashMap<String, NodeKind>> = Lazy::new(|| {
     let mut mapping = HashMap::new();
     mapping.extend(plateau3::mapping::ACTION_FACTORY_MAPPINGS.clone());
+    mapping.extend(plateau4::mapping::ACTION_FACTORY_MAPPINGS.clone());
     mapping
 });
