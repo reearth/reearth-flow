70848a912754ab1a6183d4629a15241ae4d0a4ed
Yuji Mise <miseyu2000@gmail.com>
2025-01-08 19:07:24 +0900

    feat(engine): add ProcessorFailed event and handle processor failure in event handler (#747)

--- engine/worker/src/event_handler.rs
+++ engine/worker/src/event_handler.rs
@@ -1,3 +1,7 @@
+use std::sync::Arc;
+
+use parking_lot::Mutex;
+use reearth_flow_runtime::node::NodeHandle;
 use uuid::Uuid;
 
 use crate::{
@@ -10,6 +14,26 @@ use crate::{
 
 use self::edge_pass_through_event::UpdatedEdge;
 
+#[derive(Debug)]
+pub(crate) struct ProcessorFailedHandler {
+    pub(crate) failed_nodes: Arc<Mutex<Vec<NodeHandle>>>,
+}
+
+impl ProcessorFailedHandler {
+    pub(crate) fn new() -> Self {
+        let failed_nodes = Arc::new(Mutex::new(Vec::new()));
+        Self { failed_nodes }
+    }
+}
+
+#[async_trait::async_trait]
+impl reearth_flow_runtime::event::EventHandler for ProcessorFailedHandler {
+    async fn on_event(&self, event: &reearth_flow_runtime::event::Event) {
+        if let reearth_flow_runtime::event::Event::ProcessorFailed { node, .. } = event {
+            self.failed_nodes.lock().push(node.clone());
+        }
+    }
+}
 pub(crate) struct EventHandler<P: Publisher> {
     pub(crate) workflow_id: Uuid,
     pub(crate) job_id: Uuid,
