256a68c7aeee645f9f17068d952661b426ae1fb3
Bill Cook <billcook8122@gmail.com>
2025-01-08 10:00:57 +0900

    fix(ui): Created reusable hook for isMainWorkFlow

--- ui/src/features/Editor/index.tsx
+++ ui/src/features/Editor/index.tsx
@@ -2,6 +2,8 @@ import { Array as YArray, UndoManager as YUndoManager } from "yjs";
 
 import { YWorkflow } from "@flow/lib/yjs/utils";
 
+import { useIsMainWorkflow } from "../KeyboardShortcutDialog/useHooks";
+
 import {
   BottomPanel,
   Canvas,
@@ -51,7 +53,7 @@ export default function Editor({
     handleWorkflowUndo,
     handleWorkflowRename,
   } = useHooks({ yWorkflows, undoManager, undoTrackerActionWrapper });
-  const isMainWorkflow = openWorkflows[0]?.id === currentWorkflowId;
+  const isMainWorkflow = useIsMainWorkflow(currentWorkflowId);
   return (
     <div className="flex h-screen flex-col">
       <div className="relative flex flex-1">
