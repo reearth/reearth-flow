a58b8ba9761385ed521d764e6527663900bba33a
KaWaite <kyle.waite@outlook.com>
2025-01-17 11:27:09 +0900

    Clean job type

--- ui/src/types/job.ts
+++ ui/src/types/job.ts
@@ -1,9 +1,4 @@
-import { Deployment } from "./deployment";
-// import { Project } from "./project";
-
 export type JobStatus = "queued" | "running" | "completed" | "failed";
-
-export type TriggerType = "api" | "cms" | "manual"; // do we need CMS? Or maybe "api" and "native" is enough? or..?
 
 export type Job = {
   id: string;
@@ -12,24 +7,4 @@ export type Job = {
   status: JobStatus;
   startedAt: string;
   completedAt: string;
-  logs?: unknown; // or boolean? or logId?
-  trigger?: TriggerType;
-  deployment?: Deployment;
-  // workspace: Workspace;
 };
-
-// type Run = {
-//   id: string;
-//   project: Pick<Project, "id" | "name" | "workflows" | "createdAt">;
-//   // projectId: string;
-//   // projectRevisionId: string; OR projectVersionId: string; OR workflowId: string;
-//   status: "running" | "queued" | "completed" | "failed";
-//   startedAt: string;
-//   completedAt?: string;
-//   logs?: unknown; // or boolean? or logId?
-//   ranBy?: string;
-//   trigger?: Trigger;
-// };
-
-// Do we need a RunResult type?
-// How do we append NodeResults? Does order matter?
