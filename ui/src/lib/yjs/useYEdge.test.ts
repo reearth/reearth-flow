c22b17c4c0abd2d577863123c9975ad555bfe246
KaWaite <kyle.waite@outlook.com>
2025-01-28 14:56:02 +0900

    add additional test

--- ui/src/lib/yjs/useYEdge.test.ts
+++ ui/src/lib/yjs/useYEdge.test.ts
@@ -4,7 +4,7 @@ import * as Y from "yjs";
 
 import type { Edge } from "@flow/types";
 
-import { yWorkflowConstructor } from "./conversions";
+import { yEdgeConstructor, yWorkflowConstructor } from "./conversions";
 import { YEdgesArray, YWorkflow } from "./types";
 import useYEdge from "./useYEdge";
 
@@ -13,7 +13,7 @@ afterEach(() => {
 });
 
 describe("useYEdge", () => {
-  test("should update edges correctly", () => {
+  test("should add edges correctly", () => {
     const yDoc = new Y.Doc();
     const yWorkflows = yDoc.getArray<YWorkflow>("workflows");
     const yWorkflow = yWorkflowConstructor("workflow-1", "My Workflow");
@@ -23,11 +23,12 @@ describe("useYEdge", () => {
     const { result } = renderHook(() =>
       useYEdge({
         currentYWorkflow: yWorkflow,
+        setSelectedEdgeIds: () => {},
         undoTrackerActionWrapper: (callback) => act(callback),
       }),
     );
 
-    const { handleYEdgesUpdate } = result.current;
+    const { handleYEdgesAdd } = result.current;
 
     const newEdges: Edge[] = [
       {
@@ -46,7 +47,80 @@ describe("useYEdge", () => {
       },
     ];
 
-    handleYEdgesUpdate(newEdges);
+    handleYEdgesAdd(newEdges);
+
+    const yEdges = yWorkflow.get("edges") as YEdgesArray;
+
+    const e = yEdges.toJSON() as Edge[];
+
+    expect(e).toEqual(newEdges);
+  });
+
+  test("should update edges correctly", () => {
+    const yDoc = new Y.Doc();
+    const yWorkflows = yDoc.getArray<YWorkflow>("workflows");
+    const yWorkflow = yWorkflowConstructor("workflow-1", "My Workflow");
+
+    const initialEdges: Edge[] = [
+      {
+        id: "edge-1",
+        source: "node-1",
+        target: "node-2",
+        sourceHandle: "output1",
+        targetHandle: "input1",
+      },
+      {
+        id: "edge-2",
+        source: "node-3",
+        target: "node-4",
+        sourceHandle: "output2",
+        targetHandle: "input2",
+      },
+    ];
+    (yWorkflow.get("edges") as YEdgesArray)?.insert(
+      0,
+      initialEdges.map((ie) => yEdgeConstructor(ie)),
+    );
+
+    yWorkflows.push([yWorkflow]);
+
+    const { result } = renderHook(() =>
+      useYEdge({
+        currentYWorkflow: yWorkflow,
+        setSelectedEdgeIds: () => {},
+        undoTrackerActionWrapper: (callback) => act(callback),
+      }),
+    );
+
+    const { handleYEdgesChange } = result.current;
+
+    const newEdges: Edge[] = [
+      {
+        id: "edge-1",
+        source: "node-5",
+        target: "node-2",
+        sourceHandle: "output1",
+        targetHandle: "input1",
+      },
+      {
+        id: "edge-2",
+        source: "node-3",
+        target: "node-4",
+        sourceHandle: "output4",
+        targetHandle: "input2",
+      },
+    ];
+
+    handleYEdgesChange([
+      {
+        type: "add",
+        item: newEdges[0],
+      },
+      {
+        type: "add",
+        item: newEdges[1],
+      },
+    ]);
 
     const yEdges = yWorkflow.get("edges") as YEdgesArray;
 
