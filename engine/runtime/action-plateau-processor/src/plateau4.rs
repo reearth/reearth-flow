74d53e85045db8687c3202d26eb2955990eb4b81
Yuji Mise <miseyu2000@gmail.com>
2025-01-28 12:02:18 +0900

    feat(engine): add ObjectListExtractor for extracting object lists

--- engine/runtime/action-plateau-processor/src/plateau4.rs
+++ engine/runtime/action-plateau-processor/src/plateau4.rs
@@ -3,4 +3,5 @@ pub(crate) mod city_code_extractor;
 pub(crate) mod errors;
 pub(crate) mod mapping;
 pub(crate) mod max_lod_extractor;
+pub(crate) mod object_list_extractor;
 pub(crate) mod udx_folder_extractor;
