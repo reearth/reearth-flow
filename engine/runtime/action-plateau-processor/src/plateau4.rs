a3f018bdee496f4c379cf2f70669834108ff1989
Yuji Mise <miseyu2000@gmail.com>
2025-01-30 14:21:24 +0900

    feat(engine): integrate MissingAttributeDetector and enhance ObjectListExtractor functionality

--- engine/runtime/action-plateau-processor/src/plateau4.rs
+++ engine/runtime/action-plateau-processor/src/plateau4.rs
@@ -3,5 +3,6 @@ pub(crate) mod city_code_extractor;
 pub(crate) mod errors;
 pub(crate) mod mapping;
 pub(crate) mod max_lod_extractor;
+pub(crate) mod missing_attribute_detector;
 pub(crate) mod object_list_extractor;
 pub(crate) mod udx_folder_extractor;
