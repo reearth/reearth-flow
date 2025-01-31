97b11f2a18676c34fc47e3dcfa12b5ba31d5354e
Bill Cook <102199454+billcookie@users.noreply.github.com>
2025-01-31 14:16:57 +0900

    fix(ui): Added mock data and new log type (#784)
    * fix(ui): Added mock data and new log type
    
    * fix(ui): added tooltip message for log filters
    
    * fix(ui): chinese translations added
    
    * fix(ui): Logs added for job details page
    
    * fix(ui): removed rounded border and colour change

--- ui/src/types/log.ts
+++ ui/src/types/log.ts
@@ -1,1 +1,23 @@
-export type LogStatus = "WARNING" | "INFO" | "ERROR";
+import { ApiResponse } from "./api";
+
+export enum LogLevel {
+  ERROR = "ERROR",
+  WARN = "WARN",
+  INFO = "INFO",
+  DEBUG = "DEBUG",
+  TRACE = "TRACE",
+}
+
+export type Log = {
+  id: string;
+  workflowId: string;
+  jobId: string;
+  nodeId?: string;
+  timestamp: string;
+  logLevel: LogLevel;
+  message: string;
+};
+
+export type GetLogs = {
+  Logs?: Log[];
+} & ApiResponse;
