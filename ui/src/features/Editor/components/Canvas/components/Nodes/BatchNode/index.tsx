078f606ff0237fec70938c29d4f892b05adaf78a
Bill Cook <billcook8122@gmail.com>
2025-02-07 11:48:47 +0900

    fix(ui): updated customName to be correct params

--- ui/src/features/Editor/components/Canvas/components/Nodes/BatchNode/index.tsx
+++ ui/src/features/Editor/components/Canvas/components/Nodes/BatchNode/index.tsx
@@ -97,7 +97,7 @@ const BatchNode: React.FC<BatchNodeProps> = ({ data, selected, id }) => {
   // background color will always be a hex color, therefore needs to be converted to rgba
   const backgroundColor = data.params?.backgroundColor || "";
   const rgbaColor = convertHextoRgba(backgroundColor, 0.5);
-
+  console.log("data", data.customName);
   return (
     <>
       {selected && (
@@ -144,7 +144,7 @@ const BatchNode: React.FC<BatchNodeProps> = ({ data, selected, id }) => {
               );
           }}>
           <RectangleDashed />
-          <p>{data.customName || data.officialName}</p>
+          <p>{data.params?.customName || data.officialName}</p>
         </div>
       </div>
     </>
