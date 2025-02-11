20c5a1dc84010bf8bad67413429192cc008d844e
pyshx <piyushchauhan1004@gmail.com>
2025-02-12 03:57:27 +0530

    chore(api): add websocket transport layer to graphql api

--- api/internal/app/app.go
+++ api/internal/app/app.go
@@ -92,7 +92,7 @@ func initEcho(ctx context.Context, cfg *ServerConfig) *echo.Echo {
 	// authenticated routes
 	apiPrivate := api.Group("", privateCache)
 	apiPrivate.Use(authMiddleware, attachOpMiddleware(cfg))
-	apiPrivate.POST("/graphql", GraphqlAPI(cfg.Config.GraphQL, gqldev))
+	apiPrivate.Any("/graphql", GraphqlAPI(cfg.Config.GraphQL, gqldev, origins))
 	apiPrivate.POST("/signup", Signup())
 
 	if !cfg.Config.AuthSrv.Disabled {
