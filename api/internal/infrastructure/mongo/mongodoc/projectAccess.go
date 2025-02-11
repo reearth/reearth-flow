c2e0f7d8dc1495786f061015e5826336b17a1c70
akiyatomohiro <thairwars@gmail.com>
2025-02-11 21:22:28 +0700

    add projectAccess mongo

---
+++ api/internal/infrastructure/mongo/mongodoc/projectAccess.go
@@ -0,0 +1,50 @@
+package mongodoc
+
+import (
+	"github.com/reearth/reearth-flow/api/pkg/id"
+	"github.com/reearth/reearth-flow/api/pkg/projectAccess"
+)
+
+type ProjectAccessDocument struct {
+	ID       string
+	Project  string
+	IsPublic bool
+	Token    string
+}
+
+type ProjectAccessConsumer = Consumer[*ProjectAccessDocument, *projectAccess.ProjectAccess]
+
+func NewProjectAccessConsumer() *ProjectAccessConsumer {
+	return NewConsumer[*ProjectAccessDocument, *projectAccess.ProjectAccess](func(a *projectAccess.ProjectAccess) bool {
+		return true
+	})
+}
+
+func NewProjectAccess(projectAccess *projectAccess.ProjectAccess) (*ProjectAccessDocument, string) {
+	paid := projectAccess.ID().String()
+
+	return &ProjectAccessDocument{
+		ID:       paid,
+		Project:  projectAccess.Project().String(),
+		IsPublic: projectAccess.IsPublic(),
+		Token:    projectAccess.Token(),
+	}, paid
+}
+
+func (d *ProjectAccessDocument) Model() (*projectAccess.ProjectAccess, error) {
+	paid, err := id.ProjectAccessIDFrom(d.ID)
+	if err != nil {
+		return nil, err
+	}
+	pid, err := id.ProjectIDFrom(d.Project)
+	if err != nil {
+		return nil, err
+	}
+
+	return projectAccess.New().
+		ID(paid).
+		Project(pid).
+		IsPublic(d.IsPublic).
+		Token(d.Token).
+		Build()
+}
---
+++ api/internal/infrastructure/mongo/projectAccess_test.go
@@ -0,0 +1,97 @@
+package mongo
+
+import (
+	"context"
+	"testing"
+
+	"github.com/reearth/reearth-flow/api/pkg/id"
+
+	"github.com/reearth/reearthx/mongox"
+	"github.com/reearth/reearthx/mongox/mongotest"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func TestProjectAccess_FindByProjectID(t *testing.T) {
+	c := mongotest.Connect(t)(t)
+	ctx := context.Background()
+	paid1 := id.NewProjectAccessID()
+	paid2 := id.NewProjectAccessID()
+	pid1 := id.NewProjectID()
+	pid2 := id.NewProjectID()
+
+	_, _ = c.Collection("projectAccess").InsertMany(ctx, []any{
+		bson.M{"id": paid1.String(), "projelct": pid1.String(), "ispublic": true, "token": "token"},
+		bson.M{"id": paid2.String(), "projelct": pid2.String(), "ispublic": true, "token": "token"},
+	})
+
+	pa := NewProjectAccess(mongox.NewClientWithDatabase(c))
+
+	got, err := pa.FindByProjectID(ctx, pid1)
+	assert.NoError(t, err)
+	assert.Equal(t, paid1, got.ID())
+	assert.Equal(t, pid1, got.Project())
+	assert.True(t, got.IsPublic())
+	assert.Equal(t, "token", got.Token())
+
+	got, err = pa.FindByProjectID(ctx, id.NewProjectID())
+	assert.Error(t, err)
+	assert.Nil(t, got)
+}
+
+func TestProjectAccess_FindByToken(t *testing.T) {
+	c := mongotest.Connect(t)(t)
+	ctx := context.Background()
+	paid1 := id.NewProjectAccessID()
+	paid2 := id.NewProjectAccessID()
+	pid1 := id.NewProjectID()
+	pid2 := id.NewProjectID()
+
+	_, _ = c.Collection("projectAccess").InsertMany(ctx, []any{
+		bson.M{"id": paid1.String(), "projelct": pid1.String(), "ispublic": true, "token": "token1"},
+		bson.M{"id": paid2.String(), "projelct": pid2.String(), "ispublic": true, "token": "token2"},
+	})
+
+	pa := NewProjectAccess(mongox.NewClientWithDatabase(c))
+
+	got, err := pa.FindByToken(ctx, "token1")
+	assert.NoError(t, err)
+	assert.Equal(t, paid1, got.ID())
+	assert.Equal(t, pid1, got.Project())
+	assert.True(t, got.IsPublic())
+	assert.Equal(t, "token1", got.Token())
+
+	got, err = pa.FindByToken(ctx, "not-exist-token")
+	assert.Error(t, err)
+	assert.Nil(t, got)
+}
+
+func TestProjectAccess_Save(t *testing.T) {
+	c := mongotest.Connect(t)(t)
+	ctx := context.Background()
+	paid := id.NewProjectAccessID()
+	pid := id.NewProjectID()
+
+	_, _ = c.Collection("projectAccess").InsertOne(ctx, bson.M{"id": paid.String(), "projelct": pid.String(), "ispublic": true, "token": "token"})
+
+	pa := NewProjectAccess(mongox.NewClientWithDatabase(c))
+
+	got, err := pa.FindByProjectID(ctx, pid)
+	assert.NoError(t, err)
+	assert.Equal(t, paid, got.ID())
+	assert.Equal(t, pid, got.Project())
+	assert.True(t, got.IsPublic())
+	assert.Equal(t, "token", got.Token())
+
+	got.SetIsPublic(false)
+	got.SetToken("")
+	err = pa.Save(ctx, got)
+	assert.NoError(t, err)
+
+	got, err = pa.FindByProjectID(ctx, pid)
+	assert.NoError(t, err)
+	assert.Equal(t, paid, got.ID())
+	assert.Equal(t, pid, got.Project())
+	assert.False(t, got.IsPublic())
+	assert.Empty(t, got.Token())
+}
