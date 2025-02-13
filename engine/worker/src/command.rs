d8f8f5620da549bd84b1aeb19bb105ec57ea27ea
Yuji Mise <miseyu2000@gmail.com>
2025-02-13 14:04:02 +0900

    feat(cli): update CLI and worker command to include version information

--- engine/worker/src/command.rs
+++ engine/worker/src/command.rs
@@ -24,9 +24,10 @@ const WORKER_ASSET_GLOBAL_PARAMETER_VARIABLE: &str = "workerAssetPath";
 const WORKER_ARTIFACT_GLOBAL_PARAMETER_VARIABLE: &str = "workerArtifactPath";
 
 pub fn build_worker_command() -> Command {
-    Command::new("worker")
-        .about("Start worker.")
+    Command::new("Re:Earth Flow Worker")
+        .about("Start flow worker.")
         .long_about("Start a worker to run a workflow.")
+        .version(env!("CARGO_PKG_VERSION"))
         .arg(workflow_arg())
         .arg(asset_arg())
         .arg(worker_num_arg())
