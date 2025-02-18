ba1e93998831af0768bddd405d45a98c8eeb663d
xy <xyzmhx@gmail.com>
2025-02-19 05:27:39 +0900

    chore(websocket): remove http prefix from auth and gcs endpoint URLs

--- server/api/internal/infrastructure/websocket/client.go
+++ server/api/internal/infrastructure/websocket/client.go
@@ -20,9 +20,6 @@ type Client struct {
 
 func NewClient(address string) (*Client, error) {
 
-	address = strings.TrimPrefix(address, "http://")
-	address = strings.TrimPrefix(address, "https://")
-
 	// If no port is specified, use the default gRPC port
 	if !strings.Contains(address, ":") {
 		address = fmt.Sprintf("%s:50051", address)
