74b334b456a9b2d2215e446baf64cb9ba4404874
Bill Cook <102199454+billcookie@users.noreply.github.com>
2025-02-19 15:02:56 +0900

    fix(ui): Improved Canvas Nav V-2 (#871)
    * fix(ui): hide empty transformers
    
    * fix(ui): added focus on double click
    
    * fix(ui): small spelling fix and parent position for nested nodes
    
    * fix(ui): center on focus node
    
    * fix(ui): Lock from old PR added.
    
    * fix(ui): removed left panel condition
    
    * fix(ui): Removed console logs
    
    * fix(ui): cleanup and removed locker
    
    * fix(ui): added doubleclick for params
    
    * fix(ui): added zoom position reset
    
    * fix(ui): small refacotr of tree items
    
    * fix(ui): cleanup for tree items
    
    * fix(ui): removed params open
    
    * fix(ui): remove previous zoom

--- ui/src/features/Editor/index.tsx
+++ ui/src/features/Editor/index.tsx
@@ -66,11 +66,13 @@ export default function Editor({
       <div className="relative flex flex-1">
         <LeftPanel
           nodes={nodes}
-          isOpen={openPanel === "left" && !locallyLockedNode}
+          isOpen={openPanel === "left"}
           onOpen={handlePanelOpen}
           onNodesAdd={handleNodesAdd}
           isMainWorkflow={isMainWorkflow}
           hasReader={hasReader}
+          // onNodeDoubleClick={handleNodeDoubleClick}
+          // selected={locallyLockedNode}
         />
         <div className="flex flex-1 flex-col">
           <OverlayUI
