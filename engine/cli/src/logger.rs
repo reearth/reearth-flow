6eb339eaddd873402ec5ef9b4f52db4ff0dbf9fb
Yuji Mise <miseyu2000@gmail.com>
2025-02-19 16:38:42 +0900

    feat(engine): add json-subscriber dependency and update logging configuration

--- engine/cli/src/logger.rs
+++ engine/cli/src/logger.rs
@@ -1,27 +1,61 @@
+use std::env;
+
+use once_cell::sync::Lazy;
 use tracing::Level;
 use tracing_subscriber::fmt::time::UtcTime;
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::EnvFilter;
 
-pub fn setup_logging_and_tracing(level: Level, ansi_colors: bool) {
+static ENABLE_JSON_LOG: Lazy<bool> = Lazy::new(|| {
+    env::var("FLOW_CLI_ENABLE_JSON_LOG")
+        .ok()
+        .map(|s| s.to_lowercase() == "true")
+        .unwrap_or(false)
+});
+
+pub fn setup_logging_and_tracing() -> crate::errors::Result<()> {
+    let log_level = env::var("RUST_LOG")
+        .ok()
+        .and_then(|s| s.parse::<Level>().ok())
+        .unwrap_or(Level::INFO);
     let env_filter = EnvFilter::builder()
-        .with_default_directive(level.into())
+        .with_default_directive(log_level.into())
         .from_env_lossy()
-        .add_directive("opendal=error".parse().unwrap());
-    let registry = tracing_subscriber::registry().with(env_filter);
-    let event_format = tracing_subscriber::fmt::format()
-        .with_target(true)
-        .with_timer(UtcTime::new(
-            time::format_description::parse(
-                "[year]-[month]-[day]T[hour]:[minute]:[second].[subsecond digits:3]Z",
-            )
-            .expect("Time format invalid."),
-        ));
-    let _ = registry
-        .with(
-            tracing_subscriber::fmt::layer()
-                .event_format(event_format)
-                .with_ansi(ansi_colors),
+        .add_directive(
+            "opendal=error"
+                .parse()
+                .map_err(crate::errors::Error::init)?,
+        );
+    let time_format = UtcTime::new(
+        time::format_description::parse(
+            "[year]-[month]-[day]T[hour]:[minute]:[second].[subsecond digits:3]Z",
         )
-        .try_init();
+        .map_err(crate::errors::Error::init)?,
+    );
+    if *ENABLE_JSON_LOG {
+        let mut layer = json_subscriber::JsonLayer::stdout();
+        layer.with_flattened_event();
+        layer.with_level("severity");
+        layer.with_current_span("span");
+        layer.with_timer("time", time_format);
+        tracing_subscriber::registry()
+            .with(env_filter)
+            .with(layer)
+            .try_init()
+            .map_err(crate::errors::Error::init)
+    } else {
+        let event_format = tracing_subscriber::fmt::format()
+            .with_target(true)
+            .with_timer(time_format);
+
+        tracing_subscriber::registry()
+            .with(env_filter)
+            .with(
+                tracing_subscriber::fmt::layer()
+                    .event_format(event_format)
+                    .with_ansi(true),
+            )
+            .try_init()
+            .map_err(crate::errors::Error::init)
+    }
 }
