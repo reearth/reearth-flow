6cfb6bb861820bf1aa3eb11f107a4fa2bd9bc055
Bill Cook <billcook8122@gmail.com>
2025-02-26 15:52:18 +0900

    fix(ui): correct green added

--- ui/src/features/Editor/components/RightPanel/components/VersionHistory/Version.tsx
+++ ui/src/features/Editor/components/RightPanel/components/VersionHistory/Version.tsx
@@ -24,7 +24,7 @@ const Version: React.FC<VersionProps> = ({
   return (
     <div key={version.id}>
       <div
-        className={`flex cursor-pointer select-none justify-between gap-2 rounded px-4 py-2 ${isSelected ? "relative before:absolute before:inset-y-0 before:left-0 before:right-1 before:-z-10 before:rounded-md before:border before:border-l-2 before:border-accent before:border-l-green-800/70 before:bg-primary before:opacity-100" : "hover:bg-primary"}`}
+        className={`flex cursor-pointer select-none justify-between gap-2 rounded px-4 py-2 ${isSelected ? "relative before:absolute before:inset-y-0 before:left-0 before:right-1 before:-z-10 before:rounded-md before:border before:border-l-2 before:border-accent before:border-l-[#00a340]/30 before:bg-primary before:opacity-100" : "hover:bg-primary"}`}
         onClick={onClick}
         onDoubleClick={onDoubleClick}
         style={{ height: "100%" }}>
