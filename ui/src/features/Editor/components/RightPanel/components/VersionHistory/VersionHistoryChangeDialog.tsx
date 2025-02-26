54ef4a21395fdf3f51f57acf12310427f6e9798e
Bill Cook <billcook8122@gmail.com>
2025-02-26 16:31:32 +0900

    fix(ui): corrected translations and removed old dialog

--- ui/src/features/Editor/components/RightPanel/components/VersionHistory/VersionHistoryChangeDialog.tsx
+++ ui/src/features/Editor/components/RightPanel/components/VersionHistory/VersionHistoryChangeDialog.tsx
@@ -13,9 +13,9 @@ const VersionHistoryChangeDialog: React.FC<Props> = ({
   const t = useT();
   return (
     <ConfirmationDialog
-      title={t("Are you sure you want to revert to Version?")}
+      title={t("Are you sure you want to revert to this version?")}
       description={t(
-        "By clicking continue you will be reverting back to Version {{version}}.",
+        "By clicking continue you will be reverting to version {{version}}.",
         { version: selectedVersion },
       )}
       isOpen={!!selectedVersion}
