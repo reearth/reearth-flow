2cf4f2d20e4b3c934fb52a5ad9f2eda6df49fb04
Bill Cook <billcook8122@gmail.com>
2025-02-26 11:14:47 +0900

    fix(ui): Change Dialog added to version history

--- ui/src/features/Editor/components/RightPanel/index.tsx
+++ ui/src/features/Editor/components/RightPanel/index.tsx
@@ -1,27 +1,54 @@
 import { LetterCircleV, X } from "@phosphor-icons/react";
-import { Fragment, memo } from "react";
+import { Fragment, memo, useState } from "react";
 
 import { IconButton, ScrollArea } from "@flow/components";
 import { useT } from "@flow/lib/i18n";
 import { mockVersionHistory } from "@flow/mock_data/versionHistoryData";
 import { formatDate } from "@flow/utils";
 
+import { VersionHistoryChangeDialog } from "./VersionHistoryChangeDialog";
+
 type Props = {
   contentType?: "version-history";
   onClose: () => void;
 };
 
+type VersionProps = {
+  version: {
+    id: string;
+    version: string;
+    createdAt: string;
+    isCurrent?: boolean;
+  };
+  isSelected: boolean;
+  onClick: () => void;
+  onDoubleClick: () => void;
+};
+
 const RightPanel: React.FC<Props> = ({ contentType, onClose }) => {
   const t = useT();
+  const [selectedVersionId, setSelectedVersionId] = useState<string | null>(
+    null,
+  );
+
+  const [openVersionChangeDialog, setOpenVersionChangeDialog] = useState(false);
 
   const versionHistory = [...mockVersionHistory];
 
   const currentVersion = versionHistory.shift();
 
+  const handleVersionClick = (id: string) => {
+    setSelectedVersionId(id);
+  };
+
+  const handleDoubleClick = () => {
+    setOpenVersionChangeDialog(true);
+  };
+
   return (
     <div
       id="right-panel"
-      className="fixed right-0 flex h-full w-[300px] flex-col border-l bg-background transition-all"
+      className="fixed right-0 z-10 flex h-full w-[300px] flex-col border-l bg-background transition-all"
       style={{
         transform: `translateX(${contentType ? "0" : "100%"})`,
         transitionDuration: contentType ? "500ms" : "300ms",
@@ -36,14 +63,14 @@ const RightPanel: React.FC<Props> = ({ contentType, onClose }) => {
         />
         <div className="flex items-center gap-1 p-2">
           <LetterCircleV weight="light" />
-          <p className="text-xs font-extralight">Version History</p>
+          <p className="text-xs font-extralight">{t("Version")}</p>
         </div>
       </div>
       {contentType === "version-history" && (
         <div className="flex h-full flex-col overflow-auto">
           <ScrollArea>
             {currentVersion && (
-              <div className="m-1 flex items-center justify-between rounded bg-primary p-1">
+              <div className="flex items-center justify-between rounded bg-primary p-1">
                 <div>
                   <p className="text-sm font-light">{t("Current Version")}</p>
                   <p className="flex-[2] text-xs font-thin">
@@ -57,26 +84,54 @@ const RightPanel: React.FC<Props> = ({ contentType, onClose }) => {
               </div>
             )}
             {versionHistory.map((history) => (
-              <Fragment key={history.id}>
-                <div className="m-1 flex cursor-pointer justify-between gap-2 rounded px-1 py-2 hover:bg-primary">
-                  <p className="flex-[2] text-xs font-thin">
-                    {formatDate(history.createdAt)}
-                  </p>
-                  <div className="flex justify-end">
-                    <p className="text-xs font-thin">
-                      {t("Version ")}
-                      <span className="font-light">{history.version}</span>
-                    </p>
-                  </div>
-                </div>
-                <div className="h-px bg-primary" />
-              </Fragment>
+              <Version
+                key={history.id}
+                version={history}
+                isSelected={history.id === selectedVersionId}
+                onClick={() => handleVersionClick(history.id)}
+                onDoubleClick={handleDoubleClick}
+              />
             ))}
           </ScrollArea>
         </div>
+      )}
+      {openVersionChangeDialog && selectedVersionId && (
+        <VersionHistoryChangeDialog
+          selectedVersion={selectedVersionId}
+          onDialogClose={() => setOpenVersionChangeDialog(false)}
+        />
       )}
     </div>
   );
 };
 
 export default memo(RightPanel);
+
+const Version: React.FC<VersionProps> = ({
+  version,
+  isSelected,
+  onClick,
+  onDoubleClick,
+}) => {
+  const t = useT();
+
+  return (
+    <Fragment key={version.id}>
+      <div
+        className={`flex cursor-pointer justify-between gap-2 rounded px-1 py-2 hover:bg-primary ${isSelected ? "bg-primary before:rounded-md before:border before:border-l-2 before:border-accent before:border-l-green-800/70 before:bg-primary before:opacity-100" : ""}`}
+        onClick={onClick}
+        onDoubleClick={onDoubleClick}>
+        <p className="flex-[2] text-xs font-thin">
+          {formatDate(version.createdAt)}
+        </p>
+        <div className="flex justify-end">
+          <p className="text-xs font-thin">
+            {t("Version ")}
+            <span className="font-light">{version.version}</span>
+          </p>
+        </div>
+      </div>
+      <div className="h-px bg-primary" />
+    </Fragment>
+  );
+};
