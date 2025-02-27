d9fcb05cb567990ca157e3758d0da04e16f16462
Piyush Chauhan <42397980+pyshx@users.noreply.github.com>
2025-02-28 01:25:30 +0530

    chore(api): save sharing token instead sharing url with project (#940)
    * chore(api): save sharing token instead sharing url with project
    
    * fix test

--- server/api/internal/adapter/gql/gqlmodel/convert_project.go
+++ server/api/internal/adapter/gql/gqlmodel/convert_project.go
@@ -9,9 +9,9 @@ func ToProject(p *project.Project) *Project {
 		return nil
 	}
 
-	var sharedURL *string
-	if p.SharedURL() != nil {
-		sharedURL = p.SharedURL()
+	var sharedToken *string
+	if p.SharedToken() != nil {
+		sharedToken = p.SharedToken()
 	}
 
 	return &Project{
@@ -23,7 +23,7 @@ func ToProject(p *project.Project) *Project {
 		BasicAuthPassword: p.BasicAuthPassword(),
 		Name:              p.Name(),
 		Description:       p.Description(),
-		SharedURL:         sharedURL,
+		SharedToken:       sharedToken,
 		UpdatedAt:         p.UpdatedAt(),
 		WorkspaceID:       IDFrom(p.Workspace()),
 	}
