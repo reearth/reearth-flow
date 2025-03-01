999866a161ddebca2b2b82e13a6ebcaaed85d47a
Bill Cook <102199454+billcookie@users.noreply.github.com>
2025-03-02 08:18:18 +0900

    fix(ui): Job Details Update (#902)
    * fix(ui): added job cancel mutation and logs for details
    
    * fix(ui): logs type updates
    
    * fix(ui): new translations
    
    * fix(ui): changed columnId from ts to timeStamp
    
    * fix(ui): ja translations added
    
    * fix(ui): added cancelled status
    
    * fix(ui): removed logs url from job details
    
    * fix(ui): removed translations for logs url
    
    * fix(ui): type change for outputUrls
    
    * fix(ui): generic JSONL parser added
    
    * fix(ui): deployments run button and no logs placeholder
    
    * fix(ui): added timestamp formating
    
    * fix(ui): gql updated and
    
    * improve styles
    
    * regen i18n
    
    * fixes
    
    ---------
    
    Co-authored-by: KaWaite <kyle.waite@outlook.com>

--- ui/src/features/WorkspaceTriggers/components/TriggerAddDialog.tsx
+++ ui/src/features/WorkspaceTriggers/components/TriggerAddDialog.tsx
@@ -149,14 +149,14 @@ const TriggerAddDialog: React.FC<Props> = ({ setShowDialog }) => {
           <DialogContentSection className="flex flex-col">
             <Label>{t("Deployment: ")}</Label>
             <div
-              className="flex h-8 w-full rounded-md border bg-transparent px-3 py-1 text-sm"
+              className="flex min-h-8 w-full items-center rounded-md border bg-transparent px-3 py-1 text-sm"
               onClick={() => setOpenSelectDeploymentsDialog(true)}>
               <span className="cursor-default whitespace-nowrap pr-2 text-muted-foreground">
                 {t("Select Deployment: ")}
               </span>
               {selectedDeployment ? (
-                <span className="cursor-default truncate">
-                  {selectedDeployment.description} @{selectedDeployment.version}
+                <span className="cursor-default">
+                  {selectedDeployment.description}@{selectedDeployment.version}
                 </span>
               ) : (
                 <span className="cursor-default">
