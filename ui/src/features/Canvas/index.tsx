ed66d17f5886d29fcd6b6e015e02d46cc589330b
Bill Cook <billcook8122@gmail.com>
2025-03-14 16:01:24 +0900

    fix(ui): context menu and node pickers added for canvas

--- ui/src/features/Canvas/index.tsx
+++ ui/src/features/Canvas/index.tsx
@@ -1,3 +1,4 @@
+import { Database, Disc, Lightning } from "@phosphor-icons/react";
 import {
   ReactFlow,
   Background,
@@ -11,6 +12,12 @@ import {
 } from "@xyflow/react";
 import { MouseEvent, memo } from "react";
 
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@flow/components";
 import {
   isValidConnection,
   CustomConnectionLine,
@@ -84,70 +91,102 @@ const Canvas: React.FC<Props> = ({
     onEdgesChange,
     onNodePickerOpen,
   });
-
   return (
-    <ReactFlow
-      // minZoom={0.7}
-      // maxZoom={1}
-      // defaultViewport={{ zoom: 0.8, x: 200, y: 200 }}
-      // nodeDragThreshold={60}
-      // translateExtent={[
-      //   [-1000, -1000],
-      //   [1000, 1000],
-      // ]}
-      // onInit={setReactFlowInstance}
-      // selectNodesOnDrag={false}
-      // fitViewOptions={{ padding: 0.5 }}
-      // fitView
+    <ContextMenu>
+      <ContextMenuTrigger asChild>
+        <ReactFlow
+          // minZoom={0.7}
+          // maxZoom={1}
+          // defaultViewport={{ zoom: 0.8, x: 200, y: 200 }}
+          // nodeDragThreshold={60}
+          // translateExtent={[
+          //   [-1000, -1000],
+          //   [1000, 1000],
+          // ]}
+          // onInit={setReactFlowInstance}
+          // selectNodesOnDrag={false}
+          // fitViewOptions={{ padding: 0.5 }}
+          // fitView
 
-      // Locking props START
-      nodesDraggable={!canvasLock}
-      nodesConnectable={!canvasLock}
-      nodesFocusable={!canvasLock}
-      edgesFocusable={!canvasLock}
-      // elementsSelectable={!canvasLock}
-      autoPanOnConnect={!canvasLock}
-      autoPanOnNodeDrag={!canvasLock}
-      // panOnDrag={!canvasLock}
-      selectionOnDrag={!canvasLock}
-      // panOnScroll={!canvasLock}
-      // zoomOnScroll={!canvasLock}
-      // zoomOnPinch={!canvasLock}
-      // zoomOnDoubleClick={!canvasLock}
-      connectOnClick={!canvasLock}
-      // Locking props END
+          // Locking props START
+          nodesDraggable={!canvasLock}
+          nodesConnectable={!canvasLock}
+          nodesFocusable={!canvasLock}
+          edgesFocusable={!canvasLock}
+          // elementsSelectable={!canvasLock}
+          autoPanOnConnect={!canvasLock}
+          autoPanOnNodeDrag={!canvasLock}
+          // panOnDrag={!canvasLock}
+          selectionOnDrag={!canvasLock}
+          // panOnScroll={!canvasLock}
+          // zoomOnScroll={!canvasLock}
+          // zoomOnPinch={!canvasLock}
+          // zoomOnDoubleClick={!canvasLock}
+          connectOnClick={!canvasLock}
+          // Locking props END
 
-      snapGrid={snapGrid}
-      selectionMode={SelectionMode["Partial"]}
-      nodes={nodes}
-      nodeTypes={nodeTypes}
-      edges={edges}
-      edgeTypes={edgeTypes}
-      defaultEdgeOptions={defaultEdgeOptions}
-      connectionLineComponent={CustomConnectionLine}
-      connectionLineStyle={connectionLineStyle}
-      isValidConnection={isValidConnection}
-      onNodesChange={handleNodesChange}
-      onEdgesChange={handleEdgesChange}
-      onNodeDoubleClick={handleNodeDoubleClick}
-      onNodeDragStop={handleNodeDragStop}
-      onNodesDelete={handleNodesDelete}
-      onNodeMouseEnter={onNodeHover}
-      onNodeMouseLeave={onNodeHover}
-      onDrop={handleNodeDrop}
-      onDragOver={handleNodeDragOver}
-      onEdgeMouseEnter={onEdgeHover}
-      onEdgeMouseLeave={onEdgeHover}
-      onConnect={handleConnect}
-      onReconnect={handleReconnect}
-      proOptions={proOptions}>
-      <Background
-        className="bg-background"
-        variant={BackgroundVariant["Lines"]}
-        gap={gridSize}
-        color="rgba(63, 63, 70, 0.3)"
-      />
-    </ReactFlow>
+          snapGrid={snapGrid}
+          selectionMode={SelectionMode["Partial"]}
+          nodes={nodes}
+          nodeTypes={nodeTypes}
+          edges={edges}
+          edgeTypes={edgeTypes}
+          defaultEdgeOptions={defaultEdgeOptions}
+          connectionLineComponent={CustomConnectionLine}
+          connectionLineStyle={connectionLineStyle}
+          isValidConnection={isValidConnection}
+          onNodesChange={handleNodesChange}
+          onEdgesChange={handleEdgesChange}
+          onNodeDoubleClick={handleNodeDoubleClick}
+          onNodeDragStop={handleNodeDragStop}
+          onNodesDelete={handleNodesDelete}
+          onNodeMouseEnter={onNodeHover}
+          onNodeMouseLeave={onNodeHover}
+          onDrop={handleNodeDrop}
+          onDragOver={handleNodeDragOver}
+          onEdgeMouseEnter={onEdgeHover}
+          onEdgeMouseLeave={onEdgeHover}
+          onConnect={handleConnect}
+          onReconnect={handleReconnect}
+          proOptions={proOptions}>
+          <Background
+            className="bg-background"
+            variant={BackgroundVariant["Lines"]}
+            gap={gridSize}
+            color="rgba(63, 63, 70, 0.3)"
+          />
+        </ReactFlow>
+      </ContextMenuTrigger>
+      <ContextMenuContent>
+        {!nodes.some((node) => node.type === "reader") && (
+          <ContextMenuItem
+            className="justify-between gap-4 text-xs"
+            onClick={(e) =>
+              onNodePickerOpen &&
+              onNodePickerOpen({ x: e.clientX, y: e.clientY }, "reader")
+            }>
+            <Database weight="light" /> Add Reader
+          </ContextMenuItem>
+        )}
+        <ContextMenuItem
+          className="justify-between gap-4 text-xs"
+          onClick={(e) =>
+            onNodePickerOpen &&
+            onNodePickerOpen({ x: e.clientX, y: e.clientY }, "transformer")
+          }>
+          <Lightning weight="light" /> Add Transformer
+        </ContextMenuItem>
+        <ContextMenuItem
+          className="justify-between gap-4 text-xs"
+          onClick={(e) =>
+            onNodePickerOpen &&
+            onNodePickerOpen({ x: e.clientX, y: e.clientY }, "writer")
+          }>
+          <Disc weight="light" />
+          Add Writer
+        </ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
   );
 };
 
