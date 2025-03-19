9b69508ebb6e3b15b8e9687a84af83e31dddccf4
xy <xyzmhx@gmail.com>
2025-03-20 03:22:09 +0900

    fix(broadcast): adjust timeout value in BroadcastGroup to improve connection handling

--- server/websocket/src/broadcast/group.rs
+++ server/websocket/src/broadcast/group.rs
@@ -281,7 +281,7 @@ impl BroadcastGroup {
                         &doc_name_for_sub,
                         &group_name_clone,
                         &consumer_name_clone,
-                        15,
+                        12,
                     )
                     .await
                 {
