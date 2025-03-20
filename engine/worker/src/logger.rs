93c7450dc1e1d4d9792de6ed7bf9f6283cd041b7
soma00333 <soma03432303@gmail.com>
2025-03-20 18:26:41 +0900

    Add tracing-appender and enhance logging functionality

--- engine/worker/src/logger.rs
+++ engine/worker/src/logger.rs
@@ -1,7 +1,14 @@
-use std::env;
+use std::{
+    env,
+    fs::OpenOptions,
+    io::{self, Write},
+    sync::RwLock,
+};
 
+use crate::errors::Error;
 use once_cell::sync::Lazy;
 use tracing::Level;
+use tracing_appender::non_blocking::NonBlocking;
 use tracing_subscriber::fmt::time::UtcTime;
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::EnvFilter;
@@ -12,6 +19,22 @@ static ENABLE_JSON_LOG: Lazy<bool> = Lazy::new(|| {
         .map(|s| s.to_lowercase() == "true")
         .unwrap_or(false)
 });
+
+static WORKER_FILE_WRITER: Lazy<RwLock<Option<NonBlocking>>> = Lazy::new(|| RwLock::new(None));
+
+#[derive(Clone)]
+struct DynamicFileWriter;
+impl<'a> tracing_subscriber::fmt::MakeWriter<'a> for DynamicFileWriter {
+    type Writer = Box<dyn Write + Send + 'a>;
+
+    fn make_writer(&'a self) -> Self::Writer {
+        if let Some(ref nb) = *WORKER_FILE_WRITER.read().unwrap() {
+            Box::new(nb.make_writer())
+        } else {
+            Box::new(io::sink())
+        }
+    }
+}
 
 pub fn setup_logging_and_tracing() -> crate::errors::Result<()> {
     let log_level = env::var("RUST_LOG")
@@ -28,30 +51,70 @@ pub fn setup_logging_and_tracing() -> crate::errors::Result<()> {
         )
         .map_err(crate::errors::Error::init)?,
     );
+    let registry = tracing_subscriber::registry();
     if *ENABLE_JSON_LOG {
-        let mut layer = json_subscriber::JsonLayer::stdout();
-        layer.with_flattened_event();
-        layer.with_level("severity");
-        layer.with_current_span("span");
-        layer.with_timer("time", time_format);
-        tracing_subscriber::registry()
+        let mut console_layer = json_subscriber::JsonLayer::stdout();
+        console_layer.with_flattened_event();
+        console_layer.with_level("severity");
+        console_layer.with_current_span("span");
+        console_layer.with_timer("time", time_format.clone());
+
+        let mut file_layer = json_subscriber::JsonLayer::new(DynamicFileWriter);
+        file_layer.with_flattened_event();
+        file_layer.with_level("severity");
+        file_layer.with_current_span("span");
+        file_layer.with_timer("time", time_format);
+
+        registry
             .with(env_filter)
-            .with(layer)
+            .with(console_layer)
+            .with(file_layer)
             .try_init()
-            .map_err(crate::errors::Error::init)
+            .map_err(Error::init)
     } else {
         let event_format = tracing_subscriber::fmt::format()
             .with_target(true)
-            .with_timer(time_format);
+            .with_timer(time_format.clone());
 
-        tracing_subscriber::registry()
+        let console_layer = tracing_subscriber::fmt::layer()
+            .event_format(event_format)
+            .with_ansi(true)
+            .with_writer(std::io::stdout);
+
+        let file_layer = tracing_subscriber::fmt::layer()
+            .with_timer(time_format)
+            .with_ansi(false)
+            .with_writer(DynamicFileWriter);
+
+        registry
             .with(env_filter)
-            .with(
-                tracing_subscriber::fmt::layer()
-                    .event_format(event_format)
-                    .with_ansi(true),
-            )
+            .with(console_layer)
+            .with(file_layer)
             .try_init()
-            .map_err(crate::errors::Error::init)
+            .map_err(Error::init)
     }
+}
+
+pub fn enable_file_logging(job_id: uuid::Uuid) -> crate::errors::Result<()> {
+    let worker_uri = reearth_flow_common::dir::setup_job_directory("workers", "worker", job_id)
+        .map_err(Error::init)?;
+    let path_ref = worker_uri.path();
+    let worker_path = path_ref.as_path();
+    std::fs::create_dir_all(worker_path)
+        .map_err(|e| Error::init(format!("Failed to create worker dir: {e}")))?;
+    let log_path = worker_path.join("worker.log");
+    let file = std::fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(&log_path)
+        .map_err(Error::init)?;
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file);
+
+    {
+        let mut writer_guard = WORKER_FILE_WRITER.write().unwrap();
+        writer_guard.replace(non_blocking);
+    }
+
+    tracing::info!("File logging enabled: {}", log_path.to_string_lossy());
+    Ok(())
 }
