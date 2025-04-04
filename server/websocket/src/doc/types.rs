f6614a2010e838591edad4a9228d7a1366f278db
xy <xyzmhx@gmail.com>
2025-04-05 01:48:31 +0900

    refactor(types): streamline timestamp handling in HistoryItem and Document structs

--- server/websocket/src/doc/types.rs
+++ server/websocket/src/doc/types.rs
@@ -1,21 +1,21 @@
+use crate::storage::gcs::UpdateInfo;
 use chrono::Utc;
 use serde::{Deserialize, Serialize};
 use yrs::updates::encoder::Encode;
-use crate::storage::gcs::UpdateInfo;
 
 #[derive(Debug, Clone)]
 pub struct Document {
     pub id: String,
     pub updates: Vec<u8>,
     pub version: u64,
-    pub timestamp: chrono::DateTime<chrono::Utc>,
+    pub timestamp: chrono::DateTime<Utc>,
 }
 
 #[derive(Debug, Clone)]
 pub struct HistoryItem {
     pub version: u64,
     pub updates: Vec<u8>,
-    pub timestamp: chrono::DateTime<chrono::Utc>,
+    pub timestamp: chrono::DateTime<Utc>,
 }
 
 impl From<UpdateInfo> for HistoryItem {
@@ -23,11 +23,8 @@ impl From<UpdateInfo> for HistoryItem {
         HistoryItem {
             version: info.clock as u64,
             updates: info.update.encode_v1(),
-            timestamp: chrono::DateTime::from_timestamp(
-                info.timestamp.unix_timestamp(),
-                0,
-            )
-            .unwrap_or(Utc::now()),
+            timestamp: chrono::DateTime::from_timestamp(info.timestamp.unix_timestamp(), 0)
+                .unwrap_or(Utc::now()),
         }
     }
 }
