4e2d8e95996d24b3fd0d2c7039a62fd35a5193b1
Bill Cook <billcook8122@gmail.com>
2025-04-08 14:11:26 +0900

    fix(ui): changed name from clipboard refresh to expiration time

--- ui/src/global-constants.ts
+++ ui/src/global-constants.ts
@@ -4,4 +4,4 @@ export const DEFAULT_ROUTING_PORT = "default";
 export const DEFAULT_NODE_SIZE = { width: 150, height: 25 };
 export const ALLOWED_WORKFLOW_FILE_EXTENSIONS = ".json, .yaml, .yml";
 export const ALLOWED_PROJECT_IMPORT_EXTENSIONS = ".zip";
-export const CLIPBOARD_REFRESH = 1000 * 60 * 5;
+export const CLIPBOARD_EXPIRATION_TIME = 1000 * 60 * 5;
