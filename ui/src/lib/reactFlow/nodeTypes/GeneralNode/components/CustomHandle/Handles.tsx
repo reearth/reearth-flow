6d95f16c19e952e607c1b43e3f9ca7b95192f752
KaWaite <kyle.waite@outlook.com>
2025-05-22 13:59:19 +0900

    General node ui overhaul

--- ui/src/lib/reactFlow/nodeTypes/GeneralNode/components/CustomHandle/Handles.tsx
+++ ui/src/lib/reactFlow/nodeTypes/GeneralNode/components/CustomHandle/Handles.tsx
@@ -1,4 +1,3 @@
-import { CaretRight } from "@phosphor-icons/react";
 import { Position } from "@xyflow/react";
 import { memo } from "react";
 
@@ -13,38 +12,12 @@ type Props = {
 const Handles: React.FC<Props> = ({ nodeType, inputs, outputs }) => {
   return (
     <div className="flex justify-between">
-      {nodeType !== "reader" && inputs && inputs.length === 1 && (
-        <div className="relative flex-1 flex gap-0.5 items-center">
-          <CustomHandle
-            id={inputs[0]}
-            className="left-2 z-1001 w-[16px] rounded-l rounded-r-none"
-            type="target"
-            position={Position.Left}
-          />
-          <div className="flex items-center translate-x-0.5">
-            <div className="size-1.5 bg-gray-300 rounded-full" />
-            <p className="pl-1 text-[10px] dark:font-thin">{inputs[0]}</p>
-          </div>
-        </div>
-      )}
-      {outputs && outputs.length === 1 && (
-        <div>
-          <CustomHandle
-            id={outputs[0]}
-            className="right-2 z-1001 w-[16px] rounded-l-none rounded-r"
-            type="source"
-            position={Position.Right}
-          />
-          <CaretRight className="relative" weight="fill" size={10} />
-        </div>
-      )}
-      <div className="inset-x-0 mx-auto flex-1">
-        {inputs &&
-          inputs.length > 1 &&
-          inputs.map((input, index) => (
+      {nodeType !== "reader" && inputs && (
+        <div className="inset-x-0 mx-auto flex-1">
+          {inputs.map((input, index) => (
             <div
               key={input + index}
-              className="relative border-b px-1.5 py-0.5">
+              className="relative border-b py-0.5 flex items-center last-of-type:border-none">
               <CustomHandle
                 type="target"
                 className={`left-1 w-[8px] rounded-none transition-colors ${index === (!outputs && inputs && inputs.length - 1) ? "rounded-bl-sm" : undefined}`}
@@ -52,30 +25,38 @@ const Handles: React.FC<Props> = ({ nodeType, inputs, outputs }) => {
                 id={input}
                 // isConnectable={1}
               />
-              <p className="pl-1 text-[10px] dark:font-thin">{input}</p>
+              <div className="flex items-center translate-x-0.5">
+                <div className="size-1.5 bg-gray-300 rounded-full" />
+                <p className="pl-1 text-[10px] dark:font-thin italic">
+                  {input}
+                </p>
+              </div>
             </div>
           ))}
-        {outputs &&
-          outputs.length > 1 &&
-          outputs.map((output, index) => (
+        </div>
+      )}
+      {outputs && (
+        <div className="inset-x-0 mx-auto flex-1">
+          {outputs.map((output, index) => (
             <div
               key={output + index}
               className="relative flex justify-end items-center border-b py-0.5 last-of-type:border-none">
               <CustomHandle
                 type="source"
-                className="w-[10px] right-1 rounded-none transition-colors z-10"
+                className="w-[8px] right-1 rounded-none transition-colors z-10"
                 position={Position.Right}
                 id={output}
               />
               <div className="flex items-center -translate-x-0.5">
-                <p className="pr-1 text-end text-[10px] dark:font-thin">
+                <p className="pr-1 text-end text-[10px] dark:font-thin italic">
                   {output}
                 </p>
                 <div className="size-1.5 bg-gray-300 rounded-full" />
               </div>
             </div>
           ))}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
