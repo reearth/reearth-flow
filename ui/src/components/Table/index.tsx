cd3fcf058f700e29b2fe78ec75e4f75c4a674549
Bill Cook <102199454+billcookie@users.noreply.github.com>
2025-06-06 11:19:24 +0900

    fix(ui): added virtualizer for data table [FLOW-FE-180] (#1210)
    * fix(ui): added virtualizer and removed div from table component
    
    * fix(ui): border issue and resize fix for data table virtualized items
    
    * fix(ui): added a comment about extra border using ::after pseudo-element
    
    * fix(ui): changed fixed height of 500px to 100%

--- ui/src/components/Table/index.tsx
+++ ui/src/components/Table/index.tsx
@@ -6,13 +6,11 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="relative w-full overflow-auto">
-    <table
-      ref={ref}
-      className={cn("w-full caption-bottom text-xs overflow-auto", className)}
-      {...props}
-    />
-  </div>
+  <table
+    ref={ref}
+    className={cn("w-full caption-bottom text-xs overflow-auto", className)}
+    {...props}
+  />
 ));
 Table.displayName = "Table";
 
