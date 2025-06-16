a259606a514ed461bf394b12e62d308aa41a0e73
Bill Cook <billcook8122@gmail.com>
2025-06-16 15:31:49 +0900

    fix(ui): dropdown now matches context menu

--- ui/src/features/Editor/components/TopBar/components/ActionBar/index.tsx
+++ ui/src/features/Editor/components/TopBar/components/ActionBar/index.tsx
@@ -10,6 +10,7 @@ import { memo } from "react";
 import { Doc } from "yjs";
 
 import {
+  ContextMenuShortcut,
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
@@ -97,27 +98,39 @@ const ActionBar: React.FC<Props> = ({
               />
             </DropdownMenuTrigger>
             <DropdownMenuContent
-              className="flex flex-col gap-2"
+              className="min-w-[170px] select-none rounded-md bg-primary p-1 text-popover-foreground shadow-md"
               align="end"
               sideOffset={10}
               alignOffset={2}>
               <DropdownMenuItem
-                className="flex justify-between gap-4"
+                className="flex items-center justify-between rounded-sm px-2 py-1.5 text-xs"
                 onClick={onProjectExport}>
-                <p>{t("Export Project")}</p>
-                <Export weight="thin" size={18} />
+                <div className="flex items-center gap-1">
+                  <Export weight="light" />
+                  <p>{t("Export Project")}</p>
+                </div>
               </DropdownMenuItem>
               <DropdownMenuItem
-                className="flex justify-between gap-4"
+                className="flex items-center justify-between rounded-sm px-2 py-1.5 text-xs"
                 onClick={handleShowVersionDialog}>
-                <p>{t("Version History")}</p>
-                <ClockCounterClockwise weight="thin" size={18} />
+                <div className="flex items-center gap-1">
+                  <ClockCounterClockwise weight="light" />
+                  <p>{t("Version History")}</p>
+                </div>
               </DropdownMenuItem>
               <DropdownMenuItem
-                className="flex justify-between gap-4"
+                className="flex items-center justify-between rounded-sm px-2 py-1.5 text-xs"
                 onClick={handleProjectSnapshotSave}>
-                <p>{t("Manually Save")}</p>
-                <FloppyDiskIcon weight="thin" size={18} />
+                <div className="flex items-center gap-1">
+                  <FloppyDiskIcon weight="light" />
+                  <p>{t("Manual Save")}</p>
+                </div>
+
+                <div className="flex flex-row gap-1">
+                  <ContextMenuShortcut
+                    keyBinding={{ key: "s", commandKey: true }}
+                  />
+                </div>
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
