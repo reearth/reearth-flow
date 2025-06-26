722d2b4ad46005a2dd8403e24f0a019054ea26ce
Bill Cook <102199454+billcookie@users.noreply.github.com>
2025-06-26 11:46:58 +0900

    fix(ui): added a generic error page for general errors [FLOW-FE-212] (#1241)
    * fix(ui): added a generic error page for general errors
    
    * fix(ui): 404 added for all routes, custom errors, logo fix
    
    * fix(ui): error message improvement

--- ui/src/routes/index.tsx
+++ ui/src/routes/index.tsx
@@ -1,9 +1,11 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 import { LoadingSplashscreen } from "@flow/components";
+import ErrorPage from "@flow/components/errors/ErrorPage";
 
 export const Route = createFileRoute("/")({
   component: () => <LoadingSplashscreen />,
+  errorComponent: () => <ErrorPage />,
   loader: () => {
     throw redirect({ to: "/workspaces" });
   },
