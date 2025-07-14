3ef3f8a44f2ce544f22465cca4c89c129886f18a
Bill Cook <billcook8122@gmail.com>
2025-07-14 17:03:15 +0900

    chore(ui): tanstack router fix and update to latest version

--- ui/src/routes/index.tsx
+++ ui/src/routes/index.tsx
@@ -1,12 +1,22 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { useEffect } from "react";
 
 import { LoadingSplashscreen } from "@flow/components";
 import ErrorPage from "@flow/components/errors/ErrorPage";
+import { useAuth } from "@flow/lib/auth";
 
 export const Route = createFileRoute("/")({
-  component: () => <LoadingSplashscreen />,
+  component: Index,
   errorComponent: () => <ErrorPage />,
-  loader: () => {
-    throw redirect({ to: "/workspaces" });
-  },
 });
+
+function Index() {
+  const { isLoading } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLoading) return;
+    navigate({ to: "/workspaces", replace: true });
+  });
+  return <LoadingSplashscreen />;
+}
