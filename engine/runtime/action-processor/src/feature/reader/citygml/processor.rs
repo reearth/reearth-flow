6214d41c6d1727988462c38960b05c76f53cbc49
KaWaite <kyle.waite@outlook.com>
2025-08-05 11:38:30 +0900

    fix(reader): enhance error handling by adding original expression retention in compiled parameters

--- engine/runtime/action-processor/src/feature/reader/citygml/processor.rs
+++ engine/runtime/action-processor/src/feature/reader/citygml/processor.rs
@@ -71,10 +71,11 @@ impl ProcessorFactory for FeatureCityGmlReaderFactory {
             .into());
         };
         let expr_engine = Arc::clone(&ctx.expr_engine);
-        let params = CompiledFeatureCityGmlReaderParam {
+        let compiled_params = CompiledFeatureCityGmlReaderParam {
             dataset: expr_engine
                 .compile(params.dataset.as_ref())
                 .map_err(|e| FeatureProcessorError::FileCityGmlReaderFactory(format!("{e:?}")))?,
+            original_dataset: params.dataset.clone(),
             flatten: params.flatten,
         };
         let threads_num = {
@@ -91,7 +92,7 @@ impl ProcessorFactory for FeatureCityGmlReaderFactory {
             .unwrap();
         let process = FeatureCityGmlReader {
             global_params: with,
-            params,
+            params: compiled_params,
             join_handles: Vec::new(),
             thread_pool: Arc::new(parking_lot::Mutex::new(pool)),
         };
@@ -121,6 +122,7 @@ pub struct FeatureCityGmlReaderParam {
 #[derive(Debug, Clone)]
 struct CompiledFeatureCityGmlReaderParam {
     dataset: rhai::AST,
+    original_dataset: Expr,
     flatten: Option<bool>,
 }
 
@@ -139,6 +141,7 @@ impl Processor for FeatureCityGmlReader {
         let ctx = ctx.as_context();
         let global_params = self.global_params.clone();
         let dataset = self.params.dataset.clone();
+        let original_dataset = self.params.original_dataset.clone();
         let flatten = self.params.flatten;
         let pool = self.thread_pool.lock();
         let (tx, rx) = std::sync::mpsc::channel();
@@ -150,6 +153,7 @@ impl Processor for FeatureCityGmlReader {
                 fw,
                 feature,
                 dataset,
+                original_dataset,
                 flatten,
                 global_params.clone(),
             );
