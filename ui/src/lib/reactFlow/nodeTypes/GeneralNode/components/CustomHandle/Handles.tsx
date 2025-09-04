8e54f9e8f9c2fa71881a84ec52188e618dd1e266
Bill Cook <billcook8122@gmail.com>
2025-09-04 10:28:39 +0900

    fix(ui): add collapsible handles for nodes with many input/output

--- ui/src/lib/reactFlow/nodeTypes/GeneralNode/components/CustomHandle/Handles.tsx
+++ ui/src/lib/reactFlow/nodeTypes/GeneralNode/components/CustomHandle/Handles.tsx
@@ -1,5 +1,13 @@
+import { ChevronDownIcon, ChevronUpIcon } from "@radix-ui/react-icons";
 import { Position } from "@xyflow/react";
-import { memo } from "react";
+import { memo, useState } from "react";
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+  IconButton,
+} from "@flow/components";
 
 import CustomHandle from "./CustomHandle";
 
@@ -10,56 +18,120 @@ type Props = {
 };
 
 const Handles: React.FC<Props> = ({ nodeType, inputs, outputs }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="flex justify-between gap-0.5">
-      {nodeType !== "reader" && inputs && (
-        <div className="inset-x-0 mx-auto min-w-0 flex-1">
-          {inputs.map((input, index) => (
-            <div
-              key={input + index}
-              className="relative flex items-center border-b py-0.5 last-of-type:border-none">
-              <CustomHandle
-                type="target"
-                className={`left-1 w-[8px] rounded-none transition-colors ${index === (!outputs && inputs && inputs.length - 1) ? "rounded-bl-sm" : undefined}`}
-                position={Position.Left}
-                id={input}
-                // isConnectable={1}
-              />
-              <div className="flex w-full translate-x-0.5 items-center">
-                <div>
+    <Collapsible
+      className="flex flex-col"
+      open={isOpen}
+      onOpenChange={setIsOpen}>
+      <div className="flex justify-between gap-0.5">
+        {nodeType !== "reader" && inputs && (
+          <div className="inset-x-0 mx-auto min-w-0 flex-1">
+            {inputs.slice(0, 5).map((input, index) => (
+              <div
+                key={input + index}
+                className="relative flex items-center border-b py-0.5 last-of-type:border-none">
+                <CustomHandle
+                  type="target"
+                  className={`left-1 w-[8px] rounded-none transition-colors ${index === (!outputs && inputs && inputs.length - 1) ? "rounded-bl-sm" : undefined}`}
+                  position={Position.Left}
+                  id={input}
+                  // isConnectable={1}
+                />
+                <div className="flex w-full translate-x-0.5 items-center">
+                  <div>
+                    <div className="size-1.5 rounded-full bg-gray-300" />
+                  </div>
+                  <p className="w-[90%] pl-1 text-[10px] break-words italic dark:font-thin">
+                    {input}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+        {outputs && (
+          <div className="inset-x-0 mx-auto min-w-0 flex-1 overflow-hidden">
+            {outputs.slice(0, 5).map((output, index) => (
+              <div
+                key={output + index}
+                className="relative flex items-center justify-end border-b py-0.5 last-of-type:border-none">
+                <CustomHandle
+                  type="source"
+                  className="right-1 z-10 w-[8px] rounded-none transition-colors"
+                  position={Position.Right}
+                  id={output}
+                />
+                <div className="flex w-full -translate-x-0.5 items-center justify-end">
+                  <p className="w-[90%] pr-1 text-end text-[10px] break-words italic dark:font-thin">
+                    {output}
+                  </p>
                   <div className="size-1.5 rounded-full bg-gray-300" />
                 </div>
-                <p className="w-[90%] pl-1 text-[10px] break-words italic dark:font-thin">
-                  {input}
-                </p>
               </div>
-            </div>
-          ))}
-        </div>
-      )}
-      {outputs && (
-        <div className="inset-x-0 mx-auto min-w-0 flex-1 overflow-hidden">
-          {outputs.map((output, index) => (
-            <div
-              key={output + index}
-              className="relative flex items-center justify-end border-b py-0.5 last-of-type:border-none">
-              <CustomHandle
-                type="source"
-                className="right-1 z-10 w-[8px] rounded-none transition-colors"
-                position={Position.Right}
-                id={output}
-              />
-              <div className="flex w-full -translate-x-0.5 items-center justify-end">
-                <p className="w-[90%] pr-1 text-end text-[10px] break-words italic dark:font-thin">
-                  {output}
-                </p>
-                <div className="size-1.5 rounded-full bg-gray-300" />
+            ))}
+          </div>
+        )}
+      </div>
+      <CollapsibleContent className="justfiy-between flex gap-2">
+        {nodeType !== "reader" && inputs && (
+          <div className="inset-x-0 mx-auto min-w-0 flex-1">
+            {inputs.slice(5).map((input, index) => (
+              <div
+                key={input + index}
+                className="relative flex items-center border-b py-0.5 last-of-type:border-none">
+                <CustomHandle
+                  type="target"
+                  className={`left-1 w-[8px] rounded-none transition-colors ${index === (!outputs && inputs && inputs.length - 1) ? "rounded-bl-sm" : undefined}`}
+                  position={Position.Left}
+                  id={input}
+                  // isConnectable={1}
+                />
+                <div className="flex w-full translate-x-0.5 items-center">
+                  <div>
+                    <div className="size-1.5 rounded-full bg-gray-300" />
+                  </div>
+                  <p className="w-[90%] pl-1 text-[10px] break-words italic dark:font-thin">
+                    {input}
+                  </p>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
+        {outputs && (
+          <div className="inset-x-0 mx-auto min-w-0 flex-1 overflow-hidden">
+            {outputs.slice(5).map((output, index) => (
+              <div
+                key={output + index}
+                className="relative flex items-center justify-end border-b py-0.5 last-of-type:border-none">
+                <CustomHandle
+                  type="source"
+                  className="right-1 z-10 w-[8px] rounded-none transition-colors"
+                  position={Position.Right}
+                  id={output}
+                />
+                <div className="flex w-full -translate-x-0.5 items-center justify-end">
+                  <p className="w-[90%] pr-1 text-end text-[10px] break-words italic dark:font-thin">
+                    {output}
+                  </p>
+                  <div className="size-1.5 rounded-full bg-gray-300" />
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </CollapsibleContent>
+      {outputs && outputs?.length >= 6 && (
+        <CollapsibleTrigger asChild className="justify-center self-center">
+          <IconButton
+            className="h-6 w-6"
+            icon={isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+          />
+        </CollapsibleTrigger>
       )}
-    </div>
+    </Collapsible>
   );
 };
 
