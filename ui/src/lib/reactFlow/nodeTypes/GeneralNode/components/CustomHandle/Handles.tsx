cd491c027657f9bd0ce689099d982b2ba3ca4088
Bill Cook <billcook8122@gmail.com>
2025-09-09 15:15:35 +0900

    fix(ui): added support for inputs

--- ui/src/lib/reactFlow/nodeTypes/GeneralNode/components/CustomHandle/Handles.tsx
+++ ui/src/lib/reactFlow/nodeTypes/GeneralNode/components/CustomHandle/Handles.tsx
@@ -28,11 +28,12 @@ const Handles: React.FC<Props> = ({
   onCollapsedToggle,
 }) => {
   const t = useT();
+  const hasMoreThanFiveInputHandles = inputs && inputs.length >= 5;
   const hasMoreThanFiveOutputHandles = outputs && outputs.length >= 5;
   return (
     <Collapsible className="flex flex-col" open={!isCollapsed}>
       <div className="flex justify-between gap-0.5">
-        {nodeType !== "reader" && inputs && (
+        {nodeType !== "reader" && inputs && !hasMoreThanFiveInputHandles && (
           <div className="inset-x-0 mx-auto min-w-0 flex-1">
             {inputs.map((input, index) => (
               <div
@@ -57,6 +58,87 @@ const Handles: React.FC<Props> = ({
             ))}
           </div>
         )}
+        {nodeType !== "reader" &&
+          inputs &&
+          inputs.length >= 5 &&
+          isCollapsed && (
+            <div className="inset-x-0 min-w-0 flex-1 overflow-hidden">
+              <div className="relative flex items-center py-0.5">
+                <div className="left-1 z-10 w-[8px] rounded-none transition-colors" />
+                <div className="flex w-full translate-x-0.5 items-center">
+                  <div className="size-1.5 rounded-full bg-gray-300" />
+                  <p className="w-[90%] pl-1 text-[10px] break-words italic dark:font-thin">
+                    {t("Multiple")}
+                  </p>
+                </div>
+              </div>
+            </div>
+          )}
+
+        {outputs && outputs.length >= 5 && isCollapsed && (
+          <div className="inset-x-0 mx-auto min-w-0 flex-1 overflow-hidden">
+            <div className="relative flex items-center justify-end py-0.5 last-of-type:border-none">
+              <div className="right-1 z-10 w-[8px] rounded-none transition-colors" />
+              <div className="flex w-full -translate-x-0.5 items-center justify-end">
+                <p className="w-[90%] pr-1 text-end text-[10px] break-words italic dark:font-thin">
+                  {t("Multiple")}
+                </p>
+                <div className="size-1.5 rounded-full bg-gray-300" />
+              </div>
+            </div>
+          </div>
+        )}
+        <div className="max-w[50%]">
+          <CollapsibleContent>
+            {nodeType !== "reader" && inputs && hasMoreThanFiveInputHandles && (
+              <div className="inset-x-0 mx-auto min-w-0 flex-1">
+                {inputs.map((input, index) => (
+                  <div
+                    key={input + index}
+                    className="relative flex items-center border-b py-0.5 last-of-type:border-none">
+                    <CustomHandle
+                      type="target"
+                      className={`left-1 w-[8px] rounded-none transition-colors ${index === (!outputs && inputs && inputs.length - 1) ? "rounded-bl-sm" : undefined}`}
+                      position={Position.Left}
+                      id={input}
+                    />
+                    <div className="flex w-full translate-x-0.5 items-center">
+                      <div>
+                        <div className="size-1.5 rounded-full bg-gray-300" />
+                      </div>
+                      <p className="w-[90%] pl-1 text-[10px] break-words italic dark:font-thin">
+                        {input}
+                      </p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+            {outputs && hasMoreThanFiveOutputHandles && (
+              <div className="inset-x-0 mx-auto min-w-0 flex-1 overflow-hidden">
+                {outputs.map((output, index) => (
+                  <div
+                    key={output + index}
+                    className="relative flex items-center justify-end  border-b py-0.5 last-of-type:border-none">
+                    <CustomHandle
+                      type="source"
+                      className="right-1 z-10 w-[8px] rounded-none transition-colors"
+                      position={Position.Right}
+                      id={output}
+                    />
+                    <div className="flex w-full -translate-x-0.5 items-center justify-end">
+                      <p className="w-[90%] pr-1 text-end text-[10px] break-words italic dark:font-thin">
+                        {output}
+                      </p>
+                      <div className="size-1.5 rounded-full bg-gray-300" />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </CollapsibleContent>
+        </div>
+
         {outputs && !hasMoreThanFiveOutputHandles && (
           <div className="inset-x-0 mx-auto min-w-0 flex-1 overflow-hidden">
             {outputs.map((output, index) => (
@@ -79,63 +161,39 @@ const Handles: React.FC<Props> = ({
             ))}
           </div>
         )}
-        {outputs && outputs.length >= 5 && isCollapsed && (
-          <div className="inset-x-0 mx-auto min-w-0 flex-1 overflow-hidden">
-            <div className="relative flex items-center justify-end border-b py-0.5 last-of-type:border-none">
-              <div className="right-1 z-10 w-[8px] rounded-none transition-colors" />
-              <div className="flex w-full -translate-x-0.5 items-center justify-end">
-                <p className="w-[90%] pr-1 text-end text-[10px] break-words italic dark:font-thin">
-                  {t("Multiple")}
-                </p>
-                <div className="size-1.5 rounded-full bg-gray-300" />
-              </div>
-            </div>
-          </div>
-        )}
-        <CollapsibleContent>
-          <div className="inset-x-0 mx-auto min-w-0 flex-1" />
-          {outputs && hasMoreThanFiveOutputHandles && (
-            <div className="inset-x-0 mx-auto min-w-0 flex-1 overflow-hidden">
-              {outputs.map((output, index) => (
-                <div
-                  key={output + index}
-                  className="relative flex items-center justify-end  border-b py-0.5 last-of-type:border-none">
-                  <CustomHandle
-                    type="source"
-                    className="right-1 z-10 w-[8px] rounded-none transition-colors"
-                    position={Position.Right}
-                    id={output}
-                  />
-                  <div className="flex w-full -translate-x-0.5 items-center justify-end">
-                    <p className="w-[90%] pr-1 text-end text-[10px] break-words italic dark:font-thin">
-                      {output}
-                    </p>
-                    <div className="size-1.5 rounded-full bg-gray-300" />
-                  </div>
-                </div>
-              ))}
-            </div>
-          )}
-        </CollapsibleContent>
       </div>
       {isCollapsed && (
         <>
+          {nodeType !== "reader" &&
+            inputs &&
+            inputs.map((input, index) => (
+              <CustomHandle
+                key={`collapsed-input-${input}-${index}`}
+                type="target"
+                className="absolute left-1 z-10 w-[8px] rounded-none transition-colors"
+                position={Position.Left}
+                id={input}
+                isConnectable={1}
+                style={{ top: "44px" }}
+              />
+            ))}
           {outputs &&
             outputs.map((output, index) => (
               <CustomHandle
-                key={`collapsed-${output}-${index}`}
+                key={`collapsed-output-${output}-${index}`}
                 type="source"
                 className="absolute right-1 z-10 w-[8px] rounded-none transition-colors"
                 position={Position.Right}
                 id={output}
                 isConnectable={1}
-                style={{ top: "55%", transform: "translateY(-50%)" }}
+                style={{ top: "44px" }}
               />
             ))}
         </>
       )}
 
-      {outputs && outputs.length >= 5 && (
+      {((nodeType !== "reader" && inputs && inputs.length >= 5) ||
+        (outputs && outputs.length >= 5)) && (
         <CollapsibleTrigger asChild className="justify-center self-center">
           <IconButton
             onClick={() => onCollapsedToggle?.(!isCollapsed)}
