35ffdc4f649762d4d7a032417b75f0df29b9a547
pyshx <piyushchauhan1004@gmail.com>
2025-09-11 10:59:50 +0530

    simplify test_run function by removing unnecessary line breaks

--- engine/runtime/tests/testcase/file/reader/geopackage.rs
+++ engine/runtime/tests/testcase/file/reader/geopackage.rs
@@ -2,9 +2,6 @@ use crate::helper::execute;
 
 #[test]
 fn test_run() {
-    let result = execute(
-        "file/reader/geopackage",
-        vec!["test_geopackage.gpkg"],
-    );
+    let result = execute("file/reader/geopackage", vec!["test_geopackage.gpkg"]);
     assert!(result.is_ok());
 }
