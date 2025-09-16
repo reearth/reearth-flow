90c0db47b5251ebef870bf4c31726c3cbb446b53
akiyatomohiro <thairwars@gmail.com>
2025-09-16 19:55:26 +0800

    fix convert_workspace_test

--- server/api/internal/adapter/gql/gqlmodel/convert_workspace_test.go
+++ server/api/internal/adapter/gql/gqlmodel/convert_workspace_test.go
@@ -8,17 +8,17 @@ import (
 )
 
 func TestToRole(t *testing.T) {
-	assert.Equal(t, Role(RoleOwner), ToRole(workspace.RoleOwner))
-	assert.Equal(t, Role(RoleMaintainer), ToRole(workspace.RoleMaintainer))
-	assert.Equal(t, Role(RoleWriter), ToRole(workspace.RoleWriter))
-	assert.Equal(t, Role(RoleReader), ToRole(workspace.RoleReader))
-	assert.Equal(t, Role(""), ToRole(workspace.Role("unknown")))
+	assert.Equal(t, Role(RoleOwner), ToRoleFromReearthx(workspace.RoleOwner))
+	assert.Equal(t, Role(RoleMaintainer), ToRoleFromReearthx(workspace.RoleMaintainer))
+	assert.Equal(t, Role(RoleWriter), ToRoleFromReearthx(workspace.RoleWriter))
+	assert.Equal(t, Role(RoleReader), ToRoleFromReearthx(workspace.RoleReader))
+	assert.Equal(t, Role(""), ToRoleFromReearthx(workspace.Role("unknown")))
 }
 
 func TestFromRole(t *testing.T) {
-	assert.Equal(t, workspace.RoleOwner, FromRole(RoleOwner))
-	assert.Equal(t, workspace.RoleMaintainer, FromRole(RoleMaintainer))
-	assert.Equal(t, workspace.RoleWriter, FromRole(RoleWriter))
-	assert.Equal(t, workspace.RoleReader, FromRole(RoleReader))
-	assert.Equal(t, workspace.Role(""), FromRole("unknown"))
+	assert.Equal(t, workspace.RoleOwner, FromRoleToReearthx(RoleOwner))
+	assert.Equal(t, workspace.RoleMaintainer, FromRoleToReearthx(RoleMaintainer))
+	assert.Equal(t, workspace.RoleWriter, FromRoleToReearthx(RoleWriter))
+	assert.Equal(t, workspace.RoleReader, FromRoleToReearthx(RoleReader))
+	assert.Equal(t, workspace.Role(""), FromRoleToReearthx("unknown"))
 }
