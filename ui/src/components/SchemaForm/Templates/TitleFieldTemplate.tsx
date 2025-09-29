56b6edb555be0a3c207d73c0314586fbbe625af9
Bill Cook <billcook8122@gmail.com>
2025-09-30 07:40:31 +0900

    fix(ui): pass down originalSchema to titlefieldtemplate

--- ui/src/components/SchemaForm/Templates/TitleFieldTemplate.tsx
+++ ui/src/components/SchemaForm/Templates/TitleFieldTemplate.tsx
@@ -14,6 +14,8 @@ import {
 } from "@flow/components";
 import { extractDescriptions } from "@flow/features/Editor/components/ParamsDialog/utils/extractDescriptions";
 
+import { ExtendedFormContext } from "./BaseInputTemplate";
+
 /** The `TitleField` is the template to use to render the title of a field
  *
  * @param props - The `TitleFieldProps` for this component
@@ -27,12 +29,16 @@ const TitleFieldTemplate = <
   title,
   required,
   schema,
+  registry,
 }: TitleFieldProps<T, S, F>) => {
+  const formContext = registry.formContext;
+  const { originalSchema } = (formContext as ExtendedFormContext) || {};
   // If the schema has a $schema property, it means it's the root title
-  const isRootTitle = schema.$schema && schema.title === title;
-  const descriptions = extractDescriptions(schema);
+  const isRootTitle = schema.$schema;
+  const descriptions = extractDescriptions(originalSchema);
   const hasDescriptions = Object.keys(descriptions).length > 0;
 
+  console.log({ descriptions, schema });
   return (
     <Label id={id}>
       <div className="my-4 mb-1 flex flex-row items-center justify-between">
