adbf2111dc05e7b22730b44b5c94069c0c3855d6
Bill Cook <billcook8122@gmail.com>
2025-09-30 11:07:01 +0900

    fix(ui): refactor findParentWorkflowWithSubworkflowNode function to improve clarity and structure

--- ui/src/lib/yjs/useParentYWorkflow/addParentYworkflowNodePseudoPort.ts
+++ ui/src/lib/yjs/useParentYWorkflow/addParentYworkflowNodePseudoPort.ts
@@ -6,16 +6,25 @@ import type { Node, Workflow } from "@flow/types";
 import { yNodeConstructor } from "../conversions";
 import { YNodesMap, YWorkflow } from "../types";
 
-function findParentWorkflow(
+function findParentWorkflowWithSubworkflowNode(
   currentWorkflowId: string,
   rawWorkflows: Workflow[],
+  yWorkflows: Y.Map<YWorkflow>,
 ) {
   const parentWorkflow = rawWorkflows.find((w) => {
     const nodes = w.nodes as Node[];
     return nodes.some((n) => n.data.subworkflowId === currentWorkflowId);
   });
+  if (!parentWorkflow) return undefined;
+  const parentYWorkflow = yWorkflows.get(parentWorkflow.id);
+  if (!parentYWorkflow) return undefined;
+  const parentYNodes = parentYWorkflow.get("nodes") as YNodesMap;
+  const parentNodes = Object.values(parentYNodes.toJSON()) as Node[];
+  const subworkflowNode = parentNodes.find(
+    (n) => n.data.subworkflowId === currentWorkflowId,
+  );
 
-  return parentWorkflow;
+  return { subworkflowNode, parentYNodes };
 }
 
 export function addParentYWorkflowNodePseudoPort(
@@ -27,24 +36,19 @@ export function addParentYWorkflowNodePseudoPort(
   const isInputRouter = newNode.data.officialName === "InputRouter";
   const isOutputRouter = newNode.data.officialName === "OutputRouter";
   let hasNoPseudoInputsOrOutputs = false;
+  const parentWorkflowInfo = findParentWorkflowWithSubworkflowNode(
+    currentWorkflowId,
+    rawWorkflows,
+    yWorkflows,
+  );
+
   if (isInputRouter || isOutputRouter) {
-    const parentWorkflow = findParentWorkflow(currentWorkflowId, rawWorkflows);
-
-    if (parentWorkflow) {
-      const parentYWorkflow = yWorkflows.get(parentWorkflow.id);
-      if (parentYWorkflow) {
-        const parentYNodes = parentYWorkflow.get("nodes") as YNodesMap;
-        const parentNodes = Object.values(parentYNodes.toJSON()) as Node[];
-        const subworkflowNode = parentNodes.find(
-          (n) => n.data.subworkflowId === currentWorkflowId,
-        );
-
-        if (subworkflowNode) {
-          hasNoPseudoInputsOrOutputs =
-            (isInputRouter && !subworkflowNode.data.pseudoInputs?.length) ||
-            (isOutputRouter && !subworkflowNode.data.pseudoOutputs?.length);
-        }
-      }
+    if (parentWorkflowInfo?.subworkflowNode) {
+      hasNoPseudoInputsOrOutputs =
+        (isInputRouter &&
+          !parentWorkflowInfo.subworkflowNode.data.pseudoInputs?.length) ||
+        (isOutputRouter &&
+          !parentWorkflowInfo.subworkflowNode.data.pseudoOutputs?.length);
     }
   }
 
@@ -53,40 +57,25 @@ export function addParentYWorkflowNodePseudoPort(
       ...newNode.data.params,
       routingPort: DEFAULT_ROUTING_PORT,
     };
-  }
 
-  if (hasNoPseudoInputsOrOutputs) {
-    const parentWorkflow = findParentWorkflow(currentWorkflowId, rawWorkflows);
+    if (parentWorkflowInfo?.subworkflowNode) {
+      const newPseudoPort = {
+        nodeId: newNode.id,
+        portName: DEFAULT_ROUTING_PORT,
+      };
 
-    if (parentWorkflow) {
-      const parentYWorkflow = yWorkflows.get(parentWorkflow.id);
-      if (parentYWorkflow) {
-        const parentYNodes = parentYWorkflow.get("nodes") as YNodesMap;
-        const parentNodes = Object.values(parentYNodes.toJSON()) as Node[];
-        const subworkflowNode = parentNodes.find(
-          (n) => n.data.subworkflowId === currentWorkflowId,
-        );
+      const updatedSubworkflowNode = { ...parentWorkflowInfo?.subworkflowNode };
 
-        if (subworkflowNode) {
-          const newPseudoPort = {
-            nodeId: newNode.id,
-            portName: DEFAULT_ROUTING_PORT,
-          };
-
-          const updatedSubworkflowNode = { ...subworkflowNode };
-
-          if (isInputRouter) {
-            updatedSubworkflowNode.data.pseudoInputs = [newPseudoPort];
-          } else if (isOutputRouter) {
-            updatedSubworkflowNode.data.pseudoOutputs = [newPseudoPort];
-          }
-
-          parentYNodes.set(
-            subworkflowNode.id,
-            yNodeConstructor(updatedSubworkflowNode),
-          );
-        }
+      if (isInputRouter) {
+        updatedSubworkflowNode.data.pseudoInputs = [newPseudoPort];
+      } else if (isOutputRouter) {
+        updatedSubworkflowNode.data.pseudoOutputs = [newPseudoPort];
       }
+
+      parentWorkflowInfo.parentYNodes.set(
+        parentWorkflowInfo?.subworkflowNode.id,
+        yNodeConstructor(updatedSubworkflowNode),
+      );
     }
   }
 }
