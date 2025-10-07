ce007e33f315a2119f60b670a3713fc1308bde33
Bill Cook <billcook8122@gmail.com>
2025-10-07 09:11:54 +0900

    fix(ui): preserve original title when simplifying anyOf with a single type

--- ui/src/components/SchemaForm/patchSchemaTypes.ts
+++ ui/src/components/SchemaForm/patchSchemaTypes.ts
@@ -26,10 +26,10 @@ const simplifyAnyOf = (
     // If only one type remains, replace `anyOf` with that schema
     if (filteredSchemas.length === 1) {
       if (isJSONSchema(filteredSchemas[0])) {
-        const title = filteredSchemas[0].title || newSchema.title;
+        const originalTitle = newSchema.title;
         newSchema = { ...filteredSchemas[0] };
-        // Preserve the title
-        if (title) newSchema.title = title;
+        // Preserve the title if missing from the new schema
+        if (!newSchema.title && originalTitle) newSchema.title = originalTitle;
       }
     } else {
       newSchema.anyOf = filteredSchemas;
