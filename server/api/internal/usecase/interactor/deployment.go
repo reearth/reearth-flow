969b53caf40b288c7435659f21a505486f884f07
Piyush Chauhan <42397980+pyshx@users.noreply.github.com>
2025-10-24 01:45:42 +0530

    fix(api): add FindByDeployment method to trigger and error handling for active triggers (#1579)

--- server/api/internal/usecase/interactor/deployment.go
+++ server/api/internal/usecase/interactor/deployment.go
@@ -23,6 +23,7 @@ type Deployment struct {
 	projectRepo       repo.Project
 	workflowRepo      repo.Workflow
 	jobRepo           repo.Job
+	triggerRepo       repo.Trigger
 	transaction       usecasex.Transaction
 	batch             gateway.Batch
 	file              gateway.File
@@ -36,6 +37,7 @@ func NewDeployment(r *repo.Container, gr *gateway.Container, jobUsecase interfac
 		projectRepo:       r.Project,
 		workflowRepo:      r.Workflow,
 		jobRepo:           r.Job,
+		triggerRepo:       r.Trigger,
 		transaction:       r.Transaction,
 		batch:             gr.Batch,
 		file:              gr.File,
@@ -245,6 +247,14 @@ func (i *Deployment) Update(ctx context.Context, dp interfaces.UpdateDeploymentP
 func (i *Deployment) Delete(ctx context.Context, deploymentID id.DeploymentID) (err error) {
 	if err := i.checkPermission(ctx, rbac.ActionAny); err != nil {
 		return err
+	}
+
+	triggers, err := i.triggerRepo.FindByDeployment(ctx, deploymentID)
+	if err != nil {
+		return err
+	}
+	if len(triggers) > 0 {
+		return interfaces.ErrDeploymentHasTriggers
 	}
 
 	tx, err := i.transaction.Begin(ctx)
