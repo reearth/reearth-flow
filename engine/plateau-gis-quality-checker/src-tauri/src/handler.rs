7519f06dd643a10b854dbf553b1e4da003ec2b51
akiyatomohiro <thairwars@gmail.com>
2025-10-24 16:01:48 +0930

    add ingress_state to Runner::run

--- engine/plateau-gis-quality-checker/src-tauri/src/handler.rs
+++ engine/plateau-gis-quality-checker/src-tauri/src/handler.rs
@@ -53,10 +53,18 @@ pub(crate) async fn run_flow(
     let job_id = uuid::Uuid::new_v4();
     let action_log_uri = setup_job_directory("plateau-gis-quality-checker", "action-log", job_id)
         .map_err(crate::errors::Error::setup)?;
-    let state_uri = setup_job_directory("plateau-gis-quality-checker", "feature-store", job_id)
-        .map_err(crate::errors::Error::setup)?;
-    let state =
-        Arc::new(State::new(&state_uri, &storage_resolver).map_err(crate::errors::Error::setup)?);
+    let ingress_state_uri =
+        setup_job_directory("plateau-gis-quality-checker", "ingress-store", job_id)
+            .map_err(crate::errors::Error::setup)?;
+    let ingress_state = Arc::new(
+        State::new(&ingress_state_uri, &storage_resolver).map_err(crate::errors::Error::setup)?,
+    );
+    let feature_state_uri =
+        setup_job_directory("plateau-gis-quality-checker", "feature-store", job_id)
+            .map_err(crate::errors::Error::setup)?;
+    let feature_state = Arc::new(
+        State::new(&feature_state_uri, &storage_resolver).map_err(crate::errors::Error::setup)?,
+    );
 
     let logger_factory = Arc::new(LoggerFactory::new(
         create_root_logger(action_log_uri.path()),
@@ -68,7 +76,8 @@ pub(crate) async fn run_flow(
         ALL_ACTION_FACTORIES.clone(),
         logger_factory,
         storage_resolver,
-        state,
+        ingress_state,
+        feature_state,
     )
     .await
     .map_err(crate::errors::Error::execute_failed)
