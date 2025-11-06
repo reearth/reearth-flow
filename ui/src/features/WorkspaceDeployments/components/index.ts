afaf9c3d63ef9062e7908391847160d547a2fe0b
billcookie <102199454+billcookie@users.noreply.github.com>
2025-11-06 16:41:05 +0900

    feat(ui): added confirmation for debug and deployment runs. (#1608)

--- ui/src/features/WorkspaceDeployments/components/index.ts
+++ ui/src/features/WorkspaceDeployments/components/index.ts
@@ -2,3 +2,4 @@ export * from "./DeploymentAddDialog";
 export * from "./DeploymentDetails";
 export * from "./DeploymentDeletionDialog";
 export * from "./DeploymentEditDialog";
+export * from "./DeploymentRunDialog";
