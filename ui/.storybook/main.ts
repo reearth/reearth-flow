29fcdb933843c9f5e8536e3e79b22c70e4018bc1
Bill Cook <billcook8122@gmail.com>
2025-11-20 15:41:02 +0900

    chore(ui): storybook auto migration check v9 to v10

--- ui/.storybook/main.ts
+++ ui/.storybook/main.ts
@@ -2,17 +2,17 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   core: {
     disableTelemetry: true,
   },
+
   staticDirs: ["./assets"],
   addons: ["@storybook/addon-links", "@storybook/addon-onboarding"],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+  }
 };
 export default config;
