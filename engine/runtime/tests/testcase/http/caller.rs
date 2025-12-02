2fe0b3ed296b48cff0b9886e4e7a4ad98f4f000d
xy <xyzmhx@gmail.com>
2025-12-03 04:09:06 +0900

    refactor(tests): remove unnecessary comment from HTTP caller test case

--- engine/runtime/tests/testcase/http/caller.rs
+++ engine/runtime/tests/testcase/http/caller.rs
@@ -1,7 +1,7 @@
 use crate::helper::execute;
 
 #[test]
-#[ignore] // Ignore by default as it requires network access
+#[ignore]
 fn test_http_caller() {
     let result = execute("http/caller", vec!["test_data.json"]);
     assert!(result.is_ok());
