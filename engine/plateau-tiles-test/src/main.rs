8a673cdebb8980edf9e2f60e1b241a6a2aa40e06
asrcpq <asrcpq@gmail.com>
2026-01-20 15:13:11 +0900

    rename to lod1

--- engine/plateau-tiles-test/src/main.rs
+++ engine/plateau-tiles-test/src/main.rs
@@ -93,7 +93,7 @@ fn zip_dir(src_dir: &Path, zip_path: &Path) {
 }
 
 const DEFAULT_TESTS: &[&str] = &[
-    "data-convert/plateau4/01-bldg/lod01",
+    "data-convert/plateau4/01-bldg/lod1",
     "data-convert/plateau4/02-tran-rwy-trk-squr-wwy/multipolygon",
     "data-convert/plateau4/02-tran-rwy-trk-squr-wwy/squr",
     "data-convert/plateau4/02-tran-rwy-trk-squr-wwy/dm",
