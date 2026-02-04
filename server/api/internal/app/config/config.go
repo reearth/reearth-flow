f3c9c53f53d7c4df026530b21b771e183eba6806
pyshx <piyushchauhan1004@gmail.com>
2026-02-04 12:06:38 +0530

    gofmt

--- server/api/internal/app/config/config.go
+++ server/api/internal/app/config/config.go
@@ -79,7 +79,7 @@ type (
 		Worker_PubSubLogStreamTopic            string `envconfig:"WORKER_PUBSUB_LOG_STREAM_TOPIC" default:"flow-log-stream" pp:",omitempty"`
 		Worker_PubSubNodeStatusTopic           string `envconfig:"WORKER_PUBSUB_NODE_STATUS_TOPIC" default:"flow-node-status" pp:",omitempty"`
 		Worker_PubSubUserFacingLogTopic        string `envconfig:"WORKER_PUBSUB_USER_FACING_LOG_TOPIC" default:"flow-user-facing-log" pp:",omitempty"`
-		Worker_MaxRunDurationSeconds            string `envconfig:"WORKER_MAX_RUN_DURATION_SECONDS" default:"21600" pp:",omitempty"`
+		Worker_MaxRunDurationSeconds           string `envconfig:"WORKER_MAX_RUN_DURATION_SECONDS" default:"21600" pp:",omitempty"`
 		Worker_TaskCount                       string `envconfig:"WORKER_TASK_COUNT" default:"1" pp:",omitempty"`
 		Worker_ThreadPoolSize                  string `envconfig:"WORKER_THREAD_POOL_SIZE" default:"30" pp:",omitempty"`
 		Worker_RustLog                         string `envconfig:"WORKER_RUST_LOG" default:"info" pp:",omitempty"`
