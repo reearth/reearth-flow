a8d637d6cf42181ebb48b500cac9080dc77f58b5
rot1024 <aayhrot@gmail.com>
2026-02-05 22:16:15 +0900

    refactor(engine): replace DOM parsing with StreamTransformer in plateau processors
    Upgrade fastxml to 0.8.0 and migrate plateau3/plateau4 processors from
    full CityGML DOM loading to streaming XML processing using StreamTransformer,
    reducing memory usage for large CityGML files.

--- engine/runtime/action-plateau-processor/src/plateau4/building_installation_geometry_type_checker.rs
+++ engine/runtime/action-plateau-processor/src/plateau4/building_installation_geometry_type_checker.rs
@@ -1,7 +1,9 @@
+use std::cell::RefCell;
 use std::collections::HashMap;
+use std::rc::Rc;
 use std::str::FromStr;
 
-use nusamai_citygml::GML31_NS;
+use fastxml::transform::StreamTransformer;
 use reearth_flow_common::{uri::Uri, xml};
 use reearth_flow_runtime::{
     errors::BoxedError,
@@ -121,66 +123,112 @@ impl BuildingInstallationGeometryTypeChecker {
             .get_sync(path_uri.path().as_path())
             .map_err(|e| Error::FileRead(format!("{e:?}")))?;
         let xml_content = String::from_utf8(xml_content.to_vec())?;
-        let document = xml::parse(xml_content.as_str())?;
-        let root_node = xml::get_root_readonly_node(&document)?;
-        let xml_ctx = xml::create_context(&document)?;
-        let buildings =
-            xml::find_readonly_nodes_by_xpath(&xml_ctx, "*//bldg:Building", &root_node)?;
-        for building in &buildings {
-            let building_installations = xml::find_readonly_nodes_by_xpath(
-                &xml_ctx,
-                ".//bldg:BuildingInstallation",
-                building,
-            )?;
-            for building_installation in &building_installations {
-                let mut tags = Vec::new();
-                for n in [2, 3, 4] {
-                    let geom = xml::find_readonly_nodes_by_xpath(
-                        &xml_ctx,
-                        format!("./bldg:lod{n}Geometry/*").as_str(),
-                        building_installation,
-                    )?;
-                    geom.iter().for_each(|g| {
-                        let geom_type = xml::get_readonly_node_tag(g);
-                        tags.push(geom_type);
-                    });
+
+        let stream_error: Rc<RefCell<Option<Error>>> = Rc::new(RefCell::new(None));
+
+        let transformer = StreamTransformer::new(xml_content.as_str())
+            .with_root_namespaces()
+            .map_err(|e| Error::FileRead(format!("{e:?}")))?;
+
+        let stream_error_clone = Rc::clone(&stream_error);
+        let ctx = &ctx;
+        transformer
+            .on("//bldg:Building", move |node| {
+                if stream_error_clone.borrow().is_some() {
+                    return;
                 }
-                for tag in &tags {
-                    let mut feature = feature.clone();
-                    feature.refresh_id();
-                    let attributes = HashMap::from([
-                        (
-                            Attribute::new("bldgGmlId"),
-                            AttributeValue::String(
-                                building
-                                    .get_attribute_ns(
-                                        "id",
-                                        std::str::from_utf8(GML31_NS.into_inner()).unwrap(),
-                                    )
-                                    .unwrap_or_default(),
-                            ),
-                        ),
-                        (
-                            Attribute::new("instGmlId"),
-                            AttributeValue::String(
-                                building_installation
-                                    .get_attribute_ns(
-                                        "id",
-                                        std::str::from_utf8(GML31_NS.into_inner()).unwrap(),
-                                    )
-                                    .unwrap_or_default(),
-                            ),
-                        ),
-                        (
-                            Attribute::new("geomTag"),
-                            AttributeValue::String(tag.clone()),
-                        ),
-                    ]);
-                    feature.attributes_mut().extend(attributes);
-                    fw.send(ctx.new_with_feature_and_port(feature, DEFAULT_PORT.clone()));
+
+                let doc = node.document();
+                let mut xml_ctx = match xml::create_context(doc) {
+                    Ok(ctx) => ctx,
+                    Err(e) => {
+                        *stream_error_clone.borrow_mut() = Some(Error::FileRead(format!("{e:?}")));
+                        return;
+                    }
+                };
+                for (prefix, uri) in node.namespaces() {
+                    let _ = xml_ctx.register_namespace(prefix, uri);
                 }
-            }
+                let root_node = match xml::get_root_readonly_node(doc) {
+                    Ok(n) => n,
+                    Err(e) => {
+                        *stream_error_clone.borrow_mut() = Some(Error::FileRead(format!("{e:?}")));
+                        return;
+                    }
+                };
+
+                let building = &root_node;
+                let building_installations = match xml::find_readonly_nodes_by_xpath(
+                    &xml_ctx,
+                    ".//bldg:BuildingInstallation",
+                    building,
+                ) {
+                    Ok(nodes) => nodes,
+                    Err(e) => {
+                        *stream_error_clone.borrow_mut() = Some(Error::FileRead(format!("{e:?}")));
+                        return;
+                    }
+                };
+                for building_installation in &building_installations {
+                    let mut tags = Vec::new();
+                    for n in [2, 3, 4] {
+                        let geom = match xml::find_readonly_nodes_by_xpath(
+                            &xml_ctx,
+                            format!("./bldg:lod{n}Geometry/*").as_str(),
+                            building_installation,
+                        ) {
+                            Ok(nodes) => nodes,
+                            Err(e) => {
+                                *stream_error_clone.borrow_mut() =
+                                    Some(Error::FileRead(format!("{e:?}")));
+                                return;
+                            }
+                        };
+                        geom.iter().for_each(|g| {
+                            let geom_type = xml::get_readonly_node_tag(g);
+                            tags.push(geom_type);
+                        });
+                    }
+                    for tag in &tags {
+                        let mut feature = feature.clone();
+                        feature.refresh_id();
+                        let attributes = HashMap::from([
+                            (
+                                Attribute::new("bldgGmlId"),
+                                AttributeValue::String(
+                                    building
+                                        .get_attribute_ns("id", "http://www.opengis.net/gml")
+                                        .unwrap_or_default(),
+                                ),
+                            ),
+                            (
+                                Attribute::new("instGmlId"),
+                                AttributeValue::String(
+                                    building_installation
+                                        .get_attribute_ns("id", "http://www.opengis.net/gml")
+                                        .unwrap_or_default(),
+                                ),
+                            ),
+                            (
+                                Attribute::new("geomTag"),
+                                AttributeValue::String(tag.clone()),
+                            ),
+                        ]);
+                        feature.attributes_mut().extend(attributes);
+                        fw.send(ctx.new_with_feature_and_port(feature, DEFAULT_PORT.clone()));
+                    }
+                }
+            })
+            .for_each()
+            .map_err(|e| Error::FileRead(format!("{e:?}")))?;
+
+        if let Some(err) = Rc::try_unwrap(stream_error)
+            .expect("all callback references should be dropped after for_each()")
+            .into_inner()
+        {
+            return Err(err);
         }
+
         Ok(())
     }
 }
