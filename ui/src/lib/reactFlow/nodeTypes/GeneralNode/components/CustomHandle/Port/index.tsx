8e21e53cfa494238dcd3a86d5c54439eecf176d3
Bill Cook <billcook8122@gmail.com>
2026-04-02 11:24:10 +0900

    fix(ui): style changes

--- ui/src/lib/reactFlow/nodeTypes/GeneralNode/components/CustomHandle/Port/index.tsx
+++ ui/src/lib/reactFlow/nodeTypes/GeneralNode/components/CustomHandle/Port/index.tsx
@@ -1,4 +1,4 @@
-import { TableIcon } from "@phosphor-icons/react";
+import { TableIcon, XCircleIcon } from "@phosphor-icons/react";
 import { Position } from "@xyflow/react";
 import { memo } from "react";
 
@@ -28,19 +28,20 @@ const Port: React.FC<Props> = ({ nodeId, nodeData, portName, readonly }) => {
   return (
     <div className="relative flex items-center justify-end border-b py-0.5 last-of-type:border-none">
       <CustomHandle
+        id={portName}
         type="source"
         className="right-1 z-10 w-[8px] rounded-none transition-colors"
         position={Position.Right}
-        id={portName}
+        isConnectable={hasIntermediateData ? 1 : 0}
       />
       <div className="flex w-full min-w-0 -translate-x-0.5 items-center justify-end gap-1">
         <p
           className={`min-w-0 text-end text-[10px] ${getBreakClass(portName)} italic dark:font-thin`}>
           {portName}
         </p>
-        {hasIntermediateData ? (
+        {hasIntermediateData && jobStatus === "completed" ? (
           <IconButton
-            className={`z-10 h-3 w-3 shrink-0 hover:text-success ${
+            className={`z-11 h-3 w-3 shrink-0 hover:bg-transparent hover:text-success ${
               jobStatus === "completed" && isSelected
                 ? "text-success"
                 : "text-success/60"
@@ -48,6 +49,10 @@ const Port: React.FC<Props> = ({ nodeId, nodeData, portName, readonly }) => {
             aria-label={`View intermediate data for ${portName}`}
             icon={<TableIcon />}
             onClick={handleClick}
+          />
+        ) : jobStatus === "failed" ? (
+          <XCircleIcon
+            className={`hover:text-error/60 z-11 h-3 w-3 shrink-0 text-destructive`}
           />
         ) : (
           <div className="size-1.5 shrink-0 rounded-full bg-zinc-400 dark:bg-gray-300" />
