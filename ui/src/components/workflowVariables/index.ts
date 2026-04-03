ddc1e2fd0bff3a96c1b6ffd461fb3b80c184dd4e
Bill Cook <billcook8122@gmail.com>
2026-04-03 12:03:36 +0900

    fix(ui): add ArrayDefaultItemInput component and integrate it into VariableArrayInput and ArrayEditor

--- ui/src/components/workflowVariables/index.ts
+++ ui/src/components/workflowVariables/index.ts
@@ -1,4 +1,5 @@
 export * from "./VariableRow";
+export { default as ArrayDefaultItemInput } from "./ArrayDefaultItemInput";
 export { default as CheckboxDefaultInput } from "./CheckboxDefaultInput";
 export { default as DateTimeDefaultValueInput } from "./DateTimeDefaultValueInput";
 export { default as NumberDefaultValueInput } from "./NumberDefaultValueInput";
