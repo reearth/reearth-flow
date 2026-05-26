8f3495e6a485cb1a3d8b5035be25f940b56c0213
Bill Cook <billcook8122@gmail.com>
2026-05-26 12:16:37 +0900

    fix(ui): enhance click handling for Port component with double-click support and prevent node selection on singleClick

--- ui/src/lib/reactFlow/nodeTypes/GeneralNode/components/CustomHandle/Port/index.tsx
+++ ui/src/lib/reactFlow/nodeTypes/GeneralNode/components/CustomHandle/Port/index.tsx
@@ -39,7 +39,13 @@ const Port: React.FC<Props> = ({ nodeId, nodeData, portName, readonly }) => {
       <div className="group flex w-full min-w-0 -translate-x-0.5 items-center justify-end gap-1">
         <div
           className={`flex items-center gap-1 rounded-sm  px-0.5 ${hasData ? "group-hover:cursor-pointer group-hover:bg-success/20 " : ""}${isSelected ? "bg-success/20" : ""}`}
-          onClick={hasData ? handleClick : undefined}>
+          onDoubleClick={(e) => {
+            e.stopPropagation();
+          }}
+          onClick={(e) => {
+            e.stopPropagation();
+            if (hasData) handleClick();
+          }}>
           <p
             className={`min-w-0 text-end text-[10px] ${getBreakClass(portName)} italic dark:font-thin ${
               hasData && isSelected
