ee51b355eae2c72ddb6695cc901be60dad567ab1
asrcpq <asrcpq@gmail.com>
2026-06-05 12:16:16 +0900

    minor improve

--- engine/runtime/expr/src/core/builtins/math.rs
+++ engine/runtime/expr/src/core/builtins/math.rs
@@ -2,8 +2,9 @@ use crate::core::value::{Module, NativeFn, Value};
 use crate::expect_arity;
 
 fn unary_float(name: &'static str, f: fn(f64) -> f64) -> Value {
+    let full_name = format!("math.{name}");
     Value::Fn(NativeFn::new(move |args| {
-        expect_arity(&format!("math.{name}"), args, 1, 1)?;
+        expect_arity(&full_name, args, 1, 1)?;
         Ok(Value::Float(f(args[0].as_f64()?)))
     }))
 }
