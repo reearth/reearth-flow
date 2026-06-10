65b2717a458ae805eb0e9ba18025e43ad86e7de0
Bill Cook <billcook8122@gmail.com>
2026-06-10 09:33:31 +0900

    chore(ui): update import statement for RequestOptions type in graphql-request

--- ui/src/lib/gql/__gen__/plugins/graphql-request.ts
+++ ui/src/lib/gql/__gen__/plugins/graphql-request.ts
@@ -2,7 +2,7 @@
 type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 /** Internal type. DO NOT USE DIRECTLY. */
 export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
-import { GraphQLClient, RequestOptions } from 'graphql-request';
+import { GraphQLClient, type RequestOptions } from 'graphql-request';
 import gql from 'graphql-tag';
 type GraphQLClientRequestHeaders = RequestOptions['requestHeaders'];
 export type ApiDriverInput = {
