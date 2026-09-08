8077fc4f1b07279d4c377e9ffe6ce7f6a197afa1
asrcpq <asrcpq@gmail.com>
2026-09-08 10:54:13 +0900

    fix test

--- engine/testing/plateau-tiles-test/src/main.rs
+++ engine/testing/plateau-tiles-test/src/main.rs
@@ -153,6 +153,7 @@ const DEFAULT_TESTS: &[&str] = &[
     "data-convert/plateau6/01-bldg/osaka-ward",
     "data-convert/plateau6/02-tran-rwy-trk-squr-wwy/multipolygon",
     "data-convert/plateau6/02-tran-rwy-trk-squr-wwy/dm",
+    "data-convert/plateau6/02-tran-rwy-trk-squr-wwy/squr_xlink",
 ];
 
 fn run_test<F>(test_name: &str, relative_path: &std::path::Display, test_fn: F)
